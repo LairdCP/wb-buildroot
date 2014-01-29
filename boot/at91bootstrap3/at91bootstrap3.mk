e0ac53d898658a96d461896b681177f7d3589d71
Steve deRosier <Steve.Derosier@lairdtech.com>
2014-01-28 16:46:21 -0800

    [Laird] Patch bootstrap to update

--- boot/at91bootstrap3/at91bootstrap3.mk
+++ boot/at91bootstrap3/at91bootstrap3.mk
@@ -3,10 +3,10 @@
 # at91bootstrap3
 #
 #############################################################
-AT91BOOTSTRAP3_VERSION = 3.2
+AT91BOOTSTRAP3_VERSION = 3.4.4
 AT91BOOTSTRAP3_SITE = \
 	ftp://www.at91.com/pub/at91bootstrap/AT91Bootstrap$(AT91BOOTSTRAP3_VERSION)
-AT91BOOTSTRAP3_SOURCE = at91bootstrap_9n12.tar.gz
+AT91BOOTSTRAP3_SOURCE = AT91Bootstrap-v3.4.4.tar.xz
 
 AT91BOOTSTRAP3_INSTALL_IMAGES = YES
 AT91BOOTSTRAP3_INSTALL_TARGET = NO
@@ -21,11 +21,14 @@ AT91BOOTSTRAP3_CUSTOM_PATCH_DIR = \
 AT91BOOTSTRAP3_MAKE_OPT = CROSS_COMPILE=$(TARGET_CROSS) DESTDIR=$(BINARIES_DIR)
 
 ifneq ($(AT91BOOTSTRAP3_CUSTOM_PATCH_DIR),)
+define AT91BOOTSTRAP3_REMOVE_SPECIFIC_PATCHES
+	rm -fv boot/at91bootstrap3/at91bootstrap3-u-boot-relocation-fix.patch
+endef
 define AT91BOOTSTRAP3_APPLY_CUSTOM_PATCHES
 	support/scripts/apply-patches.sh $(@D) $(AT91BOOTSTRAP3_CUSTOM_PATCH_DIR) \
 		at91bootstrap3-\*.patch
 endef
-
+AT91BOOTSTRAP3_PRE_PATCH_HOOKS += AT91BOOTSTRAP3_REMOVE_SPECIFIC_PATCHES
 AT91BOOTSTRAP3_POST_PATCH_HOOKS += AT91BOOTSTRAP3_APPLY_CUSTOM_PATCHES
 endif
 
@@ -42,11 +45,17 @@ define AT91BOOTSTRAP3_CONFIGURE_CMDS
 endef
 
 define AT91BOOTSTRAP3_BUILD_CMDS
-	$(MAKE) $(AT91BOOTSTRAP3_MAKE_OPT) -C $(@D) boot
+	$(MAKE) $(AT91BOOTSTRAP3_MAKE_OPT) -C $(@D)
 endef
 
+AT91BOOTSTRAP3_BIN_FILE = $(call qstrip,$(BR2_LRD_PLATFORM))-nandflashboot-uboot.bin
+ifeq ($(call qstrip,$(BR2_LRD_PLATFORM)),wb45n)
+AT91BOOTSTRAP3_BIN_FILE = at91sam9x5ek-nandflashboot-uboot.bin
+endif
+
 define AT91BOOTSTRAP3_INSTALL_IMAGES_CMDS
-	$(MAKE) $(AT91BOOTSTRAP3_MAKE_OPT) -C $(@D) bootstrap
+	$(INSTALL) -m 644 $(@D)/binaries/$(AT91BOOTSTRAP3_BIN_FILE) \
+	                  $(BINARIES_DIR)/$(call qstrip,$(BR2_LRD_PLATFORM)).bin
 endef
 
 $(eval $(generic-package))
