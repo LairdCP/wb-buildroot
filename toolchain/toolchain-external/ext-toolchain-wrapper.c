7af8fd4dfb84562d4c6f19e13b98b0250fa8381a
Steve deRosier <Steve.Derosier@lairdtech.com>
2014-01-30 13:34:00 -0800

    Revert "Make external toolchain relocatable"
    This reverts commit befb9a3ee0c5994162f527d8ef763d57d68ec48e.
    
       Remove the buildroot change that makes the buildroot extenral toolchain
       wrapper relocatable because it is buggy. When adding the that external
       toolchian wrapper to the PATH, it can't find its sysroot:
       http://lists.busybox.net/pipermail/buildroot/2013-February/067402.html

--- toolchain/toolchain-external/ext-toolchain-wrapper.c
+++ toolchain/toolchain-external/ext-toolchain-wrapper.c
@@ -2,12 +2,9 @@
  * Buildroot wrapper for external toolchains. This simply executes the real
  * toolchain with a number of arguments (sysroot/arch/..) hardcoded,
  * to ensure the external toolchain uses the correct configuration.
- * The hardcoded path arguments are defined relative to the actual location
- * of the binary.
  *
  * (C) 2011 Peter Korsgaard <jacmet@sunsite.dk>
  * (C) 2011 Daniel Nyström <daniel.nystrom@timeterminal.se>
- * (C) 2012 Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
  *
  * This file is licensed under the terms of the GNU General Public License
  * version 2.  This program is licensed "as is" without any warranty of any
@@ -20,12 +17,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-static char path[PATH_MAX];
-static char sysroot[PATH_MAX];
+static char path[PATH_MAX] = BR_CROSS_PATH;
 
 static char *predef_args[] = {
 	path,
-	"--sysroot", sysroot,
+	"--sysroot", BR_SYSROOT,
 #ifdef BR_ARCH
 	"-march=" BR_ARCH,
 #endif /* BR_ARCH */
@@ -52,50 +48,22 @@ static char *predef_args[] = {
 #endif
 };
 
+static const char *get_basename(const char *name)
+{
+	const char *base;
+
+	base = strrchr(name, '/');
+	if (base)
+		base++;
+	else
+		base = name;
+
+	return base;
+}
+
 int main(int argc, char **argv)
 {
 	char **args, **cur;
-	char *relbasedir, *absbasedir;
-	char *progpath = argv[0];
-	char *basename;
-	int ret;
-
-	/* Calculate the relative paths */
-	basename = strrchr(progpath, '/');
-	if (basename) {
-		*basename = '\0';
-		basename++;
-		relbasedir = malloc(strlen(progpath) + 7);
-		if (relbasedir == NULL) {
-			perror(__FILE__ ": malloc");
-			return 2;
-		}
-		sprintf(relbasedir, "%s/../..", argv[0]);
-		absbasedir = realpath(relbasedir, NULL);
-	} else {
-		basename = progpath;
-		absbasedir = realpath("../..", NULL);
-	}
-	if (absbasedir == NULL) {
-		perror(__FILE__ ": realpath");
-		return 2;
-	}
-
-	/* Fill in the relative paths */
-#ifdef BR_CROSS_PATH_REL
-	ret = snprintf(path, sizeof(path), "%s/" BR_CROSS_PATH_REL "/%s", absbasedir, basename);
-#else /* BR_CROSS_PATH_ABS */
-	ret = snprintf(path, sizeof(path), BR_CROSS_PATH_ABS "/%s", basename);
-#endif
-	if (ret >= sizeof(path)) {
-		perror(__FILE__ ": overflow");
-		return 3;
-	}
-	ret = snprintf(sysroot, sizeof(sysroot), "%s/" BR_SYSROOT, absbasedir);
-	if (ret >= sizeof(sysroot)) {
-		perror(__FILE__ ": overflow");
-		return 3;
-	}
 
 	cur = args = malloc(sizeof(predef_args) + (sizeof(char *) * argc));
 	if (args == NULL) {
@@ -113,6 +81,8 @@ int main(int argc, char **argv)
 
 	/* finish with NULL termination */
 	*cur = NULL;
+
+	strcat(path, get_basename(argv[0]));
 
 	if (execv(path, args))
 		perror(path);
