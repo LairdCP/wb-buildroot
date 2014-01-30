7038eb6f564843228a4e8b10d910513c77cedefb
Steve deRosier <Steve.Derosier@lairdtech.com>
2014-01-30 09:03:39 -0800

    [Laird] openssh: move startup and config directories
    Move the startup script to /etc/init.d/opt
    Add config option to force config directory /etc/ssh

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -8,12 +8,13 @@ OPENSSH_VERSION = 6.1p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
 OPENSSH_CONF_OPT = --libexecdir=/usr/lib --disable-lastlog --disable-utmp \
-		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip
+		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip \
+		--sysconfdir=/etc/ssh
 
 OPENSSH_DEPENDENCIES = zlib openssl
 
 define OPENSSH_INSTALL_INITSCRIPT
-	$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/S50sshd
+	$(INSTALL) -D -m 755 package/openssh/S50sshd $(TARGET_DIR)/etc/init.d/opt/S50sshd
 endef
 
 OPENSSH_POST_INSTALL_TARGET_HOOKS += OPENSSH_INSTALL_INITSCRIPT
