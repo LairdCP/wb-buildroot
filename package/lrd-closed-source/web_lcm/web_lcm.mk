a7ed91b6788c675d2b1781f42e8ae3407f9532bd
Steve deRosier <Steve.Derosier@lairdtech.com>
2014-03-07 16:11:15 -0800

    Integrate web_lcm as a buildroot project
    Bug: 4719

---
+++ package/lrd-closed-source/web_lcm/web_lcm.mk
@@ -0,0 +1,27 @@
+WEB_LCM_VERSION = local
+WEB_LCM_SITE = package/lrd-closed-source/externals/web_lcm
+WEB_LCM_SITE_METHOD = local
+
+define WEB_LCM_INSTALL_TARGET_CMDS
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/assets/css
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/assets/img
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/assets/js
+	mkdir -p -m 0775 $(TARGET_DIR)/etc/lighttpd
+	$(INSTALL) -D -m 0755 $(@D)/advancedconfig.html \
+				$(@D)/about.html \
+				$(@D)/globalconfig.html \
+				$(@D)/ifaceconfig.html \
+				$(@D)/profileconfig.html \
+				$(@D)/status.html \
+		 $(TARGET_DIR)/var/www/docs
+	$(INSTALL) -D -m 0775 $(@D)/assets/css/bootstrap-responsive.min.css $(TARGET_DIR)/var/www/docs/assets/css
+	$(INSTALL) -D -m 0755 $(@D)/assets/css/bootstrap.min.css $(TARGET_DIR)/var/www/docs/assets/css
+	$(INSTALL) -D -m 0755 $(@D)/assets/img/laird_logo.png $(TARGET_DIR)/var/www/docs/assets/img
+	$(INSTALL) -D -m 0755 $(@D)/assets/js/bootstrap.min.js
+							$(TARGET_DIR)/var/www/docs/assets/js
+	$(INSTALL) -D -m 0755 $(@D)/lighttpd.conf \
+				$(@D)/lighttpd.password \
+		$(TARGET_DIR)/etc/lighttpd
+endef
+
+$(eval $(generic-package))
