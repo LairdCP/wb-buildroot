33103296761b751cd33d564fcfd3e75435276253
Jon Hefling <jon.hefling@lairdtech.com>
2014-05-07 11:06:36 -0400

    Rewrite mkfwtxt.sh for new version of fw_update.
    First line contains WB target-build description.
    And image line entries contain: md5, name, bytes
    
    Individual images may easily enabled/disabled near top of script.
    The resulting update-list in new format for fw_update 2.0.
    And there is also a duplicated listing in old format.
    
    Bug 5375.

--- board/laird/mkfwtxt.sh
+++ board/laird/mkfwtxt.sh
@@ -1,16 +1,77 @@
 #!/bin/sh
-set -e
+# create update-list for fw_update
+# This script is to be run within the directory containing image files.
 
-URL="$1"
-TGT=fw.txt
+# update-list file
+fwul=fw.txt
 
-echo -n > "$TGT"
+# optional url
+url=${1%/}
 
-for f in bootstrap.bin u-boot.bin kernel.bin rootfs.bin; do
-    if [ -e $f ]; then
-        if [ $f = "bootstrap.bin" -o $f = "u-boot.bin" ]; then
-            echo -n "#" >> "$TGT"
-        fi
-        echo "$URL/$f `md5sum $f | cut -d ' ' -f 1`" >> "$TGT"
-    fi
+# list of image files: [#]name
+# names may be 6-10 character length
+# prepend with '#' to disable in update-list
+#
+image1=#at91bs.bin
+image2=#u-boot.bin
+image3=kernel.bin
+image4=rootfs.bin
+
+
+# write target-build description
+if [ -n "$LAIRD_RELEASE_STRING" ]
+then
+  echo "# $LAIRD_RELEASE_STRING" > $fwul
+else
+  echo "# $(hostname)-${BR2_TARGET_UBOOT_BOARDNAME:-?}" > $fwul
+fi
+
+# write update-list
+for n in 1 2 3 4
+do
+  # construct image var
+  eval name=\$image$n \
+    && image=${name#\#}
+
+  # skip non-existant files
+  [ -f $image ] || continue
+
+  # set line prefix as hash or space
+  [ $image != $name ] && x='#' || x=' '
+
+  # write image line: [w/prefix] <md5>  <name>  <bytes>
+  md5sum $image |sed "s/^.*/$x &  $( wc -c < $image )/" >>$fwul
 done
+echo >>$fwul
+
+# apply optional flags or shell lines
+#echo "  flags -c" >>$fwul
+#
+# display file
+echo $fwul:
+cat $fwul
+
+
+#####################################
+# duplicate update-list in old-format
+# for backwards compatibility
+echo >>$fwul
+echo >>$fwul
+echo "# old format..." >>$fwul
+for n in 1 2 3 4
+do
+  # construct image var
+  eval name=\$image$n \
+    && image=${name#\#}
+
+  # skip non-existant files
+  [ -f $image ] || continue
+
+  # set line prefix as hash or space
+  [ $image != $name ] && x='#' || x=' '
+
+  # write image line: <{url/}name>  <md5>
+  md5sum $image |sed "s,^\(.*[^ ]\)[ ]\+\(.*\),$x ${url:+$url/}\2  \1," >>$fwul
+done
+echo >>$fwul
+
