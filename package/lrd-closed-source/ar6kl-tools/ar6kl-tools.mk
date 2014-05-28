205a7bdac3e221c29ab989a3edd12ba2b1d3cd1e
Steve deRosier <Steve.Derosier@lairdtech.com>
2014-05-27 22:46:38 -0700

    laird: add dbgParser into ar6kl-tools and remove the old package
    After moving dbgParser into the ar6kl-tools repository, we don't
    need the other ar6kl-tools package. Adjust the makefile to add it
    and remove the old package and disable from the config.
    
    Bug: 5122

--- package/lrd-closed-source/ar6kl-tools/ar6kl-tools.mk
+++ package/lrd-closed-source/ar6kl-tools/ar6kl-tools.mk
@@ -19,11 +19,16 @@ define AR6KL_TOOLS_BUILD_CMDS
     $(MAKE) -C $(@D)/Proprietary_tools/ath6kl-wmiconfig \
                CC="$(TARGET_CC)" AR="$(TARGET_AR)" \
                PKGCONFIG="$(HOST_DIR)/usr/bin/pkg-config"
+    $(MAKE) -C $(@D)/AR6K_PKG_ISC/host/tools/dbgParser CC="$(TARGET_CC)" ARCH=arm
 endef
 
 define AR6KL_TOOLS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/Proprietary_tools/ath6kl-tcmd/athtestcmd $(TARGET_DIR)/usr/bin/athtestcmd
 	$(INSTALL) -D -m 755 $(@D)/Proprietary_tools/ath6kl-wmiconfig/wmiconfig $(TARGET_DIR)/usr/bin/wmiconfig
+
+    $(INSTALL) -D -m 755 $(@D)/AR6K_PKG_ISC/host/tools/dbgParser/dbgParser $(TARGET_DIR)/usr/bin/dbgParser
+    $(INSTALL) -D -m 755 $(@D)/AR6K_PKG_ISC/include/dbglog.h $(TARGET_DIR)/etc/ar6kl-tools/dbgParser/include/dbglog.h
+    $(INSTALL) -D -m 755 $(@D)/AR6K_PKG_ISC/include/dbglog_id.h $(TARGET_DIR)/etc/ar6kl-tools/dbgParser/include/dbglog_id.h
 endef
 
 $(eval $(generic-package))
