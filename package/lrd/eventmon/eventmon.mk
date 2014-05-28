2fadb86a4960516098022270f1ab61c0419b253b
Dan Kephart <dan.kephart@lairdtech.com>
2014-05-27 21:09:43 -0400

    Adding SDK event monitor to the wb4x build
    BUG 5526

---
+++ package/lrd/eventmon/eventmon.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# Laird Event Monitor
+#
+#############################################################
+
+EVENTMON_VERSION = 1.1
+EVENTMON_SITE = $(DL_DIR)/
+EVENTMON_METHOD = file
+
+EVENTMON_DEPENDENCIES = sdcsdk
+EVENTMON_MAKE_ENV = CC="$(TARGET_CC)" \
+                    CXX="$(TARGET_CXX)" \
+                    ARCH="$(KERNEL_ARCH)" \
+                    CFLAGS="$(TARGET_CFLAGS)"
+
+define EVENTMON_BUILD_CMDS
+    $(MAKE) -C $(@D) clean
+	$(EVENTMON_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define EVENTMON_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/event_mon $(TARGET_DIR)/usr/bin/event_mon
+endef
+
+define EVENTMON_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/event_mon
+endef
+
+$(eval $(generic-package))
