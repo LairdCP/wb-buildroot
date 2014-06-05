4e3b86575c30725c48ad5801b8e17318480f608d
Jon Hefling <jon.hefling@lairdtech.com>
2014-06-05 11:55:38 -0400

    New udhcpd wrapper with process-id control for an interface. Bug 5700.

---
+++ board/laird/rootfs-additions-common/etc/dhcp/udhcpd.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env ash
+# /etc/dhcp/udhcpd.sh
+# A wrapper for udhcpd with process-id control.
+# jon.hefling@lairdtech.com
+#
+# Usage:
+#  ./udhcpd.sh -i<iface> [-fS] <config>
+#          ...start server for iface using config file
+#
+#  ./udhcpd.sh -i<iface> stop
+#          ...stops server for iface
+#
+#  ./udhcpd.sh -i<iface>
+#          ...dump leases for iface and show status
+#
+
+
+msg() {
+  echo "$@"
+  test "${1:0:3}" == "err" \
+    && exit 1
+}
+
+# invocation
+while let $#
+do
+  case $1 in
+    -h) ## show usage
+      sed -n '3,/^[^#]/s/^./ /p;/^$/q' $0
+      exit 0
+      ;;
+    -i*) ## interface
+      dev=${1:2}
+      ;;
+    -*) ## flag
+      [ "${1/*f*/f}" != "f" ] || b=\&
+      flags=${flags:+$flags }${1}
+      ;;
+    stop)
+      act=stop
+      ;;
+    *) ## conf
+      act=start
+      conf=$1
+      ;;
+  esac
+  shift
+done
+
+tmp=/var/lib/dhcp
+
+test -n "$dev" \
+  || msg "error: -i<iface> is required"
+
+case $act in
+  stop)
+    if { read -rst1 pid < $tmp/udhcpd.$dev.pid; } 2>/dev/null
+    then
+      if [ -d /proc/$pid ]
+      then
+        msg -n "stopping udhcpd on $dev ..."
+        echo `kill $pid`
+      fi
+      rm $tmp/udhcpd.$dev.pid
+    fi
+    ;;
+
+  start) ## run on iface in config file path
+    : ${conf:=/etc/dhcp/udhcpd.$dev.conf}
+    test "$conf" != "start" \
+      || msg "error: try -h"
+
+    # conf required
+    test -f "$conf" \
+      || msg "error: $conf n/a"
+
+    # get/set/chk pidfile
+    pf=$( sed -n '/^pidfile/s/.* //p' $conf )
+    test -f "${pf:=/var/lib/dhcp/udhcpd.$dev.pid}" \
+      && msg "error"
+
+    # leases file must exist for daemon mode
+    lf=$( sed -n '/^lease_file/s/.* //p' $conf )
+    : >>${lf:=/var/lib/dhcp/udhcpd.$dev.leases}
+
+    msg "starting udhcpd on $dev"
+    eval udhcpd $flags $conf $b
+
+    if [ -n "$b" ]
+    then
+      echo $! >$pf
+    else
+      sleep 1
+      ps ax -opid,args \
+       |sed -n "/[u]dhcpd .*$dev/s/ *\([^ ]\+\).*/\1/p" >$pf
+    fi
+    ;;
+
+  status|'') ## show if running and dumpleases
+    if [ -f $tmp/udhcpd.$dev.leases ]
+    then
+      set -x
+      dumpleases -f $tmp/udhcpd.$dev.leases
+    { set +x; echo; } 2>/dev/null
+    fi
+    msg "+ ps ax -opid,stat,args"
+    ps ax -opid,stat,args \
+     |grep "$pid.*[u]dhcpd .*conf" \
+        || { msg "  ..."; false; }
+esac
+exit $?
+
+#
+#  BusyBox v1.21.1 multi-call binary.
+#
+#  udhcpd [-fS] [CONFFILE]
+#
+#    DHCP server
+#
+#        -f      Run in foreground
+#        -S      Log to syslog too
+#
+#
+#  dumpleases [-r|-a] [-f LEASEFILE]
+#
+#    display DHCP leases granted by udhcpd
+#
+#        -f,--file=FILE  Lease file
+#        -r,--remaining  Show remaining time
+#        -a,--absolute   Show expiration time
+#
+
