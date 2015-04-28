651e309c5bb6342e0e8693858acd234053206b17
James Kalbfleisch <James.Kalbfleisch@lairdtech.com>
2015-04-28 09:02:48 -0400

    Remove smartbasic dependency.
    This repo doesn't need smartbasic for anything to build and the dependency
    changes between devel and non-devel builds.  Removing.

--- package/lrd/wb_smartBASIC_apps/wb_smartBASIC_apps.mk
+++ package/lrd/wb_smartBASIC_apps/wb_smartBASIC_apps.mk
@@ -8,8 +8,6 @@ WB_SMARTBASIC_APPS_VERSION = local
 WB_SMARTBASIC_APPS_SITE = package/lrd/externals/wb_smartBASIC_apps
 WB_SMARTBASIC_APPS_SITE_METHOD = local
 
-WB_SMARTBASIC_APPS_DEPENDENCIES = smartbasic
-
 define WB_SMARTBASIC_APPS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/laird.api.sb $(TARGET_DIR)/etc/summit/laird.api.sb
 endef
