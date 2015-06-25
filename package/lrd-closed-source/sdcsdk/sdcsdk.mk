35966c0175625f28d06451564888444916063653
Steve deRosier <Steve.Derosier@lairdtech.com>
2015-06-25 13:59:05 -0700

    laird: Add wb50n to SDK and supplicant choices

--- package/lrd-closed-source/sdcsdk/sdcsdk.mk
+++ package/lrd-closed-source/sdcsdk/sdcsdk.mk
@@ -20,12 +20,14 @@ SDCSDK_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)" PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-
 SDCSDK_TARGET_DIR = $(TARGET_DIR)
 
 SDCSDK_PLATFORM := $(call qstrip,$(BR2_LRD_PLATFORM))
-ifeq ($(SDCSDK_PLATFORM),wb45n)
+ifeq ($(SDCSDK_PLATFORM),wb50n)
+    SDCSDK_RADIO_FLAGS := CONFIG_SDC_RADIO_QCA45N=y
+else ifeq ($(SDCSDK_PLATFORM),wb45n)
     SDCSDK_RADIO_FLAGS := CONFIG_SDC_RADIO_QCA45N=y
 else ifeq ($(SDCSDK_PLATFORM),wb40n)
     SDCSDK_RADIO_FLAGS := CONFIG_SDC_RADIO_BCM40N=y
-else
-    $(error "ERROR: Expected BR2_LRD_PLATFORM to be wb45n or wb40n.")
+else ifeq ($(BR2_PACKAGE_SDCSDK),y)
+    $(error "ERROR: Expected BR2_LRD_PLATFORM to be wb50n, wb45n or wb40n.")
 endif
 
 define SDCSDK_BUILD_CMDS
