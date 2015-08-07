14452e497f4e23b659f0e56a7e554abdfadb5ebd
Youssif <youssif.am.saeed@gmail.com>
2015-08-07 14:32:35 +0100

    smartBASIC: correcting the filename to escape the $ sign

--- package/lrd/wb_smartBASIC_apps/wb_smartBASIC_apps.mk
+++ package/lrd/wb_smartBASIC_apps/wb_smartBASIC_apps.mk
@@ -9,11 +9,11 @@ WB_SMARTBASIC_APPS_SITE = package/lrd/externals/wb_smartBASIC_apps
 WB_SMARTBASIC_APPS_SITE_METHOD = local
 
 define WB_SMARTBASIC_APPS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/'$autorun$.SPPBridge.Socket.wb.sb' $(TARGET_DIR)/etc/summit/'$autorun$.SPPBridge.Socket.wb.sb'
+	$(INSTALL) -D -m 755 $(@D)/'$$autorun$$.SPPBridge.Socket.wb.sb' $(TARGET_DIR)/etc/summit/'$$autorun$$.SPPBridge.Socket.wb.sb'
 endef
 
 define WB_SMARTBASIC_APPS_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/etc/summit/'$autorun$.SPPBridge.Socket.wb.sb'
+	rm -f $(TARGET_DIR)/etc/summit/'$$autorun$$.SPPBridge.Socket.wb.sb'
 endef
 
 $(eval $(generic-package))
