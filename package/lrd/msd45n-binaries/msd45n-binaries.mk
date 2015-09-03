254514e43abfc175bd2947a6e77b2b54c568cfe2
Steve deRosier <steve.derosier@lairdtech.com>
2015-09-03 12:11:33 -0700

    laird: smartBASIC: add smartBASIC flash location to msd-binary install
    The smartBASIC /usr/sbfs directories need to exist even when smartBASIC
    is installed via the msd binaries install, so create the directory.
    
    Bug: 8086

--- package/lrd/msd45n-binaries/msd45n-binaries.mk
+++ package/lrd/msd45n-binaries/msd45n-binaries.mk
@@ -39,6 +39,7 @@ define MSD45N_BINARIES_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 644  $(@D)/var/www/docs/assets/js/*.js   $(TARGET_DIR)/var/www/docs/assets/js/
     mkdir -p $(TARGET_DIR)/etc/lighttpd
     $(INSTALL) -D -m 644  $(@D)/etc/lighttpd/lighttpd.*  $(TARGET_DIR)/etc/lighttpd/
+    mkdir -p -m 700 $(TARGET_DIR)/usr/sbfs
     $(INSTALL) -D -m 644 $(@D)/etc/summit/'$$autorun$$.SPPBridge.Socket.wb.sb' $(TARGET_DIR)/etc/summit/'$$autorun$$.SPPBridge.Socket.wb.sb'
 endef
 
