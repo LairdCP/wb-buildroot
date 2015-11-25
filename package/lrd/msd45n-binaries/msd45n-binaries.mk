1f79a96c18ebe85dfbf2e9df036f79e19fe392a0
Steve deRosier <steve.derosier@lairdtech.com>
2015-11-25 12:16:48 -0800

    laird: msd45n-binaries: Fixup to allow both 45 and 50 msd packages
    The contents of msd45 and msd50 should generally be identical, so this
    allows the msd-binaries package to unpackage and utilze both automatically
    matching to the platform being built.
    
    Bug: 8649

--- package/lrd/msd45n-binaries/msd45n-binaries.mk
+++ package/lrd/msd45n-binaries/msd45n-binaries.mk
@@ -1,7 +1,17 @@
 MSD45N_BINARIES_VERSION = $(call qstrip,$(BR2_MSD45N_BINARIES_VERSION))
 MSD45N_BINARIES_SITE = http://boris.corp.lairdtech.com/builds/linux/msd45n/laird_fips/$(MSD45N_BINARIES_VERSION)
 MSD45N_BINARIES_COMPANY_PROJECT = $(call qstrip,$(BR2_MSD45N_BINARIES_COMPANY_PROJECT))
-MSD45N_BINARIES_SOURCE = msd45n-$(MSD45N_BINARIES_COMPANY_PROJECT)-$(MSD45N_BINARIES_VERSION).tar.bz2
+
+MSD45N_BINARIES_WB_PLATFORM := $(call qstrip,$(BR2_LRD_PLATFORM))
+ifeq ($(MSD45N_BINARIES_WB_PLATFORM),wb50n)
+	MSD45N_BINARIES_PLATFORM = msd50n
+else ifeq ($(MSD45N_BINARIES_WB_PLATFORM),wb45n)
+	MSD45N_BINARIES_PLATFORM = msd45n
+else ifeq ($(BR2_PACKAGE_MSD45N_BINARIES),y)
+    $(error "ERROR: Expected BR2_LRD_PLATFORM to be wb50n, or wb45n.")
+endif
+
+MSD45N_BINARIES_SOURCE = $(MSD45N_BINARIES_PLATFORM)-$(MSD45N_BINARIES_COMPANY_PROJECT)-$(MSD45N_BINARIES_VERSION).tar.bz2
 MSD45N_BINARIES_DEPENDENCIES =
 MSD45N_BINARIES_INSTALL_STAGING = YES
 
@@ -24,7 +34,6 @@ define MSD45N_BINARIES_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/usr/bin/smartBASIC $(TARGET_DIR)/usr/bin/smartBASIC
 	mkdir -p $(TARGET_DIR)/usr/lib
     $(INSTALL) -m 755 $(@D)/usr/lib/libsdc_sdk.so* $(TARGET_DIR)/usr/lib/
-	$(INSTALL) -m 755 $(@D)/usr/lib/liblrd_btsdk.so* $(TARGET_DIR)/usr/lib/
 	$(MAKE) --no-print-directory -C $(LINUX_DIR) kernelrelease ARCH=arm CROSS_COMPILE="$(TARGET_CROSS)" > $(@D)/kernel.release
 	mkdir -p $(TARGET_DIR)/lib/modules/`cat $(@D)/kernel.release`/extra
 	$(INSTALL) -D -m 755 $(@D)/etc/init.d/S95bluetooth $(TARGET_DIR)/etc/init.d/S95bluetooth
@@ -48,12 +57,8 @@ define MSD45N_BINARIES_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/usr/lib/libsdc_sdk.so.1.0 $(STAGING_DIR)/usr/lib/
 	cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1.0 libsdc_sdk.so.1
     cd  $(STAGING_DIR)/usr/lib/ && ln -s libsdc_sdk.so.1 libsdc_sdk.so
-	cd $(STAGING_DIR)/usr/lib/ && ln -s liblrd_btsdk.so.1.0 liblrd_btsdk.so.1
-	cd $(STAGING_DIR)/usr/lib/ && ln -s liblrd_btsdk.so.1 liblrd_btsdk.so
 	$(INSTALL) -D -m 0644 $(@D)/include/sdc_sdk.h \
                           $(@D)/include/sdc_events.h \
-						  $(@D)/include/lrd_bt_sdk.h \
-						  $(@D)/include/lrd_bt_errors.h \
                           $(@D)/include/lrd_sdk_pil.h \
                           $(STAGING_DIR)/usr/include/
 endef
@@ -63,8 +68,12 @@ define MSD45N_BINARIES_INSTALL_FIPS_BINARIES
 endef
 
 ifeq ($(MSD45N_BINARIES_COMPANY_PROJECT),laird_fips)
-MSD45N_BINARIES_DEPENDENCIES += linux
-MSD45N_BINARIES_POST_INSTALL_TARGET_HOOKS += MSD45N_BINARIES_INSTALL_FIPS_BINARIES
+	MSD45N_BINARIES_DEPENDENCIES += linux
+	MSD45N_BINARIES_POST_INSTALL_TARGET_HOOKS += MSD45N_BINARIES_INSTALL_FIPS_BINARIES
+else ifeq ($(MSD45N_BINARIES_WB_PLATFORM),wb50n)
+	# wb50n gets fips, but there's no extra laird_fips name in the msd package name
+	MSD45N_BINARIES_DEPENDENCIES += linux
+	MSD45N_BINARIES_POST_INSTALL_TARGET_HOOKS += MSD45N_BINARIES_INSTALL_FIPS_BINARIES
 endif
 
 $(eval $(generic-package))
