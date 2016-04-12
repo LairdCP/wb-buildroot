49403df75e9fe5db8bf86f9bea11ff16557e8df8
Steve deRosier <steve.derosier@lairdtech.com>
2016-04-11 20:34:17 -0700

    laird: package/dcas add DCAS
    DCAS is Laird's Device Client API Server and implements the daemon required
    for the WB units to be utilized by our DCAL remote API.
    
    Bug: 9198

---
+++ package/lrd/dcas/dcas.mk
@@ -0,0 +1,46 @@
+#############################################################
+#
+# Laird DCAS
+#
+#############################################################
+DCAS_VERSION = local
+DCAS_SITE = package/lrd/externals/dcas
+DCAS_SITE_METHOD = local
+
+ifeq ($(BR2_PACKAGE_MSD_BINARIES),y)
+	DCAS_DEPENDENCIES = msd-binaries
+else ifeq ($(BR2_PACKAGE_MSD40N_BINARIES),y)
+	DCAS_DEPENDENCIES = msd40n-binaries
+else
+	DCAS_DEPENDENCIES = sdcsdk
+endif
+
+DCAS_DEPENDENCIES += host-flatcc flatcc libssh
+
+DCAS_MAKE_ENV = CC="$(TARGET_CC)" \
+                    CXX="$(TARGET_CXX)" \
+                    ARCH="$(KERNEL_ARCH)" \
+                    CFLAGS="$(TARGET_CFLAGS)" \
+                    FLATCC="$(HOST_DIR)/usr/bin/flatcc"
+
+define DCAS_BUILD_CMDS
+    $(MAKE) -C $(@D) clean
+    $(DCAS_MAKE_ENV) $(MAKE) -C $(@D) dcas
+endef
+
+define DCAS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/dcas $(TARGET_DIR)/usr/bin/dcas
+	mkdir -p $(TARGET_DIR)/etc/dcas
+	cp -v $(@D)/test/ssh_host_* $(TARGET_DIR)/etc/dcas
+	install -D -m 755 $(@D)/support/S99dcas $(TARGET_DIR)/etc/init.d/S99dcas
+	install -D -m 755 $(@D)/support/loop_dcas.sh $(TARGET_DIR)/usr/bin/loop_dcas.sh
+endef
+
+define DCAS_UNINSTALL_TARGET_CMDS
+	rm $(TARGET_DIR)/usr/bin/dcas
+	rm -rf $(TARGET_DIR)/etc/dcas
+	rm $(TARGET_DIR)/etc/init.d/S99dcas
+	rm $(TARGET_DIR)/usr/bin/loop_dcas.sh
+endef
+
+$(eval $(generic-package))
