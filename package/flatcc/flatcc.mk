de48d46162afa73b61ca109003044b769dfd2561
Steve deRosier <steve.derosier@lairdtech.com>
2016-04-26 12:42:09 -0700

    laird: flatcc: Upgrade to 0.3.3 and modify for reviewer requested changes

--- package/flatcc/flatcc.mk
+++ package/flatcc/flatcc.mk
@@ -3,33 +3,55 @@
 # FLATCC
 #
 ################################################################################
-FLATCC_VERSION = v0.3.0
+FLATCC_VERSION = v0.3.3
 FLATCC_SITE =$(call github,dvidelabs,flatcc,$(FLATCC_VERSION))
 FLATCC_LICENSE = Apache-2.0
 FLATCC_LICENSE_FILES = LICENSE
 FLATCC_INSTALL_STAGING = YES
-FLATCC_DEPENDENCIES += host-flatcc
+FLATCC_DEPENDENCIES = host-flatcc
 
-FLATCC_CONF_OPTS += -DFLATCC_TEST=OFF -DFLATCC_PORTABLE=ON
+FLATCC_CONF_OPTS += -DFLATCC_TEST=OFF
 HOST_FLATCC_CONF_OPTS += -DFLATCC_TEST=OFF
 
 define HOST_FLATCC_INSTALL_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/flatcc $(HOST_DIR)/usr/bin/flatcc
 endef
 
+# Need to force flatcc to do static or shared or both libraries
 ifeq ($(BR2_STATIC_LIBS),y)
-	FLATCC_INSTALL_TARGET = NO
-define FLATCC_INSTALL_STAGING_CMDS
+FLATCC_CONF_OPTS += -DFLATCC_WITH_SHARED=OFF -DFLATCC_WITH_STATIC=ON
+else ifeq ($(BR2_SHARED_STATIC_LIBS),y)
+FLATCC_CONF_OPTS += -DFLATCC_WITH_SHARED=ON -DFLATCC_WITH_STATIC=ON
+else ifeq ($(BR2_SHARED_LIBS),y)
+FLATCC_CONF_OPTS += -DFLATCC_WITH_SHARED=ON -DFLATCC_WITH_STATIC=OFF
+endif
+
+# flatcc's cmake build doesn't include install targets, so we need to manually
+# install the various components to their respective destinations. There's
+# several cases that need to be taken care of, so we do a little dance to do so.
+ifeq ($(BR2_SHARED_LIBS),)
+define FLATCC_INSTALL_STAGING_STATIC
 	$(INSTALL) -D -m 0755 $(@D)/lib/libflatcc.a $(STAGING_DIR)/usr/lib/libflatcc.a
 	$(INSTALL) -D -m 0755 $(@D)/lib/libflatccrt.a $(STAGING_DIR)/usr/lib/libflatccrt.a
-	cp -r $(@D)/include/flatcc $(STAGING_DIR)/usr/include/.
 endef
-else
-define FLATCC_INSTALL_STAGING_CMDS
+endif
+
+ifeq ($(BR2_STATIC_LIBS),)
+define FLATCC_INSTALL_STAGING_SHARED
 	$(INSTALL) -D -m 0755 $(@D)/lib/libflatcc.so $(STAGING_DIR)/usr/lib/libflatcc.so
 	$(INSTALL) -D -m 0755 $(@D)/lib/libflatccrt.so $(STAGING_DIR)/usr/lib/libflatccrt.so
-	cp -r $(@D)/include/flatcc $(STAGING_DIR)/usr/include/.
 endef
+endif
+
+define FLATCC_INSTALL_STAGING_CMDS
+	cp -r $(@D)/include/flatcc $(STAGING_DIR)/usr/include/.
+	$(FLATCC_INSTALL_STAGING_STATIC)
+	$(FLATCC_INSTALL_STAGING_SHARED)
+endef
+
+# If we don't have the shared libs to install, don't run target install
+ifeq ($(FLATCC_INSTALL_STAGING_SHARED),)
+FLATCC_INSTALL_TARGET = NO
 endif
 
 define FLATCC_INSTALL_TARGET_CMDS
