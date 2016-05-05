4c70fc8af718ffb2f523744599b7b2097f3f27f2
Robert Studenic <robert.studenic@lairdtech.com>
2016-05-05 13:25:54 +0000

    Added certificate management integration test.

--- package/lrd-closed-source/llagent/llagent.mk
+++ package/lrd-closed-source/llagent/llagent.mk
@@ -19,6 +19,7 @@ define LLAGENT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/scripts/init.d/S99agent $(TARGET_DIR)/etc/init.d/opt/S99agent
 	mkdir -p $(TARGET_DIR)/usr/bin/integration-tests/
 	$(INSTALL) -D -m 755 $(@D)/src/platform/laird-sdk/test_radio $(TARGET_DIR)/usr/bin/integration-tests/
+	$(INSTALL) -D -m 755 $(@D)/src/platform/wb/test_certman $(TARGET_DIR)/usr/bin/integration-tests/
 endef
 
 define LLAGENT_UNINSTALL_TARGET_CMDS
