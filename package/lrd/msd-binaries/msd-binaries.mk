e1b1360bf0816fb47469cad4463b119d26eeced8
Steve deRosier <steve.derosier@lairdtech.com>
2016-06-07 09:37:40 -0700

    laird: msd-binaries: fixup typo on llagent start script
    Simple typo caused the wbXXn builds not to build.

--- package/lrd/msd-binaries/msd-binaries.mk
+++ package/lrd/msd-binaries/msd-binaries.mk
@@ -55,7 +55,7 @@ define MSD_BINARIES_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/usr/share/smartBASIC/apps/*.sb $(TARGET_DIR)/usr/share/smartBASIC/apps/
 	$(INSTALL) -D -m 755 $(@D)/usr/share/smartBASIC/apps/lib/*.sblib $(TARGET_DIR)/usr/share/smartBASIC/apps/lib/
 	$(INSTALL) -D -m 755 $(@D)/usr/bin/llagent $(TARGET_DIR)/usr/bin/llagent
-	$(INSTALL) -D -m 755 $(@D)/scripts/init.d/opt/S99agent $(TARGET_DIR)/etc/init.d/opt/S99agent
+	$(INSTALL) -D -m 755 $(@D)/etc/init.d/opt/S99agent $(TARGET_DIR)/etc/init.d/opt/S99agent
 endef
 
 define MSD_BINARIES_INSTALL_STAGING_CMDS
