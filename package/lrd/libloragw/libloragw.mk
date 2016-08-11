0a9d40ffa2941bb19b8060ac1036e2ac079ec4c4
Ben Whitten <ben.whitten@lairdtech.com>
2016-08-11 11:28:35 +0100

    laird: Added libloragw
    This package installs libloragw into the staging directory for other
    applications to statically link too.
    It also takes the reset script and puts it in /opt/lora/
    
    Bug 9658

---
+++ package/lrd/libloragw/libloragw.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# libloragw
+#
+################################################################################
+
+LIBLORAGW_VERSION = v4.0.1
+LIBLORAGW_SITE = https://github.com/Lora-net/lora_gateway.git
+LIBLORAGW_SITE_METHOD = git
+LIBLORAGW_INSTALL_STAGING = YES
+
+define LIBLORAGW_BUILD_CMDS
+	CC="$(TARGET_CC)" $(MAKE) -C $(@D)
+endef
+
+define LIBLORAGW_INSTALL_STAGING_CMDS
+	$(INSTALL) -d $(STAGING_DIR)/usr/lib/libloragw/inc/
+	$(INSTALL) -D -m 755 $(@D)/libloragw/libloragw.a $(STAGING_DIR)/usr/lib/libloragw
+	$(INSTALL) -D -m 755 $(@D)/libloragw/library.cfg $(STAGING_DIR)/usr/lib/libloragw
+	$(INSTALL) -D -m 755 $(@D)/libloragw/inc/* $(STAGING_DIR)/usr/lib/libloragw/inc
+endef
+
+define LIBLORAGW_INSTALL_TARGET_CMDS
+	$(INSTALL) -d $(TARGET_DIR)/opt/lora/
+	$(INSTALL) -D -m 755 $(@D)/reset_lgw.sh $(TARGET_DIR)/opt/lora/reset_lgw
+endef
+
+$(eval $(generic-package))
+
