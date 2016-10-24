81d719df89db48af83b20550ecb602d6c49dc442
Joe Conley <joe.conley@lairdtech.com>
2016-10-24 13:28:58 -0400

    laird: update makefiles for WebLCM
    bug 10363

--- package/lrd-closed-source/web_lcm/web_lcm.mk
+++ package/lrd-closed-source/web_lcm/web_lcm.mk
@@ -6,25 +6,17 @@ define WEB_LCM_INSTALL_TARGET_CMDS
 	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/assets/css
 	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/assets/img
 	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/assets/js
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/html
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/php
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/docs/plugins
 	mkdir -p -m 0775 $(TARGET_DIR)/etc/lighttpd
-	$(INSTALL) -D -m 0755 $(@D)/advancedconfig.html \
-				$(@D)/about.html \
-				$(@D)/globalconfig.html \
-				$(@D)/ifaceconfig.html \
-				$(@D)/profileconfig.html \
-				$(@D)/status.html \
-				$(@D)/status_update.php \
-				$(@D)/title_update.php \
-				$(@D)/remote_update.php \
-		 $(TARGET_DIR)/var/www/docs
-	$(INSTALL) -D -m 0775 $(@D)/assets/css/bootstrap-responsive.min.css $(TARGET_DIR)/var/www/docs/assets/css
-	$(INSTALL) -D -m 0755 $(@D)/assets/css/bootstrap.min.css $(TARGET_DIR)/var/www/docs/assets/css
-	$(INSTALL) -D -m 0755 $(@D)/assets/img/logo.png $(TARGET_DIR)/var/www/docs/assets/img
-	$(INSTALL) -D -m 0755 $(@D)/assets/js/bootstrap.min.js \
-							$(@D)/assets/js/jquery.min.js \
-			$(TARGET_DIR)/var/www/docs/assets/js
-	$(INSTALL) -D -m 0755 $(@D)/lighttpd.conf \
-				$(@D)/lighttpd.password \
+	cp -r $(@D)/php/* $(TARGET_DIR)/var/www/docs/php
+	cp -r $(@D)/plugins/* $(TARGET_DIR)/var/www/docs/plugins
+	$(INSTALL) -D -m 0755 $(@D)/webLCM.* $(TARGET_DIR)/var/www/docs/
+	$(INSTALL) -D -m 0775 $(@D)/assets/css/*.css $(TARGET_DIR)/var/www/docs/assets/css
+	$(INSTALL) -D -m 0755 $(@D)/assets/img/*.png $(TARGET_DIR)/var/www/docs/assets/img
+	$(INSTALL) -D -m 0755 $(@D)/assets/js/*.js $(TARGET_DIR)/var/www/docs/assets/js
+	$(INSTALL) -D -m 0755 $(@D)/lighttpd.conf $(@D)/lighttpd.password \
 		$(TARGET_DIR)/etc/lighttpd
 endef
 
