1067b80659262ca9cee33bdc5ea27d44f871f035
Ben Whitten <ben.whitten@lairdtech.com>
2016-11-04 15:06:44 +0000

    laird: Add angular-cli to the list of host tools
    This package is required to build Angular2 web front ends and is used
    by the gatwick weblcm.
    It requires npm to be installed as a host package.
    
    Bug 10391

---
+++ package/angular-cli/angular-cli.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# host-angular-cli
+#
+################################################################################
+
+HOST_ANGULAR_CLI_VERSION = v1.0.0-beta.19
+HOST_ANGULAR_CLI_SITE = https://github.com/angular/angular-cli.git
+HOST_ANGULAR_CLI_SITE_METHOD = git
+HOST_ANGULAR_CLI_DEPENDENCIES = host-nodejs
+
+define HOST_ANGULAR_CLI_INSTALL_CMDS
+	cd $(@D); \
+		PATH=$(BR_PATH) \
+		$(HOST_NPM) install -g $(@D)
+endef
+
+# Angluar-cli uses npm to install
+$(eval $(host-generic-package))
