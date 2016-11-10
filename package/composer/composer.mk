e00b40365a83de22431aa5ae8abfe632cbbeec4b
Ben Whitten <ben.whitten@lairdtech.com>
2016-11-10 16:16:47 +0000

    laird: Add composer to the list of host tools
    This package is required to build the SLIM web backend and is used
    by the gatwick weblcm.
    It requires php to be installed on the building system, I believe by
    default on Ubuntu/Mint it is.
    
    Bug 10391

---
+++ package/composer/composer.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# host-composer
+#
+################################################################################
+
+HOST_COMPOSER_VERSION = 1.2.2
+HOST_COMPOSER_SITE = https://getcomposer.org/download/$(HOST_COMPOSER_VERSION)
+HOST_COMPOSER_SOURCE = composer.phar
+
+define HOST_COMPOSER_EXTRACT_CMDS
+	mv $(DL_DIR)/$(HOST_COMPOSER_SOURCE) $(@D)
+endef
+
+define HOST_COMPOSER_INSTALL_CMDS
+	$(INSTALL) $(@D)/composer.phar $(HOST_DIR)/usr/bin/composer
+endef
+
+
+# Composer uses its own download script to install
+$(eval $(host-generic-package))
