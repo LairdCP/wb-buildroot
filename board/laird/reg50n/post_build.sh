56d194796666084ee6b247fdf83d6dc36655461d
Joe Conley <joe.conley@lairdtech.com>
2016-11-22 14:32:54 -0500

    laird: regXX: add regulatory tools target
    Bug 8888

---
+++ board/laird/reg50n/post_build.sh
@@ -0,0 +1,21 @@
+
+export BR2_LRD_PRODUCT=reg50n
+export BR2_LRD_PLATFORM=msd50n
+
+echo "REG50n POST BUILD script: starting..."
+
+# enable tracing and exit on errors
+set -x -e
+
+# generate manifest file
+echo "/usr/bin/dbgParser" > "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
+echo "/usr/bin/athtestcmd" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
+echo "/usr/bin/wmiconfig" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
+echo "/etc/ar6kl-tools/dbgParser/include/dbglog.h" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
+echo "/etc/ar6kl-tools/dbgParser/include/dbglog_id.h" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
+
+# make sure board script is not in target directory and copy it from rootfs-additions
+rm -f $TARGET_DIR/reg_tools.sh
+cp board/laird/reg50n/rootfs-additions/reg_tools.sh $TARGET_DIR
+
+echo "REG50n POST BUILD script: done."
