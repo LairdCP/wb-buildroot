17dd95b486206572ba8c61aca9f901a47ea89ccb
Doug Smith <Doug.Smith@lairdtech.com>
2016-12-09 16:21:31 -0500

    BZ10468: supplicant restructure
    Part of supplicant restructure.
    Fix WB package makefile for new directory structure.
    
    Bug: 10468

--- package/lrd-closed-source/sdcsupp/sdcsupp.mk
+++ package/lrd-closed-source/sdcsupp/sdcsupp.mk
@@ -13,6 +13,9 @@ SDCSUPP_DEPENDENCIES = host-pkgconf dbus libnl openssl sdcsdk
 SDCSUPP_TARGET_DIR = $(TARGET_DIR)
 SDCSUPP_MAKE_ENV = PKG_CONFIG="$(HOST_DIR)"/usr/bin/pkg-config
 
+# old supplicant structure used $(@D)/wpa_supplicant/wpa_supplicant
+SDCSUPP_D = $(@D)/wpa_supplicant
+
 SDCSUPP_PLATFORM := $(call qstrip,$(BR2_LRD_PLATFORM))
 ifeq ($(SDCSUPP_PLATFORM),wb50n)
     SDCSUPP_RADIO_FLAGS := CONFIG_SDC_RADIO_QCA45N=y CONFIG_DRIVER_NL80211=y
@@ -31,36 +34,36 @@ else ifeq ($(BR2_PACKAGE_SDCSUPP),y)
 endif
 
 define SDCSUPP_BUILD_CMDS
-    cp $(@D)/wpa_supplicant/wpa_supplicant/config_openssl $(@D)/wpa_supplicant/wpa_supplicant/.config
-    $(MAKE) -C $(@D)/wpa_supplicant/wpa_supplicant clean
+    cp $(SDCSUPP_D)/config_openssl $(SDCSUPP_D)/.config
+    $(MAKE) -C $(SDCSUPP_D) clean
     CFLAGS="-I$(STAGING_DIR)/usr/include/libnl3 $(TARGET_CFLAGS) -MMD -Wall -g" \
-        $(SDCSUPP_MAKE_ENV) $(MAKE) -C $(@D)/wpa_supplicant/wpa_supplicant V=1 NEED_TLS_LIBDL=1 $(SDCSUPP_FIPS) \
+        $(SDCSUPP_MAKE_ENV) $(MAKE) -C $(SDCSUPP_D) V=1 NEED_TLS_LIBDL=1 $(SDCSUPP_FIPS) \
             $(SDCSUPP_RADIO_FLAGS) CROSS_COMPILE="$(TARGET_CROSS)"
-    $(TARGET_CROSS)objcopy -S $(@D)/wpa_supplicant/wpa_supplicant/wpa_supplicant $(@D)/wpa_supplicant/wpa_supplicant/sdcsupp
-    #(cd $(@D)/wpa_supplicant/wpa_supplicant && CROSS_COMPILE=arm-sdc-linux-gnueabi ./sdc-build-linux.sh 4 1 2 3 1)
+    $(TARGET_CROSS)objcopy -S $(SDCSUPP_D)/wpa_supplicant $(SDCSUPP_D)/sdcsupp
+    #(cd $(SDCSUPP_D) && CROSS_COMPILE=arm-sdc-linux-gnueabi ./sdc-build-linux.sh 4 1 2 3 1)
 endef
 
 define SDCSUPP_INSTALL_DBUS_NEW
 	$(INSTALL) -m 0644 -D \
-		$(@D)/wpa_supplicant/wpa_supplicant/dbus/$(SDCSUPP_DBUS_NEW_SERVICE).service \
+		$(SDCSUPP_D)/dbus/$(SDCSUPP_DBUS_NEW_SERVICE).service \
 		$(TARGET_DIR)/usr/share/dbus-1/system-services/$(SDCSUPP_DBUS_NEW_SERVICE).service
 endef
 
 ifeq ($(BR2_PACKAGE_SDCSUPP_WPA_CLI),y)
 define SDCSUPP_INSTALL_WPA_CLI
-	$(INSTALL) -D -m 755 $(@D)/wpa_supplicant/wpa_supplicant/wpa_cli $(SDCSUPP_TARGET_DIR)/usr/bin/wpa_cli
+	$(INSTALL) -D -m 755 $(SDCSUPP_D)/wpa_cli $(SDCSUPP_TARGET_DIR)/usr/bin/wpa_cli
 endef
 endif
 
 define SDCSUPP_INSTALL_DBUS
 	$(INSTALL) -m 0644 -D \
-		$(@D)/wpa_supplicant/wpa_supplicant/dbus/dbus-wpa_supplicant.conf \
+		$(SDCSUPP_D)/dbus/dbus-wpa_supplicant.conf \
 		$(TARGET_DIR)/etc/dbus-1/system.d/wpa_supplicant.conf
 	$(SDCSUPP_INSTALL_DBUS_NEW)
 endef
 
 define SDCSUPP_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/wpa_supplicant/wpa_supplicant/sdcsupp $(SDCSUPP_TARGET_DIR)/usr/bin/sdcsupp
+	$(INSTALL) -D -m 755 $(SDCSUPP_D)/sdcsupp $(SDCSUPP_TARGET_DIR)/usr/bin/sdcsupp
 	$(SDCSUPP_INSTALL_WPA_CLI)
 	$(SDCSUPP_INSTALL_DBUS)
 endef
