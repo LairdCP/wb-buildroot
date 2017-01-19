a603c9d72d2a55b9cb36cc2a1690ef439bb58717
Ben Whitten <ben.whitten@lairdtech.com>
2017-01-19 10:32:32 +0000

    laird: Include sourcemaps and aot compile in web_lcm_gatwick
    Keeping source maps for the time being as its useful for debugging
    in the browser. We turn AOT on to reduce page generation on the fly.
    
    We are now able to call the local installation of ng instead of having
    a host version to keep in sync with.
    
    Production also zips up a copy of the files we can safely remove the .gz
    
    Bug 10611

--- package/lrd-closed-source/web_lcm_gatwick/web_lcm_gatwick.mk
+++ package/lrd-closed-source/web_lcm_gatwick/web_lcm_gatwick.mk
@@ -1,7 +1,7 @@
 WEB_LCM_GATWICK_VERSION = local
 WEB_LCM_GATWICK_SITE = package/lrd-closed-source/externals/web_lcm_gatwick
 WEB_LCM_GATWICK_SITE_METHOD = local
-WEB_LCM_GATWICK_DEPENDENCIES = host-angular-cli host-composer php_sdk
+WEB_LCM_GATWICK_DEPENDENCIES = host-nodejs host-composer php_sdk
 
 define WEB_LCM_GATWICK_BUILD_CMDS
 	cd $(@D); $(HOST_DIR)/usr/bin/php $(HOST_DIR)/usr/bin/composer install --no-dev --optimize-autoloader --ignore-platform-reqs
@@ -12,13 +12,15 @@ define WEB_LCM_GATWICK_BUILD_CMDS
 
 	cd $(@D); \
 		PATH=$(BR_PATH) \
-		$(HOST_DIR)/usr/bin/ng build --prod
+		$(HOST_NPM) run ng -- build --prod --sourcemap true --aot true
 endef
 
 define WEB_LCM_GATWICK_INSTALL_TARGET_CMDS
 	mkdir -p -m 0775 $(TARGET_DIR)/var/www/http/
 	cp -rf $(@D)/api $(TARGET_DIR)/var/www/
 	cp -rf $(@D)/dist/* $(TARGET_DIR)/var/www/http/
+
+	rm $(TARGET_DIR)/var/www/http/*.gz
 
 	mv $(TARGET_DIR)/var/www/api/api.php $(TARGET_DIR)/var/www/http/
 	cp $(TARGET_DIR)/var/www/docs/lrd_php_sdk.php $(TARGET_DIR)/var/www/api/app/plugins/wifi/classes/
