2d8df84720578eb2008d33866840dab33ab4544c
Chris Feyerchak <chris.feyerchak@lairdtech.com>
2017-01-24 10:33:07 -0500

    laird: rdvk: Start BlueZ's "bluetoothd" when initializing LWB's Bluetooth
    Make "/root/lwb-bt-start.sh" script start "bluetoothd" daemon, which is
    a core component of the BlueZ stack.
    
    Bug 10639

--- board/laird/wb50n_rdvk/rootfs-additions/root/lwb-bt-start.sh
+++ board/laird/wb50n_rdvk/rootfs-additions/root/lwb-bt-start.sh
@@ -5,17 +5,23 @@ set -e
 
 readonly PATCHRAM_DEBUG_ARG="-d"
 
+NOHUP_EXE="/usr/bin/nohup"
 PATCHRAM_EXE="/usr/bin/brcm_patchram_plus"
+BLUETOOTHD_EXE="/usr/libexec/bluetooth/bluetoothd"
 BT_FW_FILE="/lib/firmware/brcm/4343w.hcd"
 BT_UART_DEV="/dev/ttyS1"
 
+test -x "${NOHUP_EXE}"
 test -x "${PATCHRAM_EXE}"
+test -x "${BLUETOOTHD_EXE}"
 test -f "${BT_FW_FILE}"
 test -c "${BT_UART_DEV}"
 
 echo 30 > /sys/class/gpio/export
 echo high > /sys/class/gpio/pioA30/direction
 echo 1 > /sys/class/gpio/pioA30/value
+
+"${NOHUP_EXE}" "${BLUETOOTHD_EXE}" &
 
 sleep 1
 
