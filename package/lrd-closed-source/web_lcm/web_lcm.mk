b138d89268cbab34ad4dfb96cbf8eaa095ae55fe
Joe Conley <joe.conley@lairdtech.com>
2017-02-03 14:18:56 -0500

    laird: WebLCM: copy new html directory to target location
    bug 10695

--- package/lrd-closed-source/web_lcm/web_lcm.mk
+++ package/lrd-closed-source/web_lcm/web_lcm.mk
@@ -12,6 +12,7 @@ define WEB_LCM_INSTALL_TARGET_CMDS
 	mkdir -p -m 0775 $(TARGET_DIR)/etc/lighttpd
 	cp -r $(@D)/php/* $(TARGET_DIR)/var/www/docs/php
 	cp -r $(@D)/plugins/* $(TARGET_DIR)/var/www/docs/plugins
+	cp -r $(@D)/html/* $(TARGET_DIR)/var/www/docs/html
 	$(INSTALL) -D -m 0755 $(@D)/webLCM.* $(TARGET_DIR)/var/www/docs/
 	$(INSTALL) -D -m 0775 $(@D)/assets/css/*.css $(TARGET_DIR)/var/www/docs/assets/css
 	$(INSTALL) -D -m 0755 $(@D)/assets/img/*.png $(TARGET_DIR)/var/www/docs/assets/img
