4b4f6b3b4dd37a0c1f58782a38f86d7872c3aaf4
Joe Conley <joe.conley@lairdtech.com>
2017-03-17 16:03:02 -0400

    laird: wb50n_rdvk: source post_build_common and remove duplicate files
    bug 10644

--- board/laird/wb50n_rdvk/post_build.sh
+++ board/laird/wb50n_rdvk/post_build.sh
@@ -4,15 +4,6 @@ TARGETDIR=$1
 export BR2_LRD_PLATFORM=wb50n
 echo "WB50n_RDVK POST BUILD script: starting..."
 
-# remove default ssh init file
-# real version is in init.d/opt and works w/ inetd or standalone
-rm -f $TARGETDIR/etc/init.d/S50sshd
-
-# remove default init's, they are replaced
-rm -f $TARGETDIR/etc/init.d/S50lighttpd
-rm -f $TARGETDIR/etc/init.d/S01logging
-rm -f $TARGETDIR/etc/init.d/S20urandom
-
 # remove bash cruft
 rm -fr $TARGETDIR/etc/bash*
 rm -f $TARGETDIR/root/.bash*
@@ -39,46 +30,13 @@ rm -f $TARGETDIR/etc/init.d/rcK
 # create something in /run or a subdirectory.
 rm -rf $TARGETDIR/run
 
+# source the common post build script
+source "board/laird/post_build_common.sh" "$TARGETDIR"
+
 # Copy the product specific rootfs additions - we don't currently use rootfs-common
 tar c --exclude=.svn --exclude=.empty -C board/laird/wb50n_rdvk/rootfs-additions/ . | tar x -C $TARGETDIR/
 
-# install libnl*.so.3 links
-( cd "$TARGETDIR/usr/lib" \
-  && ln -sf libnl-3.so libnl.so.3 \
-  && ln -sf libnl-genl-3.so libnl-genl.so.3 )
-
-# create missing symbolic link
-# TODO: shouldn't have to do this here, temporary workaround
-( cd $TARGETDIR/usr/lib \
-  && ln -sf libsdc_sdk.so.1.0 libsdc_sdk.so.1 )
-
-# Services to disable by default
-[ -f $TARGETDIR/etc/init.d/S??lighttpd ] \
-&& chmod a-x $TARGETDIR/etc/init.d/S??lighttpd
-[ -f $TARGETDIR/etc/init.d/S??openvpn ] \
-&& chmod a-x $TARGETDIR/etc/init.d/S??openvpn     #not ready for use
-[ -f $TARGETDIR/etc/init.d/S??bluetooth ] \
-&& chmod a-x $TARGETDIR/etc/init.d/S??bluetooth
-
-# background the bluetooth init-script
-[ -x $TARGETDIR/etc/init.d/S95bluetooth ] \
-&& mv $TARGETDIR/etc/init.d/S95bluetooth $TARGETDIR/etc/init.d/S95bluetooth.bg
-
-# create a compressed backup copy of the /e/n/i file
-gzip -c $TARGETDIR/etc/network/interfaces >$TARGETDIR/etc/network/interfaces~.gz
-
-# Create default firmware description file.
-# This may be overwritten by a proper release file.
-if [ -z "$LAIRD_RELEASE_STRING" ]; then
-  echo "Laird Linux development build `date +%Y%m%d`" \
-    > $TARGETDIR/etc/laird-release
-else
-  echo "$LAIRD_RELEASE_STRING" > $TARGETDIR/etc/laird-release
-fi
-
 # Fixup and add debugfs to fstab
 echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
-
-mkdir -p $TARGETDIR/etc/NetworkManager/system-connections
 
 echo "WB50n RDVK POST BUILD script: done."
