ab62f78c30cb5faf59de68a4fda806a00baede77
Joe Conley <joe.conley@lairdtech.com>
2017-03-31 13:51:13 -0400

    laird: wb50n_rdvk: fix lwb5-bt-start script path to patchram
    Add notification if file used in script cant be found.
    
    bug 10923

--- board/laird/wb50n_rdvk/rootfs-additions/root/lwb5-bt-start.sh
+++ board/laird/wb50n_rdvk/rootfs-additions/root/lwb5-bt-start.sh
@@ -6,16 +6,16 @@ set -e
 readonly PATCHRAM_DEBUG_ARG="-d"
 
 NOHUP_EXE="/usr/bin/nohup"
-PATCHRAM_EXE="/usr/bin/brcm_patchram_plus"
+PATCHRAM_EXE="/usr/bin/patchram"
 BLUETOOTHD_EXE="/usr/libexec/bluetooth/bluetoothd"
 BT_FW_FILE="/lib/firmware/brcm/4339.hcd"
 BT_UART_DEV="/dev/ttyS1"
 
-test -x "${NOHUP_EXE}"
-test -x "${PATCHRAM_EXE}"
-test -x "${BLUETOOTHD_EXE}"
-test -f "${BT_FW_FILE}"
-test -c "${BT_UART_DEV}"
+test -x "${NOHUP_EXE}" || { echo "$NOHUP_EXE n/a" ; exit 1; }
+test -x "${PATCHRAM_EXE}" || { echo "$PATCHRAM_EXE n/a" ; exit 1; }
+test -x "${BLUETOOTHD_EXE}" || { echo "$BLUETOOTHD_EXE n/a" ; exit 1; }
+test -f "${BT_FW_FILE}" || { echo "$BT_FW_FILE n/a" ; exit 1; }
+test -c "${BT_UART_DEV}" || { echo "$BT_UART_DEV n/a" ; exit 1; }
 
 echo 30 > /sys/class/gpio/export
 echo high > /sys/class/gpio/pioA30/direction
