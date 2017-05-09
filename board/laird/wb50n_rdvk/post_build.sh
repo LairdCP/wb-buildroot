f25f97009a8feb6fb1328c8edf92772c6e7b313b
Dan Kephart <dan.kephart@lairdtech.com>
2017-05-09 17:40:14 -0400

    laird: wb50n_rdvk: remove redundant actions that are in post_build_common
    When the rdvk was created, it did not source the post_build_common.sh.
    It does now, so we can remove items that are redundant with that script.
    
    Bug: 11224

--- board/laird/wb50n_rdvk/post_build.sh
+++ board/laird/wb50n_rdvk/post_build.sh
@@ -4,32 +4,6 @@ TARGETDIR=$1
 export BR2_LRD_PLATFORM=wb50n
 echo "WB50n_RDVK POST BUILD script: starting..."
 
-# remove bash cruft
-rm -fr $TARGETDIR/etc/bash*
-rm -f $TARGETDIR/root/.bash*
-rm -f $TARGETDIR/sbin/rtpr
-rm -f $TARGETDIR/usr/share/getopt/getopt-parse.bash
-
-# remove perl cruft
-rm -f $TARGETDIR/etc/ssl/misc/tsget
-rm -f $TARGETDIR/etc/ssl/misc/CA.pl
-rm -f $TARGETDIR/usr/bin/pcf2vpnc
-rm -f $TARGETDIR/usr/bin/chkdupexe
-
-# remove debian cruft
-rm -fr $TARGETDIR/etc/network/if-*
-
-# remove buildroot cruft
-rm -f $TARGETDIR/etc/os-release
-
-# remove conflicting rcK
-rm -f $TARGETDIR/etc/init.d/rcK
-
-# remove /run due to our somewhat-wonky redirection of it to /tmp via a symlink
-# avoids breaking the build, but it will also loose stuff if a package needs to
-# create something in /run or a subdirectory.
-rm -rf $TARGETDIR/run
-
 # source the common post build script
 source "board/laird/post_build_common.sh" "$TARGETDIR"
 
