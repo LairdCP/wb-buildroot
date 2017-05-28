a6bf45cd249cbc2c790d2f6dab5ceb2f992f6834
Doug Smith <Doug.Smith@lairdtech.com>
2017-05-28 16:43:30 -0400

    BZ11314: use Sterling supplicant
    Use Laird Sterling supplicantt source
    rather than the wpa_supplicant source.
    
    Bug: 11314

--- board/laird/wb50n_rdvk/wb50n_rdvk-local.mk
+++ board/laird/wb50n_rdvk/wb50n_rdvk-local.mk
@@ -1,3 +1,5 @@
 LINUX_OVERRIDE_SRCDIR = package/lrd-closed-source/externals/kernel
 AT91BOOTSTRAP3_OVERRIDE_SRCDIR = package/lrd-devel/at91bootstrap3
 UBOOT_OVERRIDE_SRCDIR = package/lrd-devel/u-boot
+# use local Laird Sterling supplicant source
+WPA_SUPPLICANT_OVERRIDE_SRCDIR = package/lrd/externals/sterling_supplicant
