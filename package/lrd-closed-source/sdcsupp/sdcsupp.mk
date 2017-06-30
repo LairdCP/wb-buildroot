9de127713ec8d0c5cf2eabe0fa11d30b59a29b4e
Joe Conley <joe.conley@lairdtech.com>
2017-06-30 14:41:31 -0400

    lrd: sdcsupp: add build switches for legacy target
    Legacy target is to be used with sdcsdk.
    Dbus will not be installed.
    
    bug 11512

--- package/lrd-closed-source/sdcsupp/sdcsupp.mk
+++ package/lrd-closed-source/sdcsupp/sdcsupp.mk
@@ -9,7 +9,7 @@ SDCSUPP_SITE = package/lrd-closed-source/externals/wpa_supplicant
 SDCSUPP_SITE_METHOD = local
 SDCSUPP_DBUS_NEW_SERVICE = fi.w1.wpa_supplicant1
 
-SDCSUPP_DEPENDENCIES = host-pkgconf dbus libnl openssl
+SDCSUPP_DEPENDENCIES = host-pkgconf libnl openssl
 SDCSUPP_TARGET_DIR = $(TARGET_DIR)
 SDCSUPP_MAKE_ENV = PKG_CONFIG="$(HOST_DIR)"/usr/bin/pkg-config
 
@@ -21,8 +21,15 @@ ifneq ($(BR2_PACKAGE_OPENSSL_FIPS),)
 SDCSUPP_FIPS = CONFIG_FIPS=y
 endif
 
+ifeq ($(BR2_PACKAGE_LRD_LEGACY_APPS),y)
+    SDCSUPP_CONFIG = $(SDCSUPP_D)/config_legacy
+else
+    SDCSUPP_DEPENDENCIES += dbus
+    SDCSUPP_CONFIG = $(SDCSUPP_D)/config_openssl
+endif
+
 define SDCSUPP_BUILD_CMDS
-    cp $(SDCSUPP_D)/config_openssl $(SDCSUPP_D)/.config
+    cp $(SDCSUPP_CONFIG) $(SDCSUPP_D)/.config
     $(MAKE) -C $(SDCSUPP_D) clean
     CFLAGS="-I$(STAGING_DIR)/usr/include/libnl3 $(TARGET_CFLAGS) -MMD -Wall -g" \
         $(SDCSUPP_MAKE_ENV) $(MAKE) -C $(SDCSUPP_D) V=1 NEED_TLS_LIBDL=1 $(SDCSUPP_FIPS) \
@@ -31,11 +38,13 @@ define SDCSUPP_BUILD_CMDS
     #(cd $(SDCSUPP_D) && CROSS_COMPILE=arm-sdc-linux-gnueabi ./sdc-build-linux.sh 4 1 2 3 1)
 endef
 
+ifneq ($(BR2_PACKAGE_LRD_LEGACY_APPS),y)
 define SDCSUPP_INSTALL_DBUS_NEW
 	$(INSTALL) -m 0644 -D \
 		$(SDCSUPP_D)/dbus/$(SDCSUPP_DBUS_NEW_SERVICE).service \
 		$(TARGET_DIR)/usr/share/dbus-1/system-services/$(SDCSUPP_DBUS_NEW_SERVICE).service
 endef
+endif
 
 ifeq ($(BR2_PACKAGE_SDCSUPP_WPA_CLI),y)
 define SDCSUPP_INSTALL_WPA_CLI
@@ -43,12 +52,14 @@ define SDCSUPP_INSTALL_WPA_CLI
 endef
 endif
 
+ifneq ($(BR2_PACKAGE_LRD_LEGACY_APPS),y)
 define SDCSUPP_INSTALL_DBUS
 	$(INSTALL) -m 0644 -D \
 		$(SDCSUPP_D)/dbus/dbus-wpa_supplicant.conf \
 		$(TARGET_DIR)/etc/dbus-1/system.d/wpa_supplicant.conf
 	$(SDCSUPP_INSTALL_DBUS_NEW)
 endef
+endif
 
 define SDCSUPP_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(SDCSUPP_D)/sdcsupp $(SDCSUPP_TARGET_DIR)/usr/bin/sdcsupp
