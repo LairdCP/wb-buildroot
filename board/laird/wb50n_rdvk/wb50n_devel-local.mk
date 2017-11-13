c715b0d6b76e63afd27b4de8995a6d65006450df
Dan Kephart <dan.kephart@lairdtech.com>
2017-11-13 18:49:48 -0500

    laird: wbXXn: fix at91bootstrap3 and u-boot override directory paths
    bug 11357
    
    # Conflicts:
    #	board/laird/wb50n_rdvk/wb50n_devel-local.mk
    #	board/laird/wb50n_rdvk/wb50n_rdvk-local.mk

--- board/laird/wb50n_rdvk/wb50n_devel-local.mk
+++ board/laird/wb50n_rdvk/wb50n_devel-local.mk
@@ -1,3 +1,3 @@
 LINUX_OVERRIDE_SRCDIR = package/lrd-closed-source/externals/kernel
-AT91BOOTSTRAP3_OVERRIDE_SRCDIR = package/lrd-devel/at91bootstrap3
-UBOOT_OVERRIDE_SRCDIR = package/lrd-devel/u-boot
+AT91BOOTSTRAP3_OVERRIDE_SRCDIR = package/lrd/externals/at91bootstrap3
+UBOOT_OVERRIDE_SRCDIR = package/lrd/externals/u-boot
