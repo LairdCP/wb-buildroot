cb1d9c538a511dcdf899e4795d08a025a0bec460
Ben Whitten <ben.whitten@lairdtech.com>
2018-03-23 09:38:58 +0000

    laird: som60: get kernel.itb to build and cleanup
    Build the zImage and dtb so that we can construct the FIT image.
    We have nothing in common with the common script so just remove it
    and simplify the script to be consistent.
    
    Bug 12734

--- board/laird/som60/post_image.sh
+++ board/laird/som60/post_image.sh
@@ -9,30 +9,47 @@ echo "SOM60 POST IMAGE script: starting..."
 # enable tracing and exit on errors
 set -x -e
 
-if cd "$IMAGESDIR"; then
-	#Create the user filesystem
-	$TARGET_DIR/../host/usr/sbin/mkfs.ubifs -d $TARGET_DIR/mnt -e 0x1f000 -c 955 -m 0x800 -x lzo  -o $TARGET_DIR/../images/user.ubifs
+BOARD_DIR="$(dirname $0)"
 
-	#Ubinize the user and squash filesystems
-	$TARGET_DIR/../host/usr/sbin/ubinize -o $TARGET_DIR/../images/user.ubi -m 0x800 -p 0x20000 $TARGET_DIR/../../../board/laird/som60/user-ubinize.cfg
+# Tooling checks
+mkimage=$HOST_DIR/bin/mkimage
+ubinize=$HOST_DIR/sbin/ubinize
+mkfs_ubifs=$HOST_DIR/usr/sbin/mkfs.ubifs
 
-	cp $BINARIES_DIR/user.ubi $BINARIES_DIR/userfs.bin
+die() { echo "$@" >&2; exit 1; }
+test -x $mkimage || \
+	die "No mkimage found (host-uboot-tools has not been built?)"
+test -x $ubinize || \
+	die "no ubinize found (host-mtd has not been built?)"
+test -x $mkfs_ubifs || \
+	die "no mkfs_ubifs found (mkfs has not been built?)"
 
-	$TARGET_DIR/../host/usr/sbin/ubinize -o $TARGET_DIR/../images/sqroot.ubi -m 0x800 -p 0x20000 $TARGET_DIR/../../../board/laird/som60/squashfs-ubinize.cfg
+# Generate kernel FIT
+# kernel.its references zImage and at91-dvk_som60.dtb, and all three
+# files must be in current directory for mkimage.
+cp $BOARD_DIR/configs/kernel.its $BINARIES_DIR/kernel.its || exit 1
+echo "# entering $BINARIES_DIR for the next command"
+(cd $BINARIES_DIR && $mkimage -f kernel.its kernel.itb) || exit 1
+rm -f $BINARIES_DIR/kernel.its
 
-	cp $BINARIES_DIR/sqroot.ubi $BINARIES_DIR/sqroot.bin
-fi
+#Create the user filesystem
+$mkfs_ubifs -d $TARGET_DIR/mnt -e 0x1f000 -c 955 -m 0x800 -x lzo  -o $BINARIES_DIR/user.ubifs
 
-# source the common post image script
-if cd "$TOPDIR"; then
-	source "board/laird/post_image_common.sh" "$IMAGESDIR"
-fi
+#Ubinize the user and squash filesystems, requires .cfg to be present
+cp $BOARD_DIR/user-ubinize.cfg $BINARIES_DIR/user-ubinize.cfg
+echo "# entering $BINARIES_DIR for the next command"
+(cd $BINARIES_DIR && $ubinize -o user.ubi -m 0x800 -p 0x20000 user-ubinize.cfg) || exit 1
+cp $BINARIES_DIR/user.ubi $BINARIES_DIR/userfs.bin
+
+echo "# entering $BINARIES_DIR for the next command"
+cp $BOARD_DIR/squashfs-ubinize.cfg $BINARIES_DIR/squashfs-ubinize.cfg
+(cd $BINARIES_DIR && $ubinize -o sqroot.ubi -m 0x800 -p 0x20000 squashfs-ubinize.cfg) || exit 1
+cp $BINARIES_DIR/sqroot.ubi $BINARIES_DIR/sqroot.bin
 
 # generate SWUpdate .swu image
-cp $TOPDIR/board/laird/$BR2_LRD_PLATFORM/configs/sw-description "$IMAGESDIR/"
+cp $BOARD_DIR/configs/sw-description "$IMAGESDIR/"
 if cd "$IMAGESDIR"; then
 	$TOPDIR/board/laird/sw_image_generator.sh "$IMAGESDIR"
 fi
-
 
 echo "SOM60 POST IMAGE script: done."
