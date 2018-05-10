a3fbb85f7c072f56db053c234db0abb05fe4223d
Service DevOps Buildserver <service.devops.buildserver@lairdtech.com>
2018-05-10 00:50:03 +0000

    laird: Adopt msd binaries version 3.5.5.84

--- package/linux-backports/linux-backports.mk
+++ package/linux-backports/linux-backports.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINUX_BACKPORTS_VERSION = 3.5.5.83
+LINUX_BACKPORTS_VERSION = 3.5.5.84
 
 LINUX_BACKPORTS_SOURCE = backports-laird-$(LINUX_BACKPORTS_VERSION).tar.bz2
 BR_NO_CHECK_HASH_FOR += $(LINUX_BACKPORTS_SOURCE)
