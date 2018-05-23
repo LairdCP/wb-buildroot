105179a5e9e3382f4205f9de06d5f5483dd7e3ce
Service DevOps Buildserver <service.devops.buildserver@lairdtech.com>
2018-05-23 04:10:11 +0000

    laird: Adopt msd binaries version 6.0.0.2

--- package/backports/backports.mk
+++ package/backports/backports.mk
@@ -7,7 +7,7 @@
 # sudo apt-get install libncurses-dev
 #    Note that libncurses-dev is required by the WB build, so you probably already have it
 
-BACKPORTS_VERSION = 3.5.6.25
+BACKPORTS_VERSION = 6.0.0.2
 BACKPORTS_LICENSE = GPL-2.0
 BACKPORTS_LICENSE_FILES = COPYING
 BACKPORTS_SITE = $(TOPDIR)/package/lrd/externals/backports
