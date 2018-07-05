10f36eba7e5af8c0cf389e6f53f9cf4f1f001245
Joe Conley <joe.conley@lairdtech.com>
2018-07-05 13:30:28 -0400

    laird: move ssh/sshd key storage into /etc/ to avoid issues when flashing user partion
    bug 13081

--- board/laird/wb45n/post_build_legacy.sh
+++ board/laird/wb45n/post_build_legacy.sh
@@ -19,6 +19,25 @@ chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
 # Remove the custom bluetooth init-script if bluez utility is not included
 [ -x $TARGETDIR/usr/sbin/hciconfig ] || rm -f /etc/init.d/opt/S??bluetooth
 
+# adjust ssh_config and sshd_config to stop using root and use /etc/ instead.
+sed -i "s/AuthorizedKeysFile.*/AuthorizedKeysFile\t\/etc\/.ssh\/authorized_keys/" $TARGETDIR/etc/ssh/sshd_config
+echo "IdentityFile /etc/.ssh/identity" >> $TARGETDIR/etc/ssh/ssh_config
+echo "IdentityFile /etc/.ssh/id_rsa" >> $TARGETDIR/etc/ssh/ssh_config
+echo "IdentityFile /etc/.ssh/id_rsa" >> $TARGETDIR/etc/ssh/ssh_config
+echo "UserKnownHostsFile /etc/.ssh/known_hosts" >> $TARGETDIR/etc/ssh/ssh_config
+
+# add SSH directorys in /etc/
+mkdir -p $TARGETDIR/etc/.ssh
+touch $TARGETDIR/etc/.ssh/authorized_keys
+
+# make sure SSH permissions are correct
+chmod 700 $TARGETDIR/etc/.ssh
+chmod 600 $TARGETDIR/etc/.ssh/authorized_keys
+
+# adjust DCAS SSH location
+sed -i "s/dcas_auth_dir.*/dcas_auth_dir=\/etc\/.ssh/" $TARGETDIR/etc/dcas.conf
+sed -i "s/DEFAULT_AUTH_DIR.*/DEFAULT_AUTH_DIR=\/etc\/.ssh/" $TARGETDIR/etc/init.d/S99dcas
+
 # Fixup and add debugfs to fstab
 echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
 
