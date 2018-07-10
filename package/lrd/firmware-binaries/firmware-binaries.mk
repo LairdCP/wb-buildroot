b744efd31502a7dc6490149bb1d1fb38929e22c1
sean meng <sean.meng@lairdtech.com>
2018-07-10 11:07:47 -0400

    laird:bdimx6: wireless driver/firmware to be  generated from the backports/firmware jobs
    1) crda package is included and the database will be installed to the target;
    2) lwb-mfg, lwb5-mfg and sterling-60 are needed on the bdimx6 platform;
    3) don't need to tar all firmwares in one file;
    
    Bug 13089, 13090

--- package/lrd/firmware-binaries/firmware-binaries.mk
+++ package/lrd/firmware-binaries/firmware-binaries.mk
@@ -1,8 +1,4 @@
 FIRMWARE_BINARIES_VERSION = 0.0.0.0
-FIRMWARE_BINARIES_SOURCE = firmware-binaries-$(FIRMWARE_BINARIES_VERSION).tar.bz2
-FIRMWARE_BINARIES_LICENSE = GPL-2.0
-FIRMWARE_BINARIES_LICENSE_FILES = COPYING
-FIRMWARE_BINARIES_SITE = http://devops.lairdtech.com/share/builds/linux/firmware
 
 STERLING-LWB-FCC=480-0079
 STERLING-LWB-ETSI=480-0080
@@ -11,97 +7,89 @@ STERLING-LWB5-FCC=480-0081
 STERLING-LWB5-ETSI=480-0082
 STERLING-LWB5-IC=480-0094
 STERLING-LWB5-JP=480-0095
-STERLING-LWB-MFG=laird-lwb-firmware-mfg
-STERLING-LWB5-MFG=laird-lwb5-firmware-mfg
+LWB-MFG=480-0108
+LWB5-MFG=480-0109
 WL-FMAC=930-0081
-STERLING-60=laird-sterling-60
+LRD-MWL=laird-sterling-60
 
-# Each firmware needs to be released individually. We don't want to have a monolithic
-# firmware tarball. However, we need a fake one to make buildroot work.
+FIRMWARE_BINARIES_SOURCE = $(LRD-MWL)-$(FIRMWARE_BINARIES_VERSION).tar.bz2
+FIRMWARE_BINARIES_EXTRA_DOWNLOADS = $(STERLING-LWB-FCC)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(STERLING-LWB-ETSI)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(STERLING-LWB5-FCC)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(STERLING-LWB5-ETSI)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(STERLING-LWB5-IC)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(STERLING-LWB5-JP)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(LWB-MFG)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(LWB5-MFG)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(STERLING-LWB-JP)-$(FIRMWARE_BINARIES_VERSION).zip \
+									$(WL-FMAC)-$(FIRMWARE_BINARIES_VERSION).zip \
+									laird-lwb-firmware-mfg-$(FIRMWARE_BINARIES_VERSION).tar.bz2 \
+									laird-lwb5-firmware-mfg-$(FIRMWARE_BINARIES_VERSION).tar.bz2
+FIRMWARE_BINARIES_LICENSE = GPL-2.0
+FIRMWARE_BINARIES_LICENSE_FILES = COPYING
+FIRMWARE_BINARIES_SITE = http://devops.lairdtech.com/share/builds/linux/firmware/$(FIRMWARE_BINARIES_VERSION)
 
-define FIRMWARE_BINARIES_PRE_DOWNLOAD_FAKE_TARBALL
-	touch $(BR2_DL_DIR)/$(FIRMWARE_BINARIES_SOURCE)
-	if [ -d $(BUILD_DIR)/firmware-binaries-$(FIRMWARE_BINARIES_VERSION) ]; \
-	then	\
-		mkdir $(BUILD_DIR)/firmware-binaries-$(FIRMWARE_BINARIES_VERSION);\
-	fi;
-endef
-FIRMWARE_BINARIES_PRE_DOWNLOAD_HOOKS += FIRMWARE_BINARIES_PRE_DOWNLOAD_FAKE_TARBALL
 
 define FIRMWARE_BINARIES_EXTRACT_CMDS
 
 endef
 
-define download-firmware-func
-	if [ ! -f $(BR2_DL_DIR)/$(1)-$(FIRMWARE_BINARIES_VERSION).$(2) ];	\
-	then	\
-		wget -P $(BR2_DL_DIR)/ $(FIRMWARE_BINARIES_SITE)/$(FIRMWARE_BINARIES_VERSION)/$(1)-$(FIRMWARE_BINARIES_VERSION).$(2);\
-	fi
+define FIRMWARE_BINARIES_CONFIGURE_CMDS
+
 endef
 
 define install-firmware-func
 	rm $(@D)/lib -fr;
-	cp $(BR2_DL_DIR)/$(1)-$(FIRMWARE_BINARIES_VERSION).$(2) $(@D)/ -fr;
-	if [ $(2) == zip ];\
-	then \
-		cd $(@D); unzip -u $(1)-$(FIRMWARE_BINARIES_VERSION).$(2);\
-	fi;
-	cd $(@D) && tar -xjf $(1)*.tar.bz2;
+	cd $(@D) && unzip -u $(BR2_DL_DIR)/$(1)-$(FIRMWARE_BINARIES_VERSION).zip  -d . ;
+	cd $(@D) && tar -xjf $(1).tar.bz2;
 	cp $(@D)/lib/firmware/* $(TARGET_DIR)/lib/firmware/ -dprf;
 endef
 
 
 define FIRMWARE_BINARIES_480_0079_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB-FCC),zip)
-	$(call install-firmware-func,$(STERLING-LWB-FCC),zip)
+	$(call install-firmware-func,$(STERLING-LWB-FCC))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0079),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0079_INSTALL_TARGET
 endif
 
 define FIRMWARE_BINARIES_480_0080_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB-ETSI),zip)
-	$(call install-firmware-func,$(STERLING-LWB-ETSI),zip)
+	$(call install-firmware-func,$(STERLING-LWB-ETSI))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0080),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0080_INSTALL_TARGET
 endif
 
 define FIRMWARE_BINARIES_480_0081_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB5-FCC),zip)
-	$(call install-firmware-func,$(STERLING-LWB5-FCC),zip)
+	$(call install-firmware-func,$(STERLING-LWB5-FCC))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0081),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0081_INSTALL_TARGET
 endif
 
 define FIRMWARE_BINARIES_480_0082_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB5-ETSI),zip)
-	$(call install-firmware-func,$(STERLING-LWB5-ETSI),zip)
+	$(call install-firmware-func,$(STERLING-LWB5-ETSI))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0082),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0082_INSTALL_TARGET
 endif
 
 define FIRMWARE_BINARIES_480_0094_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB5-IC),zip)
-	$(call install-firmware-func,$(STERLING-LWB5-IC),zip)
+	$(call install-firmware-func,$(STERLING-LWB5-IC))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0094),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0094_INSTALL_TARGET
 endif
 
 define FIRMWARE_BINARIES_480_0095_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB5-JP),zip)
-	$(call install-firmware-func,$(STERLING-LWB5-JP),zip)
+	$(call install-firmware-func,$(STERLING-LWB5-JP))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0095),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0095_INSTALL_TARGET
 endif
 
 define FIRMWARE_BINARIES_480_0116_INSTALL_TARGET
-	$(call download-firmware-func,$(STERLING-LWB-JP),zip)
-	$(call install-firmware-func,$(STERLING-LWB-JP),zip)
+	$(call install-firmware-func,$(STERLING-LWB-JP))
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0116),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0116_INSTALL_TARGET
@@ -109,8 +97,9 @@ endif
 
 define FIRMWARE_BINARIES_480_0108_INSTALL_TARGET
 	rm $(@D)/lib -fr;
-	$(call download-firmware-func,$(STERLING-LWB-MFG),tar.bz2)
-	$(call install-firmware-func,$(STERLING-LWB-MFG),tar.bz2)
+	cd $(@D) && unzip -u $(BR2_DL_DIR)/$(LWB-MFG)-$(FIRMWARE_BINARIES_VERSION).zip -d .;
+	cd $(@D) && tar -xjf laird-lwb-firmware-mfg-$(FIRMWARE_BINARIES_VERSION).tar.bz2;
+	cp $(@D)/lib/firmware/* $(TARGET_DIR)/lib/firmware/ -dprf;
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0108),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0108_INSTALL_TARGET
@@ -118,8 +107,9 @@ endif
 
 define FIRMWARE_BINARIES_480_0109_INSTALL_TARGET
 	rm $(@D)/lib -fr;
-	$(call download-firmware-func,$(STERLING-LWB5-MFG),tar.bz2)
-	$(call install-firmware-func,$(STERLING-LWB5-MFG),tar.bz2)
+	cd $(@D) && unzip -u $(BR2_DL_DIR)/$(LWB5-MFG)-$(FIRMWARE_BINARIES_VERSION).zip -d .;
+	cd $(@D) && tar -xjf laird-lwb5-firmware-mfg-$(FIRMWARE_BINARIES_VERSION).tar.bz2;
+	cp $(@D)/lib/firmware/* $(TARGET_DIR)/lib/firmware/ -dprf;
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_480_0109),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_480_0109_INSTALL_TARGET
@@ -127,9 +117,7 @@ endif
 
 define FIRMWARE_BINARIES_930_0081_INSTALL_TARGET
 	rm $(@D)/wl_fmac -fr
-	$(call download-firmware-func,$(WL-FMAC),zip)
-	cp $(BR2_DL_DIR)/$(WL-FMAC)-$(FIRMWARE_BINARIES_VERSION).zip $(@D)/ -fr
-	cd $(@D) && unzip -u $(WL-FMAC)-$(FIRMWARE_BINARIES_VERSION).zip;
+	cd $(@D) && unzip -u $(BR2_DL_DIR)/$(WL-FMAC)-$(FIRMWARE_BINARIES_VERSION).zip -d .;
 	cp $(@D)/wl_fmac $(TARGET_DIR)/usr/bin/ -f;
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_930_0081),y)
@@ -138,8 +126,8 @@ endif
 
 define FIRMWARE_BINARIES_STERLING_60_INSTALL_TARGET
 	rm $(@D)/lib/ -fr;
-	$(call download-firmware-func,$(STERLING-60),tar.bz2)
-	$(call install-firmware-func,$(STERLING-60),tar.bz2)
+	tar -xjf $(BR2_DL_DIR)/$(LRD-MWL)-$(FIRMWARE_BINARIES_VERSION).tar.bz2 -C $(@D);
+	cp $(@D)/lib/firmware/* $(TARGET_DIR)/lib/firmware/ -dprf;
 endef
 ifeq ($(BR2_FIRMWARE_BINARIES_STERLING_60),y)
 	FIRMWARE_BINARIES_POST_INSTALL_TARGET_HOOKS += FIRMWARE_BINARIES_STERLING_60_INSTALL_TARGET
