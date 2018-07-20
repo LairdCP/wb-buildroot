d4f1d14dc4b26f558189d28b654367a57a02bcac
James Kalbfleisch <James.Kalbfleisch@lairdtech.com>
2018-07-20 16:04:52 -0400

    laird:  Adding the secure update service
    This includes the make and config files related to building igupd from
    buildroot.  Also add the update service to the dbus system-local.conf
    
    Bug 13168

---
+++ package/lrd/ig/igupd/igupd.mk
@@ -0,0 +1,18 @@
+#####################################################################
+# Laird Industrial Gateway igupd
+#####################################################################
+
+IGUPD_VERSION = local
+IGUPD_SITE = package/lrd/externals/ig/igupd
+IGUPD_SITE_METHOD = local
+IGUPD_SETUP_TYPE = setuptools
+IGUPD_BUILD_OPTS = bdist_egg --exclude-source-files
+
+define IGUPD_INSTALL_TARGET_FILES
+	$(INSTALL) -D -m 755 $(@D)/dist/igupd-1.0-py2.7.egg $(TARGET_DIR)/usr/bin/igupd
+	$(INSTALL) -D -m 755 package/lrd/ig/igupd/S96igupd $(TARGET_DIR)/etc/init.d
+endef
+
+IGUPD_POST_INSTALL_TARGET_HOOKS += IGUPD_INSTALL_TARGET_FILES
+
+$(eval $(python-package))
