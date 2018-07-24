0a09a4e357dca789bb9f341635ece584d8179d50
Joe Conley <joe.conley@lairdtech.com>
2018-07-24 15:04:52 -0400

    laird: buildroot: wbXXn: post_build_legacy: limit sed modification of S99dcas to definition
    bug 13189

--- board/laird/wb45n/post_build_legacy.sh
+++ board/laird/wb45n/post_build_legacy.sh
@@ -36,7 +36,7 @@ chmod 600 $TARGETDIR/etc/.ssh/authorized_keys
 
 # adjust DCAS SSH location
 sed -i "s/dcas_auth_dir.*/dcas_auth_dir=\/etc\/.ssh/" $TARGETDIR/etc/dcas.conf
-sed -i "s/DEFAULT_AUTH_DIR.*/DEFAULT_AUTH_DIR=\/etc\/.ssh/" $TARGETDIR/etc/init.d/S99dcas
+sed -i "s/DEFAULT_AUTH_DIR=.*/DEFAULT_AUTH_DIR=\/etc\/.ssh/" $TARGETDIR/etc/init.d/S99dcas
 
 # Fixup and add debugfs to fstab
 echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
