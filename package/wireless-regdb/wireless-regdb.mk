281f81fe14db2b7a29d12995f94d1bcd2c711d05
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2018-07-27 13:00:48 -0400

    Laird: wireless-regdb Fixed CRDA database overwrite issue
    Bug 13141

--- package/wireless-regdb/wireless-regdb.mk
+++ package/wireless-regdb/wireless-regdb.mk
@@ -12,6 +12,8 @@ WIRELESS_REGDB_LICENSE_FILES = LICENSE
 
 define WIRELESS_REGDB_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -D -T $(@D)/regulatory.bin \
+		$(TARGET_DIR)/usr/lib/crda/regulatory_default.bin
+	ln -rfs $(TARGET_DIR)/usr/lib/crda/regulatory_default.bin \
 		$(TARGET_DIR)/usr/lib/crda/regulatory.bin
 	$(INSTALL) -m 644 -D -T $(@D)/sforshee.key.pub.pem \
 		$(TARGET_DIR)/etc/wireless-regdb/pubkeys/sforshee.key.pub.pem
