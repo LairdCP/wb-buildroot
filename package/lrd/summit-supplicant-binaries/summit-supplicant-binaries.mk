f1bf954388da238abbd329c3c7b6957279b622be
sean meng <sean.meng@lairdtech.com>
2018-08-31 10:19:53 -0400

    laird: Have som60 consume the summit supplicant package
    Bug 13225

---
+++ package/lrd/summit-supplicant-binaries/summit-supplicant-binaries.mk
@@ -0,0 +1,17 @@
+SUMMIT_SUPPLICANT_BINARIES_VERSION = 0.0.0.0
+SUMMIT_SUPPLICANT_BINARIES_SOURCE = summit_supplicant-arm-$(SUMMIT_SUPPLICANT_BINARIES_VERSION).tar.bz2
+SUMMIT_SUPPLICANT_BINARIES_LICENSE = GPL-2.0
+
+ifeq ($(SUMMIT_SUPPLICANT_BINARIES_SOURCE_LOCATION),laird_internal)
+  SUMMIT_SUPPLICANT_BINARIES_SITE_URL = http://devops.lairdtech.com/share/builds/linux/summit_supplicant/laird/$(SUMMIT_SUPPLICANT_BINARIES_VERSION)
+else
+  SUMMIT_SUPPLICANT_BINARIES_SITE_URL = https://github.com/LairdCP/wb-package-archive/raw/master
+endif
+
+SUMMIT_SUPPLICANT_BINARIES_SITE = $(SUMMIT_SUPPLICANT_BINARIES_SITE_URL)
+
+define SUMMIT_SUPPLICANT_BINARIES_INSTALL_TARGET_CMDS
+	tar -xf $(@D)/rootfs.tar -C $(TARGET_DIR) --overwrite
+endef
+
+$(eval $(generic-package))
