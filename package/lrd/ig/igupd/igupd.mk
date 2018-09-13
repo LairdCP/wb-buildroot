833d9ffcb7d0eb88242427708170e4b2e6482ec9
James Kalbfleisch <James.Kalbfleisch@lairdtech.com>
2018-09-13 11:52:02 -0400

    laird: Enable the ig update service
    Adding a default configuration for the swupdate. Modify the start scripts
    to start after the other services.  Also, turn off SSL for Hawkbit for
    development purposes.
    
    Bug 13454

--- package/lrd/ig/igupd/igupd.mk
+++ package/lrd/ig/igupd/igupd.mk
@@ -10,7 +10,7 @@ IGUPD_BUILD_OPTS = bdist_egg --exclude-source-files
 
 define IGUPD_INSTALL_TARGET_FILES
 	$(INSTALL) -D -m 755 $(@D)/dist/igupd-1.0-py2.7.egg $(TARGET_DIR)/usr/bin/igupd
-	$(INSTALL) -D -m 755 package/lrd/ig/igupd/S96igupd $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -D -m 755 package/lrd/ig/igupd/S98igupd $(TARGET_DIR)/etc/init.d
 endef
 
 IGUPD_POST_INSTALL_TARGET_HOOKS += IGUPD_INSTALL_TARGET_FILES
