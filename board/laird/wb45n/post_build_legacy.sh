b73ea81a63cd02bef634b90d4e3487d736931b5f
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2018-09-27 13:21:38 -0400

    Fixed duplicate debugfs mappings in fstab
    Bug 13274

--- board/laird/wb45n/post_build_legacy.sh
+++ board/laird/wb45n/post_build_legacy.sh
@@ -20,6 +20,7 @@ chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
 [ -x $TARGETDIR/usr/sbin/hciconfig ] || rm -f /etc/init.d/opt/S??bluetooth
 
 # Fixup and add debugfs to fstab
-echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
+grep -q "/sys/kernel/debug" $TARGETDIR/etc/fstab ||\
+	echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
 
 echo "WB45n POST BUILD script: done."
--- board/laird/wb50n/post_build_legacy.sh
+++ board/laird/wb50n/post_build_legacy.sh
@@ -17,6 +17,7 @@ gzip -c $TARGETDIR/etc/network/interfaces >$TARGETDIR/etc/network/interfaces~.gz
 chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
 
 # Fixup and add debugfs to fstab
-echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
+grep -q "/sys/kernel/debug" $TARGETDIR/etc/fstab ||\
+	echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
 
 echo "WB50n POST BUILD script: done."
