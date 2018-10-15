6538b29a07be9a509cbb7e258a5039c8de856e45
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2018-10-15 17:09:36 -0400

    Laird: Fixes from buildroot 2018.08 update
    Bug 13560

--- board/laird/mfg60n/post_build.sh
+++ board/laird/mfg60n/post_build.sh
@@ -4,11 +4,14 @@ export BR2_LRD_MFG_VERSION=16.205.153.252.bin
 # enable tracing and exit on errors
 set -x -e
 
+LIBEDIT=$(readlink $TARGET_DIR/usr/lib/libedit.so)
+LIBEDITLRD=${LIBEDIT/libedit./libedit.lrd.}
+
 # generate manifest file
 echo "/usr/bin/lmu"   >  "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
 echo "/usr/bin/lru"   >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
 echo "/usr/bin/btlru" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
-echo "/usr/lib/libedit.lrd.so.0.0.53"  >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
+echo "/usr/lib/$LIBEDITLRD"  >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
 echo "/lib/firmware/lrdmwl/88W8997_mfg_pcie_uart_v""$BR2_LRD_MFG_VERSION" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
 echo "/lib/firmware/lrdmwl/88W8997_mfg_pcie_usb_v""$BR2_LRD_MFG_VERSION"  >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
 echo "/lib/firmware/lrdmwl/88W8997_mfg_sdio_sdio_v""$BR2_LRD_MFG_VERSION" >> "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest"
@@ -17,4 +20,4 @@ echo "/lib/firmware/lrdmwl/88W8997_mfg_sdio_uart_v""$BR2_LRD_MFG_VERSION" >> "$T
 # make sure board script is not in target directory and copy it from rootfs-additions
 rm -f $TARGET_DIR/reg_tools.sh
 cp board/laird/mfg60n/rootfs-additions/reg_tools.sh $TARGET_DIR
-cp $TARGET_DIR/usr/lib/libedit.so.0.0.53 $TARGET_DIR/usr/lib/libedit.lrd.so.0.0.53
+cp "$TARGET_DIR/usr/lib/$LIBEDIT" "$TARGET_DIR/usr/lib/$LIBEDITLRD"
