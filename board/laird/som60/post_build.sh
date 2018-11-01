8c78b9701223c17632e6cc2e823790061247861e
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2018-11-01 12:08:22 -0400

    Laird: Added support for USB device automount
    Bug 13713

--- board/laird/som60/post_build.sh
+++ board/laird/som60/post_build.sh
@@ -1,14 +1,18 @@
 TARGETDIR="$1"
 
-export BR2_LRD_PLATFORM=som60
+export BR2_LRD_PLATFORM="$2"
 
-echo "SOM60 POST BUILD script: starting..."
+echo "${BR2_LRD_PLATFORM^^} POST BUILD script: starting..."
 
 # source the common post build script
-source "board/laird/post_build_common_60.sh" "$TARGETDIR"
+source "board/laird/post_build_common_60.sh" "$TARGETDIR" "$BR2_LRD_PLATFORM"
 
 # Copy the product specific rootfs additions, strip host user access control
-rsync -rlptDW --exclude=.empty "board/laird/som60/rootfs-additions/" "$TARGETDIR"
+rsync -rlptDWK --exclude=.empty "board/laird/som60/rootfs-additions/" "$TARGETDIR"
+
+# Remove MMC automount when booting from SD card
+[[ $BR2_LRD_PLATFORM == *"60sd"* ]] && \
+    rm -f "$TARGETDIR/etc/udev/rules.d/91-mmcmount.rules"
 
 # Make sure connection files have proper attributes
 for f in "$TARGETDIR/etc/NetworkManager/system-connections/*" ; do
@@ -21,4 +25,4 @@ for f in "$TARGETDIR/lib/firmware/lrdmwl/88W8997_sdio_uart_*" ; do
     break
 done
 
-echo "SOM60 POST BUILD script: done."
+echo "${BR2_LRD_PLATFORM^^} POST BUILD script: done."
