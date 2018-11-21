dfd6b2e71dbedcf8849c74c54e8a3cfcbf0a4770
Dan Kephart <dan.kephart@lairdtech.com>
2018-11-21 14:59:03 -0500

    laird: firmware: som60: create separate summit firmware and som60 binaries
    Create new firmware tarballs for the summit stack and the som60.  The som60
    firmware tarball is now the firmware tarball ingested by the som60 builds.
    
    Bug 13753 13816

---
+++ board/laird/firmware/post_image.sh
@@ -0,0 +1,92 @@
+IMAGESDIR="$1"
+
+export BR2_LRD_PLATFORM=laird-radio-firmware
+export FW_DIR="$TARGET_DIR/lib/firmware"
+
+echo "$BR2_LRD_PLATFORM POST IMAGE script: starting..."
+
+# enable tracing and exit on errors
+set -x -e
+
+[ -z "$LAIRD_RELEASE_STRING" ] && LAIRD_RELEASE_STRING="$(date +%Y%m%d)"
+
+cd $TARGET_DIR
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_ST_sdio_uart_*.bin $FW_DIR/lrdmwl/88W8997_sdio.bin
+ln -rsf $FW_DIR/regulatory_sterling60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-sterling60-firmware-sdio-uart.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_sdio.bin \
+	./lib/firmware/lrdmwl/88W8997_ST_sdio_uart_*.bin \
+	./lib/firmware/regulatory_sterling60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_ST_sdio_sdio_*.bin $FW_DIR/lrdmwl/88W8997_sdio.bin
+ln -rsf $FW_DIR/regulatory_sterling60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-sterling60-firmware-sdio-sdio.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_sdio.bin \
+	./lib/firmware/lrdmwl/88W8997_ST_sdio_sdio_*.bin \
+	./lib/firmware/regulatory_sterling60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_ST_pcie_uart_*.bin $FW_DIR/lrdmwl/88W8997_pcie.bin
+ln -rsf $FW_DIR/regulatory_sterling60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-sterling60-firmware-pcie-uart.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_pcie.bin \
+	./lib/firmware/lrdmwl/88W8997_ST_pcie_uart_*.bin \
+	./lib/firmware/regulatory_sterling60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_ST_pcie_usb_*.bin $FW_DIR/lrdmwl/88W8997_pcie.bin
+ln -rsf $FW_DIR/regulatory_sterling60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-sterling60-firmware-pcie-usb.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_pcie.bin \
+	./lib/firmware/lrdmwl/88W8997_ST_pcie_usb_*.bin \
+	./lib/firmware/regulatory_sterling60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_ST_usb_uart_*.bin $FW_DIR/lrdmwl/88W8997_usb.bin
+ln -rsf $FW_DIR/regulatory_sterling60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-sterling60-firmware-usb-uart.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_usb.bin \
+	./lib/firmware/lrdmwl/88W8997_ST_usb_uart_*.bin \
+	./lib/firmware/regulatory_sterling60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_SU_sdio_uart_*.bin $FW_DIR/lrdmwl/88W8997_sdio.bin
+ln -rsf $FW_DIR/regulatory_summit60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-summit60-firmware-sdio-uart.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_sdio.bin \
+	./lib/firmware/lrdmwl/88W8997_SU_sdio_uart_*.bin \
+	./lib/firmware/regulatory_summit60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_SU_sdio_sdio_*.bin $FW_DIR/lrdmwl/88W8997_sdio.bin
+ln -rsf $FW_DIR/regulatory_summit60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-summit60-firmware-sdio-sdio.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_sdio.bin \
+	./lib/firmware/lrdmwl/88W8997_SU_sdio_sdio_*.bin \
+	./lib/firmware/regulatory_summit60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_SU_pcie_uart_*.bin $FW_DIR/lrdmwl/88W8997_pcie.bin
+ln -rsf $FW_DIR/regulatory_summit60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-summit60-firmware-pcie-uart.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_pcie.bin \
+	./lib/firmware/lrdmwl/88W8997_SU_pcie_uart_*.bin \
+	./lib/firmware/regulatory_summit60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_SU_pcie_usb_*.bin $FW_DIR/lrdmwl/88W8997_pcie.bin
+ln -rsf $FW_DIR/regulatory_summit60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-summit60-firmware-pcie-usb.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_pcie.bin \
+	./lib/firmware/lrdmwl/88W8997_SU_pcie_usb_*.bin \
+	./lib/firmware/regulatory_summit60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_SU_usb_uart_*.bin $FW_DIR/lrdmwl/88W8997_usb.bin
+ln -rsf $FW_DIR/regulatory_summit60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-summit60-firmware-usb-uart.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_usb.bin \
+	./lib/firmware/lrdmwl/88W8997_SU_usb_uart_*.bin \
+	./lib/firmware/regulatory_summit60.db ./lib/firmware/regulatory.db
+
+ln -rsf $FW_DIR/lrdmwl/88W8997_SOM_sdio_uart_*.bin $FW_DIR/lrdmwl/88W8997_sdio.bin
+ln -rsf $FW_DIR/regulatory_summit60.db $FW_DIR/regulatory.db
+tar -cjf "$IMAGESDIR/laird-som60-radio-firmware.tar.bz2" \
+	./lib/firmware/lrdmwl/88W8997_sdio.bin \
+	./lib/firmware/lrdmwl/88W8997_SOM_sdio_uart_*.bin \
+	./lib/firmware/regulatory_summit60.db ./lib/firmware/regulatory.db
+
+echo "$BR2_LRD_PLATFORM POST IMAGE script: done."
