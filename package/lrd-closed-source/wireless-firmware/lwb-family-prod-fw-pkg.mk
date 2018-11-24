98b1d3e30c8e7fdc4bf44fe647db3b546359f59b
dan.kephart <dan.kephart@lairdtech.com>
2018-11-24 14:18:50 -0500

    laird: firmware: som60: remove wireless regdb from lrd-6.0.0.x branch
    Direct loading of the regulatory database is not supported by this
    branch.  This cleans up the cherry-picks from master.
    
    Bug 13753 13816

--- package/lrd-closed-source/wireless-firmware/lwb-family-prod-fw-pkg.mk
+++ package/lrd-closed-source/wireless-firmware/lwb-family-prod-fw-pkg.mk
@@ -5,12 +5,10 @@ RELEASE_STRING ?= $(shell date +%Y%m%d)
 
 LWB_MFG_NAME := laird-lwb-firmware-mfg-$(RELEASE_STRING)
 LWB5_MFG_NAME := laird-lwb5-firmware-mfg-$(RELEASE_STRING)
-60_NAME := laird-60-radio-firmware-$(RELEASE_STRING)
 WL_FMAC_930_0081_NAME := 930-0081-$(RELEASE_STRING)
 
 LWB_MFG_OUT := $(TAR_DIR)/root/$(LWB_MFG_NAME)
 LWB5_MFG_OUT := $(TAR_DIR)/root/$(LWB5_MFG_NAME)
-60_OUT := $(TAR_DIR)/root/$(60_NAME)
 
 ST_BRCM_DIR := $(BUD_DIR)/wireless-firmware-$(RELEASE_STRING)/brcm
 ST_LRDMWL_DIR := $(BUD_DIR)/wireless-firmware-$(RELEASE_STRING)/lrdmwl
@@ -26,10 +24,6 @@ $(LWB5_MFG_OUT):
 	rm $(LWB5_MFG_OUT) -fr
 	mkdir -p $(LWB5_MFG_OUT)
 
-$(60_OUT):
-	rm $(60_OUT)  -fr
-	mkdir -p $(60_OUT)
-
 $(ST_IMAGE_DIR)/$(LWB_MFG_NAME).tar.bz2: lwb-mfg-staging
 	cd $(LWB_MFG_OUT); $(TAR_CJF) $(@F) lib
 	cp $(LWB_MFG_OUT)/$(@F) $(ST_IMAGE_DIR)/
@@ -44,8 +38,6 @@ $(ST_IMAGE_DIR)/$(LWB5_MFG_NAME).tar.bz2: lwb5-mfg-staging
 $(ST_IMAGE_DIR)/480-0109-$(RELEASE_STRING).zip: $(ST_IMAGE_DIR)/$(LWB5_MFG_NAME).tar.bz2
 	cd $(ST_IMAGE_DIR) ; zip $@ $(LWB5_MFG_NAME).tar.bz2
 
-$(ST_IMAGE_DIR)/$(60_NAME).tar.bz2: 60-staging
-	cd $(60_OUT) ; $(TAR_CJF) $@ lib
 
 $(ST_IMAGE_DIR)/$(WL_FMAC_930_0081_NAME).zip:$(TMP_DIR)/$(WL_FMAC_930_0081_NAME).zip
 	cp -f $^ $@
@@ -76,18 +68,11 @@ lwb5-mfg-staging:$(LWB5_MFG_OUT)
 	ln -sf ./bcm4339/brcmfmac4339-sdio.txt . ; \
 	ln -sf ./bcm4339/4339.hcd .
 
-60-staging: $(60_OUT)
-	mkdir -p $(60_OUT)/lib/firmware/lrdmwl
-	cp -d $(ST_LRDMWL_DIR)/*.bin $(60_OUT)/lib/firmware/lrdmwl
-	cp -d $(ST_LRDMWL_DIR)/../COPYING $(60_OUT)/lib
-
 $(TMP_DIR)/$(WL_FMAC_930_0081_NAME).zip: $(T_DIR)/package/lrd-closed-source/externals/wl_fmac/bin/930-0081/wl_fmac
 	zip --junk-paths $@ $^
 
 lwb-mfg: $(ST_IMAGE_DIR)/480-0108-$(RELEASE_STRING).zip $(ST_IMAGE_DIR)/$(LWB_MFG_NAME).tar.bz2
 
 lwb5-mfg: $(ST_IMAGE_DIR)/480-0109-$(RELEASE_STRING).zip $(ST_IMAGE_DIR)/$(LWB5_MFG_NAME).tar.bz2
-
-60: $(ST_IMAGE_DIR)/$(60_NAME).tar.bz2
 
 wl: $(ST_IMAGE_DIR)/$(WL_FMAC_930_0081_NAME).zip
