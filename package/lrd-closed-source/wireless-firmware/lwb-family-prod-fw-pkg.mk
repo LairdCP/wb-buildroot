5ba4c2318406d53dda8374f70ccd0e5a9ddca34d
Dean Ramsier <dean.ramsier@lairdtech.com>
2018-12-12 20:42:44 -0500

    Revert "Fix firmware binary and supplicant packages as well as their usage"
    This reverts commit 0264a2ff42931ea9fa6cb7dcdbe44b907a1f4e58.

--- package/lrd-closed-source/wireless-firmware/lwb-family-prod-fw-pkg.mk
+++ package/lrd-closed-source/wireless-firmware/lwb-family-prod-fw-pkg.mk
@@ -1,6 +1,6 @@
 
 ST_IMAGE_DIR := $(BA_DIR)/images
-TMP_DIR := $(TAR_DIR)
+TMP_DIR := $(TAR_DIR)/tmp
 RELEASE_STRING ?= $(shell date +%Y%m%d)
 
 LWB_MFG_NAME := laird-lwb-firmware-mfg-$(RELEASE_STRING)
