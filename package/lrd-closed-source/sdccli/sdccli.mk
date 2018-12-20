a1f10eb5af8b89d23532d8d030c36e4151ec2eb3
sean meng <sean.meng@lairdtech.com>
2018-12-20 16:03:53 -0500

    laird: add new sdcsdk_nm package to buildroot
    sdcsdk is for WB platform and sdcsdk_nm is for som60
    
    Bug 12476

--- package/lrd-closed-source/sdccli/sdccli.mk
+++ package/lrd-closed-source/sdccli/sdccli.mk
@@ -8,7 +8,11 @@ SDCCLI_VERSION = local
 SDCCLI_SITE = package/lrd-closed-source/externals/sdc_cli
 SDCCLI_SITE_METHOD = local
 
-SDCCLI_DEPENDENCIES = libnl sdcsdk libedit
+ifeq ($(BR2_LRD_PLATFORM_SOM60),y)
+	SDCCLI_DEPENDENCIES = libnl sdcsdk_nm libedit
+else
+	SDCCLI_DEPENDENCIES = libnl sdcsdk libedit
+endif
 
 SDCCLI_MAKE_ENV += CC="$(TARGET_CC)" \
                   CXX="$(TARGET_CXX)" \
