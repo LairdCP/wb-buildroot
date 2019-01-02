8444453bba94285b26c6b3dfa977502a4c249143
sean meng <sean.meng@lairdtech.com>
2019-01-02 14:52:54 -0500

    laird:Add sterling firmware to the package
    Bug 13824

--- board/laird/firmware/post_image.sh
+++ board/laird/firmware/post_image.sh
@@ -10,6 +10,61 @@ set -x -e
 
 [ -z "$LAIRD_RELEASE_STRING" ] && LAIRD_RELEASE_STRING="$(date +%Y%m%d)"
 
+create_bcm4343w_firmware_zipfile()
+{
+	DOMAIN=$1
+	FIRMWARE=$2
+
+	ln -rsf $FW_DIR/regulatory_$DOMAIN.db $FW_DIR/regulatory.db;
+	ln -rsf $FW_DIR/brcm/bcm4343w/brcmfmac43430-sdio-prod.bin $FW_DIR/brcm/bcm4343w/brcmfmac43430-sdio.bin;
+	ln -rsf $FW_DIR/brcm/bcm4343w/brcmfmac43430-sdio-$DOMAIN.txt $FW_DIR/brcm/bcm4343w/brcmfmac43430-sdio.txt;
+	cd $FW_DIR/brcm/; ln -sf bcm4343w/brcmfmac43430-sdio.bin brcmfmac43430-sdio.bin; cd -;
+	ln -rsf $FW_DIR/brcm/bcm4343w/brcmfmac43430-sdio-$DOMAIN.txt $FW_DIR/brcm/brcmfmac43430-sdio.txt;
+	ln -rsf $FW_DIR/brcm/bcm4343w/brcmfmac43430-sdio.clm_blob $FW_DIR/brcm/brcmfmac43430-sdio.clm_blob;
+	ln -rsf $FW_DIR/brcm/bcm4343w/4343w.hcd $FW_DIR/brcm/4343w.hcd;
+
+	tar -cjf "$IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.tar.bz2" \
+	./lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio-prod.bin \
+	./lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio.bin \
+	./lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio.txt \
+	./lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio-$DOMAIN.txt \
+	./lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio.clm_blob \
+	./lib/firmware/brcm/bcm4343w/4343w.hcd	\
+	./lib/firmware/brcm/brcmfmac43430-sdio.bin \
+	./lib/firmware/brcm/brcmfmac43430-sdio.txt \
+	./lib/firmware/brcm/brcmfmac43430-sdio.clm_blob \
+	./lib/firmware/brcm/4343w.hcd	\
+	./lib/firmware/regulatory_$DOMAIN.db ./lib/firmware/regulatory.db
+	zip -j $IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.zip $IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.tar.bz2
+	rm $IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.tar.bz2 -fr
+}
+
+create_bcm4339_firmware_zipfile()
+{
+	DOMAIN=$1
+	FIRMWARE=$2
+
+	ln -rsf $FW_DIR/regulatory_$DOMAIN.db $FW_DIR/regulatory.db;
+	ln -rsf $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio-prod.bin $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio.bin;
+	ln -rsf $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio-$DOMAIN.txt $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio.txt;
+	cd $FW_DIR/brcm/; ln -sf bcm4339/brcmfmac4339-sdio.bin brcmfmac4339-sdio.bin; cd -;
+	ln -rsf $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio-$DOMAIN.txt $FW_DIR/brcm/brcmfmac4339-sdio.txt;
+	ln -rsf $FW_DIR/brcm/bcm4339/4339.hcd $FW_DIR/brcm/4339.hcd;
+
+	tar -cjf "$IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.tar.bz2" \
+	./lib/firmware/brcm/bcm4339/brcmfmac4339-sdio-prod.bin \
+	./lib/firmware/brcm/bcm4339/brcmfmac4339-sdio.bin \
+	./lib/firmware/brcm/bcm4339/brcmfmac4339-sdio.txt \
+	./lib/firmware/brcm/bcm4339/brcmfmac4339-sdio-$DOMAIN.txt \
+	./lib/firmware/brcm/bcm4339/4339.hcd	\
+	./lib/firmware/brcm/brcmfmac4339-sdio.bin \
+	./lib/firmware/brcm/brcmfmac4339-sdio.txt \
+	./lib/firmware/brcm/4339.hcd	\
+	./lib/firmware/regulatory_$DOMAIN.db ./lib/firmware/regulatory.db
+	zip -j $IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.zip $IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.tar.bz2
+	rm $IMAGESDIR/$FIRMWARE-$LAIRD_RELEASE_STRING.tar.bz2 -fr
+}
+
 cd $TARGET_DIR
 
 if grep -qF "BR2_PACKAGE_LAIRD_FIRMWARE_LRDMWL_ST60_SDIO_UART=y" ${BR2_CONFIG}; then
@@ -129,6 +184,10 @@ tar -cjf "$IMAGESDIR/laird-lwb-firmware-mfg-$LAIRD_RELEASE_STRING.tar.bz2" \
 	./lib/firmware/regulatory_default.db ./lib/firmware/regulatory.db
 zip -j $IMAGESDIR/480-0108-$LAIRD_RELEASE_STRING.zip $IMAGESDIR/laird-lwb-firmware-mfg-$LAIRD_RELEASE_STRING.tar.bz2
 
+create_bcm4343w_firmware_zipfile fcc  480-0079
+create_bcm4343w_firmware_zipfile etsi 480-0080
+create_bcm4343w_firmware_zipfile jp   480-0116
+
 ln -rsf $FW_DIR/regulatory_default.db $FW_DIR/regulatory.db
 ln -rsf $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio-mfg.bin $FW_DIR/brcm/brcmfmac4339-sdio.bin;
 ln -rsf $FW_DIR/brcm/bcm4339/brcmfmac4339-sdio-fcc.txt $FW_DIR/brcm/brcmfmac4339-sdio.txt;
@@ -143,6 +202,11 @@ tar -cjf "$IMAGESDIR/laird-lwb5-firmware-mfg-$LAIRD_RELEASE_STRING.tar.bz2" \
 	./lib/firmware/brcm/bcm4339/4339.hcd	\
 	./lib/firmware/regulatory_default.db ./lib/firmware/regulatory.db
 zip -j $IMAGESDIR/480-0109-$LAIRD_RELEASE_STRING.zip $IMAGESDIR/laird-lwb5-firmware-mfg-$LAIRD_RELEASE_STRING.tar.bz2
+
+create_bcm4339_firmware_zipfile fcc  480-0081
+create_bcm4339_firmware_zipfile etsi 480-0082
+create_bcm4339_firmware_zipfile ic   480-0094
+create_bcm4339_firmware_zipfile jp   480-0095
 
 ln -rsf $FW_DIR/regulatory_default.db $FW_DIR/regulatory.db
 tar -cjf "$IMAGESDIR/ath6k-6003-$LAIRD_RELEASE_STRING.tar.bz2" \
