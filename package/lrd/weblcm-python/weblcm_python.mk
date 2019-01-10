e3f77d92d54fc8526e06c5363373ec6cff9437fb
Joe Conley <joe.conley@lairdtech.com>
2019-01-10 10:59:50 -0500

    laird: add weblcm-python package to buildroot
    bug 12817

---
+++ package/lrd/weblcm-python/weblcm_python.mk
@@ -0,0 +1,40 @@
+#####################################################################
+# Laird Web Configuration Utility
+#####################################################################
+
+WEBLCM_PYTHON_VERSION = local
+WEBLCM_PYTHON_SITE = package/lrd/externals/weblcm-python
+WEBLCM_PYTHON_SITE_METHOD = local
+WEBLCM_PYTHON_SETUP_TYPE = setuptools
+WEBLCM_PYTHON_BUILD_OPTS = bdist_egg --exclude-source-files
+
+WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOKS += WEBLCM_PYTHON_INSTALL_TARGET_FILES
+
+define WEBLCM_PYTHON_INSTALL_TARGET_FILES
+	$(INSTALL) -D -m 755 $(@D)/dist/weblcm_python-1.0-py3.6.egg $(TARGET_DIR)/usr/bin/weblcm-python
+	mkdir -p -m 0775 $(TARGET_DIR)/etc/weblcm-python
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/assets/css
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/assets/img
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/assets/js
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/assets/fonts
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/html
+	mkdir -p -m 0775 $(TARGET_DIR)/var/www/plugins/wifi/html
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/*.ini $(TARGET_DIR)/etc/weblcm-python/
+	$(INSTALL) -D -m 755 $(WEBLCM_PYTHON_SITE)/html/* $(TARGET_DIR)/var/www/html/
+	$(INSTALL) -D -m 755 $(WEBLCM_PYTHON_SITE)/assets/fonts/* $(TARGET_DIR)/var/www/assets/fonts/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/assets/css/*.css $(TARGET_DIR)/var/www/assets/css/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/assets/img/*.png $(TARGET_DIR)/var/www/assets/img/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/assets/js/*.js $(TARGET_DIR)/var/www/assets/js/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/plugins/wifi/html/*.html $(TARGET_DIR)/var/www/plugins/wifi/html/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/plugins/wifi/wifi* $(TARGET_DIR)/var/www/plugins/wifi/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/*.html $(TARGET_DIR)/var/www/
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/LICENSE $(TARGET_DIR)/var/www/
+
+	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/weblcm-python.service $(TARGET_DIR)/etc/systemd/system
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -sf $(TARGET_DIR)/etc/systemd/system/weblcm-python.service $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+
+endef
+
+$(eval $(python-package))
