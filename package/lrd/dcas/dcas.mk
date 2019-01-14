955e6fd13aab88241fca0475a24db08e94b7f054
sean meng <sean.meng@lairdtech.com>
2019-01-14 10:59:38 -0500

    Revert "laird: Port dcal/dcas to use NetworkManagerlaird"
    This reverts commit aacd01aa04672e60bd518a42042d0dc3e30d18c2.

--- package/lrd/dcas/dcas.mk
+++ package/lrd/dcas/dcas.mk
@@ -11,53 +11,33 @@ ifeq ($(BR2_PACKAGE_MSD_BINARIES),y)
 	DCAS_DEPENDENCIES = msd-binaries
 else ifeq ($(BR2_PACKAGE_MSD40N_BINARIES),y)
 	DCAS_DEPENDENCIES = msd40n-binaries
+else
+	DCAS_DEPENDENCIES = sdcsdk
 endif
 
 DCAS_DEPENDENCIES += host-flatcc flatcc libssh
-
-ifeq ($(BR2_LRD_PLATFORM_SOM60),y)
-	DCAS_DEPENDENCIES = sdcsdk_nm
-	SDCSDK_PLATFORM := CONFIG_SDC_PLATFORM=som60
-else
-	DCAS_DEPENDENCIES = sdcsdk
-	SDCSDK_PLATFORM := CONFIG_SDC_PLATFORM=wb50n
-endif
 
 DCAS_MAKE_ENV = CC="$(TARGET_CC)" \
                     CXX="$(TARGET_CXX)" \
                     ARCH="$(KERNEL_ARCH)" \
                     CFLAGS="$(TARGET_CFLAGS)" \
-                    FLATCC="$(HOST_DIR)/usr/bin/flatcc" \
-					PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config"
+                    FLATCC="$(HOST_DIR)/usr/bin/flatcc"
 
 define DCAS_BUILD_CMDS
     $(MAKE) -C $(@D) clean
-    $(DCAS_MAKE_ENV) $(MAKE) -C $(@D) dcas $(SDCSDK_PLATFORM)
+    $(DCAS_MAKE_ENV) $(MAKE) -C $(@D) dcas
 endef
 
 define DCAS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/dcas $(TARGET_DIR)/usr/bin/dcas
-	mkdir -p $(TARGET_DIR)/etc/dcas
-	$(INSTALL) -D -m 644 package/lrd/dcas/dcas.conf $(TARGET_DIR)/etc/dcas/dcas.conf
+	$(INSTALL) -D -m 755 $(@D)/support/etc/init.d/S99dcas $(TARGET_DIR)/etc/init.d/S99dcas
+	$(INSTALL) -D -m 755 $(@D)/support/etc/dcas.conf $(TARGET_DIR)/etc/dcas.conf
 endef
 
 define DCAS_UNINSTALL_TARGET_CMDS
 	rm $(TARGET_DIR)/usr/bin/dcas
-	rm $(TARGET_DIR)/etc/dcas -fr
-endef
-
-define DCAS_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 package/lrd/dcas/dcas.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/dcas.service
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-	ln -fs ../../../../usr/lib/systemd/system/dcas.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/dcas.service
-	$(INSTALL) -D -m 755 package/lrd/dcas/S99dcas $(TARGET_DIR)/etc/dcas/S99dcas
-endef
-
-define DCAS_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 755 package/lrd/dcas/S99dcas \
-		$(TARGET_DIR)/etc/init.d/S99dcas
+	rm $(TARGET_DIR)/etc/dcas.conf
+	rm $(TARGET_DIR)/etc/init.d/S99dcas
 endef
 
 $(eval $(generic-package))
