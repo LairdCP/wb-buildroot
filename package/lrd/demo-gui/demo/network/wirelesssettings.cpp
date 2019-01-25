f546a00406d6bec63c39a24a95ef02c8e5587c0d
sean meng <sean.meng@lairdtech.com>
2019-01-25 16:20:32 -0500

    laird: update gui demo to Qt5.11
    Fails to build on som60.
    
    Bug 14010

--- package/lrd/demo-gui/demo/network/wirelesssettings.cpp
+++ package/lrd/demo-gui/demo/network/wirelesssettings.cpp
@@ -8,33 +8,26 @@
 
 #include "wirelesssettings.h"
 
-//#define Grey QColor(200,200,200)
-//#define Blue QColor(0,255,0)
-//#define SetRowBackgroundColor(row, color) \
-//    tableWidget_aplist->item(row, 0)->setBackgroundColor(color); \
-//    tableWidget_aplist->item(row, 1)->setBackgroundColor(color);
-    
-
 QString WirelessSettings::ap_security_flag_to_string(int flags, int wpaFlags)
 {
     QStringList s;
-    
+
     if (wpaFlags == NM_802_11_AP_SEC_NONE)
     {
-        if(flags > 0) 
+        if(flags > 0)
             return "WEP";
         return "OPEN";
     }
-    
+
     if(wpaFlags & (NM_802_11_AP_SEC_PAIR_CCMP | NM_802_11_AP_SEC_GROUP_CCMP))
         s.append("WPA2");
-       
+
     if(wpaFlags & (NM_802_11_AP_SEC_PAIR_TKIP | NM_802_11_AP_SEC_GROUP_TKIP))
         s.append("WPA1");
 
     if (wpaFlags & NM_802_11_AP_SEC_KEY_MGMT_802_1X)
         s.append("802.1X");
-    
+
     return s.join(" ");
 }
 
@@ -43,7 +36,7 @@ void WirelessSettings::do_refresh_connections()
     QString devName = comboBox_device->currentText();
     QStringList connList = nmAdaptor->get_connections_by_device(devName);
     QString ipaddr, gateway, netmask;
-    
+
     nmAdaptor->get_ipv4_info_by_device(devName, ipaddr, gateway, netmask);
     lineEdit_ipv4->setText(ipaddr);
     lineEdit_netmask->setText(netmask);
@@ -55,12 +48,12 @@ WirelessSettings::WirelessSettings(QWidget *parent) :
 {
 
     setupUi(this);
-        
+
     nmAdaptor = new LrdNmAdaptor();
     QStringList devList = nmAdaptor->get_devices_by_type(NM_DEVICE_TYPE_WIFI);
     comboBox_device->clear();
     comboBox_device->addItems(devList);
-    
+
     on_pushButton_ui_refresh_clicked();
 
 }
@@ -73,7 +66,7 @@ void WirelessSettings::on_pushButton_ui_refresh_clicked()
 void WirelessSettings::do_wifi_connect(QString passwd)
 {
     QString ssid = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 0)->text();
-    QString security = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 1)->text(); 
+    QString security = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 1)->text();
     QString res = nmAdaptor->add_connection_wpa(comboBox_device->currentText(), ssid, passwd);
     if(res.isEmpty())
         return;
@@ -83,7 +76,7 @@ void WirelessSettings::do_wifi_connect(QString passwd)
 void WirelessSettings::do_wifi_connect_wep(QString key, QString passwd)
 {
    QString ssid = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 0)->text();
-   QString security = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 1)->text(); 
+   QString security = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 1)->text();
    QString res = nmAdaptor->add_connection_wep(comboBox_device->currentText(), ssid, key.toInt(), passwd);
    if(res.isEmpty())
        return;
@@ -94,43 +87,38 @@ void WirelessSettings::on_pushButton_ap_update_clicked()
 {
     int row;
     QVector<int>flags;
-    
+
     nmAdaptor->do_scan_accesspoint_list(comboBox_device->currentText());
     QStringList aps = nmAdaptor->get_accesspoint_list(comboBox_device->currentText());
 
     this->setEnabled(false);
-    
     foreach(QString ap, aps)
     {
         if(!tableWidget_aplist->findItems(ap, Qt::MatchCaseSensitive).isEmpty())
         {
             continue;
         }
-        
         row = tableWidget_aplist->rowCount();
         tableWidget_aplist->insertRow(row);
         tableWidget_aplist->setItem(row, 0, new QTableWidgetItem(ap));
-        
         flags = nmAdaptor->get_accesspoint_security_type(ap);
         tableWidget_aplist->setItem(row, 1, new QTableWidgetItem(ap_security_flag_to_string(flags[0], flags[1])));
     }
-    
+
     this->setEnabled(true);
-    
     if(tableWidget_aplist->rowCount() < 1) return;
-    
-    tableWidget_aplist->sortByColumn(0, Qt::AscendingOrder);    
+    tableWidget_aplist->sortByColumn(0, Qt::AscendingOrder);
 }
 
 void WirelessSettings::add_ap()
 {
     if(tableWidget_aplist->currentRow() < 0) return;
     QString ssid = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 0)->text();
-    QString security = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 1)->text(); 
-    
+    QString security = tableWidget_aplist->item(tableWidget_aplist->currentRow(), 1)->text();
+
     if(nmAdaptor->is_added(ssid))
         return;
-    
+
     if(!security.compare("WEP", Qt::CaseInsensitive))
     {
         WEPInputDlg* wepDlg = new WEPInputDlg();
--- package/lrd/demo-gui/demo/network/wpaenterpriseinputdlg.h
+++ package/lrd/demo-gui/demo/network/wpaenterpriseinputdlg.h
@@ -2,6 +2,7 @@
 #define WPAENTERPRISEINPUTDLG_H
 
 #include <QWidget>
+#include <QLineEdit>
 
 #include "keyboard/keyboard.h"
 
@@ -12,13 +13,13 @@ class WPAEnterpriseIninputDlg;
 class WPAEnterpriseIninputDlg : public QWidget
 {
     Q_OBJECT
-    
+
 public:
     explicit WPAEnterpriseIninputDlg(QWidget *parent = 0);
     ~WPAEnterpriseIninputDlg();
-    
+
 signals:
-    void sendPasswd(QString security, QString auth, QString identity, QString ca, bool noCA, 
+    void sendPasswd(QString security, QString auth, QString identity, QString ca, bool noCA,
                     QString innerAuth, QString username, QString password);
 
 private slots:
@@ -27,28 +28,19 @@ private slots:
     bool eventFilter(QObject *,QEvent *);
     void on_pushButton_ok_clicked();
     void on_pushButton_cancel_clicked();
-
     void on_comboBox_auth_currentIndexChanged(int index);
-
     void on_pushButton_tunneled_open_cacert_clicked();
-    
     void on_pushButton_peap_open_cacert_clicked();
-    
     void on_pushButton_tls_open_usercert_clicked();
-    
     void on_pushButton_tls_open_cacert_clicked();
-    
     void on_pushButton_tls_open_prikey_clicked();
-        
     void on_pushButton_fast_open_pacfile_clicked();
-    
 private:
     QLineEdit* getFocusedLineEdit();
-    
 private:
     Ui::WPAEnterpriseIninputDlg *ui;
     Keyboard* kb;
-    QLineEdit *focusItem; 
+    QLineEdit *focusItem;
 };
 
 #endif // WPAENTERPRISEINPUTDLG_H
