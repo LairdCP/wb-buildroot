4f76e58c584b7834dd2fb3cd6190807feaf9bb19
sean meng <sean.meng@lairdtech.com>
2019-01-31 14:54:03 -0500

    Buildroot: add package firewalld to buildroot
    Firewalld switched to ntf as its backend since 0.6. There seems quite
    a few problems with this change, e.g.
    "/usr/sbin/nft insert rule inet firewalld raw_PREROUTING meta nfproto ipv6 fib saddr . iif oif missing drop"
    will fail etc. So IPv6_rpfilter is disabled by default in the firewalld.conf.
    
    Bug 13952

---
+++ package/firewalld/firewalld.mk
@@ -0,0 +1,46 @@
+###############################################################################
+#
+# firewalld .mk file
+#
+################################################################################
+
+FIREWALLD_VERSION = v0.6.3
+FIREWALLD_SOURCE = $(FIREWALLD_VERSION).tar.gz
+FIREWALLD_SITE = https://github.com/firewalld/firewalld/archive
+FIREWALLD_DEPENDENCIES = libglib2 iptables ebtables nftables host-intltool gettext \
+			 desktop-file-utils systemd python-decorator dbus-python python-slip-dbus
+FIREWALLD_INSTALL_STAGING = YES
+FIREWALLD_LIBTOOL_PATCH = YES
+FIREWALLD_AUTORECONF= YES
+
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+FIREWALLD_DEPENDENCIES  += python-gobject
+else
+FIREWALLD_DEPENDENCIES  += python3-gobject
+endif
+
+define FIREWALLD_RUN_INTLTOOLIZE
+	echo $(PATH)
+	cd $(@D) && $(HOST_DIR)/usr/bin/intltoolize --force --automake
+endef
+
+FIREWALLD_POST_EXTRACT_HOOKS = FIREWALLD_RUN_INTLTOOLIZE
+
+define FIREWALLD_FIX_SHEBANG
+	cd $(BUILD_DIR)/firewalld-v0.6.3/src && $(SED) "1s/.*/\#\\!\\/\bin\/\python/" firewalld \
+	&& $(SED) "1s/.*/\#\\!\\/\bin\/\python/" firewall-cmd \
+	&& $(SED) "1s/.*/\#\\!\\/\bin\/\python/" firewall-applet \
+	&& $(SED) "1s/.*/\#\\!\\/\bin\/\python/" firewall-config \
+	&& $(SED) "1s/.*/\#\\!\\/\bin\/\python/" firewall-offline-cmd
+endef
+
+FIREWALLD_PRE_INSTALL_TARGET_HOOKS = FIREWALLD_FIX_SHEBANG
+
+define FIREWALLD_FIX_CONFIG
+	$(SED) "s/ &&.*//g" $(TARGET_DIR)/etc/modprobe.d/firewalld-sysctls.conf
+	$(SED) "s/IPv6_rpfilter=yes/IPv6_rpfilter=no/g" $(TARGET_DIR)/etc/firewalld/firewalld.conf
+endef
+
+FIREWALLD_POST_INSTALL_TARGET_HOOKS = FIREWALLD_FIX_CONFIG
+
+$(eval $(autotools-package))
