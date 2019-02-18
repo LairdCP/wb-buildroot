c2afde883ff847b290c46841a297a76007156af8
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2019-02-18 12:47:36 -0500

    Fixed NAND build for som60 and wb50n_sysd, removed unneeded files
    Bug 14017

--- board/laird/post_image_common_60.sh
+++ board/laird/post_image_common_60.sh
@@ -1,96 +1,99 @@
-BOARD_DIR="$1"
+BOARD_DIR="${1}"
+
+GENIMAGE_CFG="${BOARD_DIR}/configs/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 # Copy mksdcard.sh and mksdimg.sh to images
-cp $BOARD_DIR/../scripts-common/mksdcard.sh $BINARIES_DIR/
-cp $BOARD_DIR/../scripts-common/mksdimg.sh $BINARIES_DIR/
-cp $BOARD_DIR/configs/uboot-env.txt $BINARIES_DIR/
+cp ${BOARD_DIR}/../scripts-common/mksdcard.sh ${BINARIES_DIR}/
+cp ${BOARD_DIR}/../scripts-common/mksdimg.sh ${BINARIES_DIR}/
+cp ${BOARD_DIR}/configs/uboot-env.txt ${BINARIES_DIR}/
 
 # Tooling checks
-mkimage=$HOST_DIR/bin/mkimage
-atmel_pmecc_params=$BUILD_DIR/uboot-custom/tools/atmel_pmecc_params
-openssl=$HOST_DIR/usr/bin/openssl
-genimage=$HOST_DIR/bin/genimage
-veritysetup=$HOST_DIR/sbin/veritysetup
-mkenvimage=$HOST_DIR/bin/mkenvimage
+mkimage=${HOST_DIR}/bin/mkimage
+atmel_pmecc_params=${BUILD_DIR}/uboot-custom/tools/atmel_pmecc_params
+openssl=${HOST_DIR}/usr/bin/openssl
+genimage=${HOST_DIR}/bin/genimage
+veritysetup=${HOST_DIR}/sbin/veritysetup
+mkenvimage=${HOST_DIR}/bin/mkenvimage
 
 die() { echo "$@" >&2; exit 1; }
-test -x $mkimage || \
+test -x ${mkimage} || \
 	die "No mkimage found (host-uboot-tools has not been built?)"
-test -x $atmel_pmecc_params || \
+test -x ${atmel_pmecc_params} || \
 	die "no atmel_pmecc_params found (uboot has not been built?)"
-test -x $openssl || \
+test -x ${openssl} || \
 	die "no openssl found (host-openssl has not been built?)"
-test -x $veritysetup || \
+test -x ${veritysetup} || \
 	die "No veritysetup found (host-cryptsetup has not been built?)"
-test -x $genimage || \
+test -x ${genimage} || \
 	die "No genimage found (host-genimage has not been built?)"
-test -x $mkenvimage || \
+test -x ${mkenvimage} || \
 	die "No mkenvimage found (host-uboot-tools has not been built?)"
 
-$mkenvimage -s 0x20000 -o $BINARIES_DIR/uboot-env.bin $BINARIES_DIR/uboot-env.txt -r
+$mkenvimage -s 0x20000 -o ${BINARIES_DIR}/uboot-env.bin ${BINARIES_DIR}/uboot-env.txt -r
 
 # Generate dev keys if needed
-if [ ! -f $BINARIES_DIR/keys/dev.key ]; then
-	mkdir -p $BINARIES_DIR/keys
-	$openssl genrsa -out $BINARIES_DIR/keys/dev.key 2048
-	$openssl req -batch -new -x509 -key $BINARIES_DIR/keys/dev.key -out $BINARIES_DIR/keys/dev.crt
+if [ ! -f ${BINARIES_DIR}/keys/dev.key ]; then
+	mkdir -p ${BINARIES_DIR}/keys
+	${openssl} genrsa -out ${BINARIES_DIR}/keys/dev.key 2048
+	${openssl} req -batch -new -x509 -key ${BINARIES_DIR}/keys/dev.key -out ${BINARIES_DIR}/keys/dev.crt
 fi
 
 # Copy the boot.scr for uboot
-cp $BOARD_DIR/configs/boot.scr $BINARIES_DIR/boot.scr
+cp ${BOARD_DIR}/configs/boot.scr ${BINARIES_DIR}/boot.scr
 
-if [[ $BR2_LRD_PLATFORM != *"sd" ]] && [[ $BR2_LRD_PLATFORM != *"sd_"* ]] ; then
+if [[ ${BR2_LRD_PLATFORM} != *"sd" ]] && [[ ${BR2_LRD_PLATFORM} != *"sd_"* ]] ; then
 	# Generate the hash table for squashfs
-	rm -f $BINARIES_DIR/rootfs.verity
-	$veritysetup format $BINARIES_DIR/rootfs.squashfs $BINARIES_DIR/rootfs.verity > $BINARIES_DIR/rootfs.verity.header
+	rm -f ${BINARIES_DIR}/rootfs.verity
+	${veritysetup} format ${BINARIES_DIR}/rootfs.squashfs ${BINARIES_DIR}/rootfs.verity > ${BINARIES_DIR}/rootfs.verity.header
 	# Get the hash
-	HASH="$(awk '/Root hash:/ {print $3}' $BINARIES_DIR/rootfs.verity.header)"
-	SALT="$(awk '/Salt:/ {print $2}' $BINARIES_DIR/rootfs.verity.header)"
-	BLOCKS="$(awk '/Data blocks:/ {print $3}' $BINARIES_DIR/rootfs.verity.header)"
+	HASH="$(awk '/Root hash:/ {print $3}' ${BINARIES_DIR}/rootfs.verity.header)"
+	SALT="$(awk '/Salt:/ {print $2}' ${BINARIES_DIR}/rootfs.verity.header)"
+	BLOCKS="$(awk '/Data blocks:/ {print $3}' ${BINARIES_DIR}/rootfs.verity.header)"
 	SIZE=$((${BLOCKS} * 8))
 	OFFSET=$((${BLOCKS} + 1))
 
 	# Generate a combined rootfs
-	cat $BINARIES_DIR/rootfs.squashfs $BINARIES_DIR/rootfs.verity > $BINARIES_DIR/rootfs.bin
+	cat ${BINARIES_DIR}/rootfs.squashfs ${BINARIES_DIR}/rootfs.verity > ${BINARIES_DIR}/rootfs.bin
 
 	# Generate the boot.scr for uboot
-	sed -i -e "s/SALT/${SALT}/g" -e "s/HASH/${HASH}/g" -e "s/BLOCKS/${BLOCKS}/g" -e "s/SIZE/${SIZE}/g" -e "s/OFFSET/${OFFSET}/g" $BINARIES_DIR/boot.scr
+	sed -i -e "s/SALT/${SALT}/g" -e "s/HASH/${HASH}/g" -e "s/BLOCKS/${BLOCKS}/g" -e "s/SIZE/${SIZE}/g" -e "s/OFFSET/${OFFSET}/g" ${BINARIES_DIR}/boot.scr
 fi
 
 # Generate kernel FIT
 # kernel.its references zImage and at91-dvk_som60.dtb, and all three
 # files must be in current directory for mkimage.
 DTB="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\(.*\)"$/\1/p' ${BR2_CONFIG})"
-sed "s/at91-dvk_som60/$DTB/g" $BOARD_DIR/configs/kernel.its > $BINARIES_DIR/kernel.its || exit 1
+sed "s/at91-dvk_som60/${DTB}/g" ${BOARD_DIR}/configs/kernel.its > ${BINARIES_DIR}/kernel.its || exit 1
 
-echo "# entering $BINARIES_DIR for the next command"
-(cd $BINARIES_DIR && $mkimage -f kernel.its kernel.itb) || exit 1
-(cd $BINARIES_DIR && $mkimage -F -K u-boot.dtb -k keys -r kernel.itb) || exit 1
-rm -f $BINARIES_DIR/kernel.its
+echo "# entering ${BINARIES_DIR} for the next command"
+(cd ${BINARIES_DIR} && $mkimage -f kernel.its kernel.itb) || exit 1
+(cd ${BINARIES_DIR} && $mkimage -F -K u-boot.dtb -k keys -r kernel.itb) || exit 1
+rm -f ${BINARIES_DIR}/kernel.its
 
 # Re-generate u-boot FIT with Keys
-rm -f $BINARIES_DIR/u-boot.itb
-cp $BOARD_DIR/configs/u-boot.its $BINARIES_DIR/u-boot.its || exit 1
-cp "$BINARIES_DIR/u-boot-spl.dtb" "$BINARIES_DIR/u-boot-spl-key.dtb"
+rm -f ${BINARIES_DIR}/u-boot.itb
+cp ${BOARD_DIR}/configs/u-boot.its ${BINARIES_DIR}/u-boot.its || exit 1
+cp "${BINARIES_DIR}/u-boot-spl.dtb" "${BINARIES_DIR}/u-boot-spl-key.dtb"
 
 # First check for local keys, generate own if not
 # Then update uboot dtb with keys & sign kernel
 # Then build uboot FIT
-echo "# entering $BINARIES_DIR for the next command"
-(cd "$BINARIES_DIR" && "$mkimage" -f u-boot.its -K u-boot-spl-key.dtb -k keys -r u-boot.itb) || exit 1
+echo "# entering ${BINARIES_DIR} for the next command"
+(cd "${BINARIES_DIR}" && "$mkimage" -f u-boot.its -K u-boot-spl-key.dtb -k keys -r u-boot.itb) || exit 1
 
 # Then update SPL with appended keyed DTB
-cat "$BINARIES_DIR/u-boot-spl-nodtb.bin" "$BINARIES_DIR/u-boot-spl-key.dtb" > "$BINARIES_DIR/u-boot-spl.bin"
+cat "${BINARIES_DIR}/u-boot-spl-nodtb.bin" "${BINARIES_DIR}/u-boot-spl-key.dtb" > "${BINARIES_DIR}/u-boot-spl.bin"
 
-rm -f "$BINARIES_DIR/u-boot-spl-key.dtb"
+rm -f "${BINARIES_DIR}/u-boot-spl-key.dtb"
 
-if [[ $BR2_LRD_PLATFORM != *"sd" ]] && [[ $BR2_LRD_PLATFORM != *"sd_"* ]] ; then
+if [[ ${BR2_LRD_PLATFORM} != *"sd" ]] && [[ ${BR2_LRD_PLATFORM} != *"sd_"* ]] ; then
 	# Regenerate Atmel PMECC boot.bin
-	$mkimage -T atmelimage -n $($atmel_pmecc_params) -d $BINARIES_DIR/u-boot-spl.bin $BINARIES_DIR/boot.bin
+	${mkimage} -T atmelimage -n $(${atmel_pmecc_params}) -d ${BINARIES_DIR}/u-boot-spl.bin ${BINARIES_DIR}/boot.bin
 
 	# Build the UBI
 	rm -rf "${GENIMAGE_TMP}"
-	$genimage                          \
+	${genimage}                          \
 		--rootpath "${TARGET_DIR}"     \
 		--tmppath "${GENIMAGE_TMP}"    \
 		--inputpath "${BINARIES_DIR}"  \
@@ -98,8 +101,6 @@ if [[ $BR2_LRD_PLATFORM != *"sd" ]] && [[ $BR2_LRD_PLATFORM != *"sd_"* ]] ; then
 		--config "${GENIMAGE_CFG}"
 
 	# generate SWUpdate .swu image
-	cp $BOARD_DIR/configs/sw-description "$BINARIES_DIR/"
-	if cd "$BINARIES_DIR"; then
-		$BOARD_DIR/../sw_image_generator.sh "$BINARIES_DIR" "boot.bin u-boot.itb kernel.itb rootfs.bin uboot-env.bin"
-	fi
+	cp ${BOARD_DIR}/configs/sw-description "${BINARIES_DIR}/"
+	${BOARD_DIR}/../sw_image_generator.sh "${BINARIES_DIR}" "boot.bin u-boot.itb kernel.itb rootfs.bin uboot-env.bin"
 fi
