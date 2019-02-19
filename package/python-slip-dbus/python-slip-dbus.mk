2c9794103e2d124c4c66ec952251950556008b21
sean meng <sean.meng@lairdtech.com>
2019-02-19 13:55:09 -0500

    Buildroot: add new pacage python-slip-dbus
    Needed by firewalld.
    
    Bug 13952

---
+++ package/python-slip-dbus/python-slip-dbus.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# python-slip-dbus
+#
+################################################################################
+
+PYTHON_SLIP_DBUS_VERSION_MAJOR = 0.6
+PYTHON_SLIP_DBUS_VERSION = $(PYTHON_SLIP_DBUS_VERSION_MAJOR).5
+PYTHON_SLIP_DBUS_SOURCE = python-slip-$(PYTHON_SLIP_DBUS_VERSION).tar.gz
+PYTHON_SLIP_DBUS_SITE = https://github.com/nphilipp/python-slip/archive
+PYTHON_SLIP_DBUS_LICENSE_FILES = COPYING
+PYTHON_SLIP_DBUS_DEPENDENCIES = libglib2 cairo
+PYTHON_SLIP_DBUS_SETUP_TYPE= setuptools
+PYTHON_SLIP_DBUS_INSTALL_STAGING= YES
+PYTHON_SLIP_DBUS_INSTALL_TARGET= YES
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+PYTHON_SLIP_DBUS_DEPENDENCIES += python host-python
+PYTHON_SITE_PACKAGET_PATH = python2.7
+PYTHON_SLIP_DBUS_CONF_ENV = \
+        PYTHON=$(HOST_DIR)/bin/python2 \
+        PYTHON_INCLUDES="`$(STAGING_DIR)/usr/bin/python2-config --includes`"
+else
+PYTHON_SLIP_DBUS_DEPENDENCIES += python3 host-python3
+PYTHON_SITE_PACKAGET_PATH = python3.6
+PYTHON_SLIP_DBUS_CONF_ENV = \
+        PYTHON=$(HOST_DIR)/bin/python3 \
+        PYTHON_INCLUDES="`$(STAGING_DIR)/usr/bin/python3-config --includes`"
+endif
+
+define COPY_SLIP_OVER
+	cp -r $(BUILD_DIR)/python-slip-dbus-$(PYTHON_SLIP_DBUS_VERSION)/slip $(TARGET_DIR)/usr/lib/$(PYTHON_SITE_PACKAGET_PATH)/site-packages/slip
+endef
+
+PYTHON_SLIP_DBUS_POST_INSTALL_TARGET_HOOKS= COPY_SLIP_OVER
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
