6130fc4062dc9c34e273ad72910ba1a8dcbe2063
Service DevOps Buildserver <service.devops.buildserver@lairdtech.com>
2019-04-22 19:33:18 +0000

    laird: Adopt msd binaries version 7.0.0.64

--- package/linux-backports/linux-backports.mk
+++ package/linux-backports/linux-backports.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINUX_BACKPORTS_VERSION = 7.0.0.63
+LINUX_BACKPORTS_VERSION = 7.0.0.64
 
 LINUX_BACKPORTS_SOURCE = backports-laird-$(LINUX_BACKPORTS_VERSION).tar.bz2
 BR_NO_CHECK_HASH_FOR += $(LINUX_BACKPORTS_SOURCE)
