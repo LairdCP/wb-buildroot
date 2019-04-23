d2bb9391f8bb7b02cfc68886e1768cf7e94b1260
sean meng <sean.meng@lairdtech.com>
2019-04-23 10:36:41 -0400

    buildroot:decouple summit supplicant binaries version from build number
    This reverts commit c946a983dcb2d3ef324f3831c0e38b50902aa24a.
    
    Bug 14104

--- package/lrd/summit-supplicant-binaries/summit-supplicant-binaries.mk
+++ package/lrd/summit-supplicant-binaries/summit-supplicant-binaries.mk
@@ -1,13 +1,18 @@
 ifneq ($(BR2_LRD_DEVEL_BUILD),y)
 
-SUMMIT_SUPPLICANT_BINARIES_VERSION = 7.0.0.65
+SUMMIT_SUPPLICANT_BINARIES_VERSION = 7.0.0.38
 SUMMIT_SUPPLICANT_BINARIES_SOURCE =
 SUMMIT_SUPPLICANT_BINARIES_LICENSE = GPL-2.0
-SUMMIT_SUPPLICANT_BINARIES_SITE = https://github.com/LairdCP/wb-package-archive/raw/master
 SUMMIT_SUPPLICANT_BINARIES_EXTRA_DOWNLOADS = summit_supplicant-arm-eabihf-$(SUMMIT_SUPPLICANT_BINARIES_VERSION).tar.bz2
 
+ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
+  SUMMIT_SUPPLICANT_BINARIES_SITE = http://devops.lairdtech.com/share/builds/linux/summit_supplicant/laird/$(SUMMIT_SUPPLICANT_BINARIES_VERSION)
+else
+  SUMMIT_SUPPLICANT_BINARIES_SITE = https://github.com/LairdCP/SOM60-Release-Packages/releases/download/LRD-REL-$(SUMMIT_SUPPLICANT_BINARIES_VERSION)
+endif
+
 define SUMMIT_SUPPLICANT_BINARIES_INSTALL_TARGET_CMDS
-	tar -xjf $(DL_DIR)/$(SUMMIT_SUPPLICANT_BINARIES_EXTRA_DOWNLOADS) -C $(TARGET_DIR) --keep-directory-symlink --no-overwrite-dir --touch
+	tar -xjf $(DL_DIR)/summit-supplicant-binaries/$(SUMMIT_SUPPLICANT_BINARIES_EXTRA_DOWNLOADS) -C $(TARGET_DIR) --keep-directory-symlink --no-overwrite-dir --touch
 endef
 
 endif
