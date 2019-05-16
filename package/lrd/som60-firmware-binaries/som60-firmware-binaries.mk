42e31ad59d97b87c52c91eb08a7993de0d56c53c
sean meng <sean.meng@lairdtech.com>
2019-05-16 15:10:12 -0400

    buildroot:decouple firmware binaries version from build number
    ath6003/ath6004 and som60 firmware are seperated from the package
    and WB50 sysd and SOM60 targets now are able to use different versions.
    
    The rest firmwares still roll with the versioning script.
    
    Bug 14105

---
+++ package/lrd/som60-firmware-binaries/som60-firmware-binaries.mk
@@ -0,0 +1,25 @@
+ifeq ($(BR2_LRD_DEVEL_BUILD),y)
+else
+SOM60_FIRMWARE_BINARIES_VERSION = 0.0.0.0
+SOM60_FIRMWARE_BINARIES_SOURCE =
+SOM60_FIRMWARE_BINARIES_LICENSE = GPL-2.0
+SOM60_FIRMWARE_BINARIES_LICENSE_FILES = COPYING
+SOM60_FIRMWARE_BINARIES_EXTRA_DOWNLOADS = laird-som60-radio-firmware-$(SOM60_FIRMWARE_BINARIES_VERSION).tar.bz2
+
+ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
+  SOM60_FIRMWARE_BINARIES_SITE = http://devops.lairdtech.com/share/builds/linux/firmware/$(SOM60_FIRMWARE_BINARIES_VERSION)
+else
+  SOM60_FIRMWARE_BINARIES_SITE = https://github.com/LairdCP/SOM60-Release-Packages/releases/download/LRD-REL-$(SOM60_FIRMWARE_BINARIES_VERSION)
+endif
+
+define SOM60_FIRMWARE_BINARIES_SOM60_INSTALL_TARGET
+	tar -xjf $(DL_DIR)/som60-firmware-binaries/laird-som60-radio-firmware-$(SOM60_FIRMWARE_BINARIES_VERSION).tar.bz2 -C $(TARGET_DIR) --keep-directory-symlink --no-overwrite-dir --touch
+endef
+
+define SOM60_FIRMWARE_BINARIES_INSTALL_TARGET_CMDS
+	$(SOM60_FIRMWARE_BINARIES_SOM60_INSTALL_TARGET)
+endef
+
+endif
+
+$(eval $(generic-package))
