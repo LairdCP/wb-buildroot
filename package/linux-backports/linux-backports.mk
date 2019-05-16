1954185c55d598551eb5314be777154b9ee5c94a
Service DevOps Buildserver <service.devops.buildserver@lairdtech.com>
2019-05-16 21:54:04 +0000

    laird: Adopt msd binaries version 7.0.0.72

--- package/linux-backports/linux-backports.mk
+++ package/linux-backports/linux-backports.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LINUX_BACKPORTS_VERSION = 7.0.0.71
-SOM60_LINUX_BACKPORTS_VERSION = 7.0.0.71
+LINUX_BACKPORTS_VERSION = 7.0.0.72
+SOM60_LINUX_BACKPORTS_VERSION = 7.0.0.72
 
 ifeq (som60,$(findstring som60,$(BR2_DEFCONFIG)))
-LINUX_BACKPORTS_VERSION = 7.0.0.71
+LINUX_BACKPORTS_VERSION = 7.0.0.72
 endif
 
 LINUX_BACKPORTS_SOURCE = backports-laird-$(LINUX_BACKPORTS_VERSION).tar.bz2
