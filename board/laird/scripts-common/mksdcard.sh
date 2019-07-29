9880c608c094ad372fcc5055c27e5fe847b0fb6c
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2019-07-29 13:50:53 -0400

    Building and performance fixes for mksdcard.sh and mksdimg.sh
    Bug 14832

--- board/laird/scripts-common/mksdcard.sh
+++ board/laird/scripts-common/mksdcard.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 DRIVE=${1}
 ARG=${1##*/}
 SRCDIR=${0%/*}
 DRIVE_SIZE=/sys/block/${DRIVE##*/}/size
 
-if [ -z ${DRIVE} ] || [ -z ${ARG} ]; then
+if [ -z "${DRIVE}" ] || [ -z "${ARG}" ]; then
     echo "mksdcard.sh <device>"
     echo "  <device> is the SD card to be programmed (e.g., /dev/sdc)"
     exit
@@ -17,21 +17,18 @@ if [ ! -b ${DRIVE} ] || [ "$(cat ${DRIVE_SIZE})" -eq 0 ]; then
 fi
 
 if [ ! -e ${SRCDIR}/rootfs.tar ]; then
-    echo "Can not find required rootfs.tar file. This script only works"
-    echo "if the build was done for the som60sd target."
+    echo "Can not find required rootfs.tar file."
     exit
 fi
 
-OPT=${ARG::-1}
-
-case "$OPT" in
-    "sd")
+case "${ARG}" in
+    sd*)
         PART_BOOT=${DRIVE}1
         PART_SWAP=${DRIVE}2
         PART_ROOTFS=${DRIVE}3
         ;;
 
-    "mmcblk")
+    mmcblk*)
         PART_BOOT=${DRIVE}p1
         PART_SWAP=${DRIVE}p2
         PART_ROOTFS=${DRIVE}p3
@@ -44,7 +41,7 @@ case "$OPT" in
 esac
 
 # Un-mount all mounted partitions
-umount -f ${DRIVE}? &> /dev/null
+umount -f ${DRIVE}? 2> /dev/null
 
 # Check if device is busy
 hdparm -z ${DRIVE} >/dev/null || exit
@@ -52,23 +49,27 @@ hdparm -z ${DRIVE} >/dev/null || exit
 echo "[Partitioning ${DRIVE}...]"
 
 # Wipe MBR and Partition Table
-dd if=/dev/zero of=${DRIVE} bs=512 count=1
+dd if=/dev/zero of=${DRIVE} bs=512 count=1 status=none
+dd if=/dev/zero of=${DRIVE} bs=1KiB count=1 seek=1024 status=none
+dd if=/dev/zero of=${DRIVE} bs=1KiB count=4 seek=$((49*1024)) status=none
+dd if=/dev/zero of=${DRIVE} bs=1KiB count=4 seek=$((305*1024)) status=none
 
-# Create new partition table
-sfdisk ${DRIVE} << EOF
-1M,48M,0xE,*
-49M,256M,S,-
-305M,,,-
-EOF
+parted -s ${DRIVE} mklabel msdos unit MiB \
+    mkpart primary fat16 1 49 set 1 lba on set 1 boot on \
+    mkpart primary linux-swap 49 305 \
+    mkpart primary ext4 305 100%
 
 [ $? -ne 0 ] && exit
+
+sync
 
 echo "[Making file systems...]"
 
 # Format newly created partitions
-mkfs.vfat -F 16 -n boot ${PART_BOOT} &> /dev/null
-mkswap ${PART_SWAP} &> /dev/null
-mkfs.ext4 -L rootfs ${PART_ROOTFS} -E lazy_itable_init=0,lazy_journal_init=0 &> /dev/null
+mkfs.vfat -F 16 -n BOOT ${PART_BOOT} >/dev/null
+mkswap -L swap ${PART_SWAP} >/dev/null
+mkfs.ext4 -q -L rootfs ${PART_ROOTFS} -E lazy_itable_init=0,lazy_journal_init=0
+sync
 
 echo "[Copying files...]"
 
@@ -94,6 +95,6 @@ tar xf ${SRCDIR}/rootfs.tar -C ${MNT_ROOTFS}
 sync
 
 umount ${MNT_ROOTFS} && rm -rf ${MNT_ROOTFS}
-umount -f ${DRIVE}? &> /dev/null
+umount -f ${DRIVE}? 2> /dev/null
 
 echo "[Done]"
--- board/laird/scripts-common/mksdimg.sh
+++ board/laird/scripts-common/mksdimg.sh
@@ -2,18 +2,25 @@
 
 IMGFILE=$1
 ROOTFS_EXTRA_SIZE=${2:-0}
+ROOTFSDIR=$3
 SRCDIR=${0%/*}
 
+BLOCK_SIZE=1024
 MiBCONVERTER=$(( 1024 * 1024 ))
 
 if [ -z "${IMGFILE}" ]; then
-    echo "mksdimg.sh <output filename> [optional rootfs free space size in MiB]" >&2
+    echo "mksdimg.sh <output filename> [rootfs free space size in MiB] [rootfs source directory]" >&2
     exit
 fi
 
-if [ ! -f "${SRCDIR}/rootfs.tar" ]; then
-    echo "Could not find required rootfs.tar file." >&2
-    exit
+if [ -z "${ROOTFSDIR}" ]; then
+	if [ ! -f "${SRCDIR}/rootfs.tar" ]; then
+		echo "Could not find required rootfs.tar file." >&2
+		exit
+	fi
+elif [ ! -d "${ROOTFSDIR}" ]; then
+	echo "Could not find requested rootfs directory: ${ROOTFSDIR}." >&2
+	exit
 fi
 
 [ "${ROOTFS_EXTRA_SIZE}" -eq "${ROOTFS_EXTRA_SIZE}" ] 2>/dev/null
@@ -28,19 +35,18 @@ echo "[Creating card image...]"
 ROOTFS_FILE_SIZE=$(stat -c%s "${SRCDIR}/rootfs.tar")
 
 # Set image free space to 10% of the used space by default
-[ "${ROOTFS_EXTRA_SIZE}" -eq "0" ] &&
+[ "${ROOTFS_EXTRA_SIZE}" -eq 0 ] &&
 	ROOTFS_EXTRA_SIZE=$(( ROOTFS_FILE_SIZE * 11 / 10 ))
 
 # Calculate partitions
 BOOT_IMG_SIZE_MiB=48
-BOOT_BLOCK_SIZE=512
-BOOT_BLOCKS=$(( BOOT_IMG_SIZE_MiB * MiBCONVERTER / BOOT_BLOCK_SIZE ))
+BOOT_LBS=512
+BOOT_BLOCKS=$(( BOOT_IMG_SIZE_MiB * MiBCONVERTER / BLOCK_SIZE ))
 
 SWAP_IMG_SIZE_MiB=256
 
 ROOTFS_IMG_SIZE_MiB=$(( (ROOTFS_FILE_SIZE + ROOTFS_EXTRA_SIZE) / MiBCONVERTER + 1 ))
-ROOTFS_BLOCK_SIZE=1024
-ROOTFS_BLOCKS=$(( ROOTFS_IMG_SIZE_MiB * MiBCONVERTER / ROOTFS_BLOCK_SIZE ))
+ROOTFS_BLOCKS=$(( ROOTFS_IMG_SIZE_MiB * MiBCONVERTER / BLOCK_SIZE ))
 
 BOOT_START_MiB=1
 BOOT_END_MiB=$((BOOT_START_MiB + BOOT_IMG_SIZE_MiB))
@@ -49,29 +55,19 @@ SWAP_START_MiB=${BOOT_END_MiB}
 SWAP_END_MiB=$((SWAP_START_MiB + SWAP_IMG_SIZE_MiB))
 
 ROOTFS_START_MiB=${SWAP_END_MiB}
-ROOTFS_END_MiB=$((ROOTFS_START_MiB + ROOTFS_IMG_SIZE_MiB))
-
-TOTAL_IMG_SIZE_MiB=$((ROOTFS_END_MiB + 1))
-
-# Create disk image placeholder
-rm -f ${IMGFILE}
-fallocate -l ${TOTAL_IMG_SIZE_MiB}MiB ${IMGFILE}
-[ $? -ne 0 ] && exit
-
-# Create image partition table
-parted -s ${IMGFILE} mklabel msdos unit MiB \
-    mkpart primary fat16 ${BOOT_START_MiB} ${BOOT_END_MiB} set 1 lba on set 1 boot on \
-    mkpart primary linux-swap ${SWAP_START_MiB} ${SWAP_END_MiB} \
-    mkpart primary ext4 ${ROOTFS_START_MiB} ${ROOTFS_END_MiB}
-
-[ $? -ne 0 ] && exit
+ROOTFS_END_MiB=$(( ROOTFS_START_MiB + ROOTFS_IMG_SIZE_MiB ))
 
 TMPDIR=$(mktemp -d)
+IMGTMPFILE=${TMPDIR}/${IMGFILE}
+
+# Create disk image placeholder
+fallocate -l ${BOOT_START_MiB}MiB ${IMGTMPFILE}
+[ $? -ne 0 ] && exit
 
 echo "[Creating boot partition...]"
 
 # Create boot partition image
-mkfs.vfat -F 16 -n BOOT -S ${BOOT_BLOCK_SIZE} -C ${TMPDIR}/boot.img ${BOOT_BLOCKS} > /dev/null
+mkfs.vfat -F 16 -n BOOT -S ${BOOT_LBS} -C ${TMPDIR}/boot.img ${BOOT_BLOCKS} > /dev/null
 
 # Add files to boot partition image
 mcopy -i ${TMPDIR}/boot.img ${SRCDIR}/u-boot-spl.bin ::/boot.bin
@@ -79,7 +75,7 @@ mcopy -i ${TMPDIR}/boot.img ${SRCDIR}/u-boot.itb ::/
 mcopy -i ${TMPDIR}/boot.img ${SRCDIR}/kernel.itb ::/
 
 # Add boot partition to disk image
-dd if=${TMPDIR}/boot.img of=${IMGFILE} conv=notrunc bs=1MiB seek=${BOOT_START_MiB} count=${BOOT_IMG_SIZE_MiB} status=none
+cat ${TMPDIR}/boot.img >> ${IMGTMPFILE}
 rm -f ${TMPDIR}/boot.img
 
 echo "[Creating swap partition...]"
@@ -92,29 +88,41 @@ chmod 600 ${TMPDIR}/swap.img
 mkswap -L swap ${TMPDIR}/swap.img > /dev/null
 
 # Add swap partition to disk image
-dd if=${TMPDIR}/swap.img of=${IMGFILE} conv=notrunc bs=1MiB seek=${SWAP_START_MiB} count=${SWAP_IMG_SIZE_MiB} status=none
+cat ${TMPDIR}/swap.img >> ${IMGTMPFILE}
 rm -f ${TMPDIR}/swap.img
 
 echo "[Creating rootfs partition...]"
 
 # Create rootfs directory for mkfs.ext4
-mkdir ${TMPDIR}/rootfs.tmp
-tar xf ${SRCDIR}/rootfs.tar -C ${TMPDIR}/rootfs.tmp
+if [ -z "${ROOTFSDIR}" ]; then
+	ROOTFSDIR=${TMPDIR}/rootfs.tmp
+	mkdir ${ROOTFSDIR}
+	tar xf ${SRCDIR}/rootfs.tar -C ${ROOTFSDIR}
+fi
 
 # Create rootfs partition image
-mkfs.ext4 -q -L rootfs -F -b ${ROOTFS_BLOCK_SIZE} -d ${TMPDIR}/rootfs.tmp -E lazy_itable_init=0,lazy_journal_init=0 ${TMPDIR}/rootfs.img ${ROOTFS_BLOCKS}
+mkfs.ext4 -q -L rootfs -F -b ${BLOCK_SIZE} -d ${ROOTFSDIR} -E lazy_itable_init=0,lazy_journal_init=0 ${TMPDIR}/rootfs.img ${ROOTFS_BLOCKS}
 rm -rf ${TMPDIR}/rootfs.tmp
 
 # Add rootfs partition to disk image
-dd if=${TMPDIR}/rootfs.img of=${IMGFILE} conv=notrunc bs=1MiB seek=${ROOTFS_START_MiB} count=${ROOTFS_IMG_SIZE_MiB} status=none
+cat ${TMPDIR}/rootfs.img >> ${IMGTMPFILE}
 rm -f ${TMPDIR}/rootfs.img
 
-rm -rf ${TMPDIR}
+# Create image partition table
+parted -s ${IMGTMPFILE} mklabel msdos unit MiB \
+    mkpart primary fat16 ${BOOT_START_MiB} ${BOOT_END_MiB} set 1 lba on set 1 boot on \
+    mkpart primary linux-swap ${SWAP_START_MiB} ${SWAP_END_MiB} \
+    mkpart primary ext4 ${ROOTFS_START_MiB} 100%
 
-echo "[Compressing card image...]"
-xz -9f ${IMGFILE}
+if [ $? -eq 0 ]; then
+	echo "[Compressing card image...]"
+	xz -9cT 0 ${IMGTMPFILE} > ${IMGFILE}.xz
+
+	echo "[Image file: ${IMGFILE}.xz]"
+	echo "SD Card Programming: umount /dev/sdX? ; xz -dc ${IMGFILE}.xz | sudo dd of=/dev/sdX bs=4M conv=fsync"
+fi
+
+rm -rf ${TMPDIR}
+sync
 
 echo "[Done]"
-echo
-echo "[Image file: ${IMGFILE}.xz]"
-echo "SD Card Programming: umount /dev/sdX ; xz -dc ${IMGFILE}.xz | dd of=/dev/sdX bs=4M conv=fsync"
