e77ff759534b40bfbc5738dfb6559bf5a4d7174f
Moses Correia <Moses.Correia@lairdtech.com>
2019-08-07 11:03:58 -0400

    Bug 14855: Added make file and config to add the btpa_utility to the ig60 build in buildroot

---
+++ package/lrd/lrd-bt-utils/lrd_bt_utils.mk
@@ -0,0 +1,17 @@
+##########################################################################
+# Sentrius IG60 lrd_bt_utils
+##########################################################################
+
+LRD_BT_UTILS_VERSION = local
+LRD_BT_UTILS_SITE = package/lrd/externals/lrd-bt-utils
+LRD_BT_UTILS_SITE_METHOD = local
+
+
+define LRD_BT_UTILS_INSTALL_TARGET_FILES
+	$(INSTALL) -D -m 755 $(LRD_BT_UTILS_SITE)/*.py $(TARGET_DIR)/usr/bin/
+endef
+
+LRD_BT_UTILS_POST_INSTALL_TARGET_HOOKS += LRD_BT_UTILS_INSTALL_TARGET_FILES
+
+$(eval $(generic-package))
+
