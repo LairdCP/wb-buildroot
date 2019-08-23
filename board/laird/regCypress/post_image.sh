fb508814af0c4e5a19c6f5625a23e34c84d0dfe4
sean meng <sean.meng@lairdtech.com>
2019-08-23 09:21:01 -0400

    regCypress builds: let jenkins do the versioning
    Bug 14927

--- board/laird/regCypress/post_image.sh
+++ board/laird/regCypress/post_image.sh
@@ -2,45 +2,41 @@ IMAGESDIR="$1"
 
 export BR2_LRD_PRODUCT=regCypress
 
-[ ! -d "$IMAGESDIR" ] && mkdir -p "$IMAGESDIR"
+[ ! -d "${IMAGESDIR}" ] && mkdir -p "${IMAGESDIR}"
 
 # enable tracing and exit on errors
 set -x -e
 
-if [ -z "$LAIRD_RELEASE_STRING" ]; then
+if [ -z "${LAIRD_RELEASE_STRING}" ]; then
   LAIRD_RELEASE_STRING=$(date +%Y%m%d)
 fi
 
 # insert version number
-sed -i "2i#$LAIRD_RELEASE_STRING" $TARGET_DIR/reg_tools.sh
-mv "$TARGET_DIR/$BR2_LRD_PRODUCT.manifest" "$TARGET_DIR/$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.manifest"
-sed -i "1i#$LAIRD_RELEASE_STRING" "$TARGET_DIR/$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.manifest"
+sed -i "2i#${LAIRD_RELEASE_STRING}" ${TARGET_DIR}/reg_tools.sh
+sed -i "1i#${LAIRD_RELEASE_STRING}" ${TARGET_DIR}/${BR2_LRD_PRODUCT}.manifest
 
-TARFILE="$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.tar"
+TARFILE="${BR2_LRD_PRODUCT}.tar.bz2"
 
 # generate tar.bz2 to be inserted in script
-tar -cvf $IMAGESDIR/$TARFILE --directory="$TARGET_DIR/usr/bin" .
-tar --append --file="$IMAGESDIR/$TARFILE" -C "$TARGET_DIR/lib/firmware/brcm/bcm4339/" "brcmfmac4339-sdio-mfg.bin"
-tar --append --file="$IMAGESDIR/$TARFILE" -C "$TARGET_DIR/lib/firmware/brcm/bcm4339/" "brcmfmac4339-sdio.bin"
-tar --append --file="$IMAGESDIR/$TARFILE" -C "$TARGET_DIR/lib/firmware/brcm/bcm4343w/" "brcmfmac43430-sdio-mfg.bin"
-tar --append --file="$IMAGESDIR/$TARFILE" -C "$TARGET_DIR/lib/firmware/brcm/bcm4343w/" "brcmfmac43430-sdio.bin"
-tar --append --file="$IMAGESDIR/$TARFILE" -C "$TARGET_DIR/" "$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.manifest"
-bzip2 -f "$IMAGESDIR/$TARFILE"
+tar -cjf "${IMAGESDIR}/${TARFILE}" -C ${TARGET_DIR} \
+	${BR2_LRD_PRODUCT}.manifest \
+	usr/bin/wl \
+	lib/firmware/brcm/bcm4339/brcmfmac4339-sdio-mfg.bin \
+	lib/firmware/brcm/bcm4339/brcmfmac4339-sdio.bin \
+	lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio-mfg.bin \
+	lib/firmware/brcm/bcm4343w/brcmfmac43430-sdio.bin
 
 # generate sha to validate package
-CURRENT_PWD=`pwd`
-cd $IMAGESDIR
-sha256sum "$TARFILE.bz2" > "$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.sha"
-cd $CURRENT_PWD
+sha256sum ${IMAGESDIR}/${TARFILE} > ${IMAGESDIR}/${BR2_LRD_PRODUCT}.sha
 
 # generate self-extracting script and repackage tar.bz2 to contain script and sum file
-cat $TARGET_DIR/reg_tools.sh "$IMAGESDIR/$TARFILE.bz2" > "$IMAGESDIR/$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.sh"
-chmod +x "$IMAGESDIR/$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.sh"
+cat ${TARGET_DIR}/reg_tools.sh ${IMAGESDIR}/${TARFILE} > ${IMAGESDIR}/${BR2_LRD_PRODUCT}.sh
+chmod +x ${IMAGESDIR}/${BR2_LRD_PRODUCT}.sh
 
 # remove old tarfile and recreate new one containing self-extracting script and sha file
-rm "$IMAGESDIR/$TARFILE.bz2"
-tar -cvf "$IMAGESDIR/$TARFILE" --directory="$IMAGESDIR" "$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.sh"
-tar --append --file="$IMAGESDIR/$TARFILE" --directory="$IMAGESDIR" "$BR2_LRD_PRODUCT-$LAIRD_RELEASE_STRING.sha"
-bzip2 -f "$IMAGESDIR/$TARFILE"
+rm ${IMAGESDIR}/${TARFILE}
+tar -cjf "${IMAGESDIR}/${TARFILE}" -C ${IMAGESDIR} \
+	${BR2_LRD_PRODUCT}.sh \
+	${BR2_LRD_PRODUCT}.sha
 
 echo "REGCYPRESS POST BUILD script: done."
