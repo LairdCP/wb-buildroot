a2fffa9b1698dba5250c222264106e6e130662e2
sean meng <sean.meng@lairdtech.com>
2019-08-30 12:50:10 -0400

    Improve developer keys for signed images
    1. Move devel keys to board/laird/configs-common
    2. Add keys path to post script args
    
    Bug 15030

--- board/laird/wb50n_sysd/post_image.sh
+++ board/laird/wb50n_sysd/post_image.sh
@@ -1,12 +1,13 @@
 BOARD_DIR="$(dirname $0)"
 
 export BR2_LRD_PLATFORM="${2}"
+DEVEL_KEYS="${3}"
 
 echo "${BR2_LRD_PLATFORM^^} POST IMAGE script: starting..."
 
 [[ ${BR2_LRD_PLATFORM} == *"sd_sysd" ]]
 SD=$?
 
-. "${BOARD_DIR}/../post_image_common_60.sh" "${BOARD_DIR}" ${SD}
+. "${BOARD_DIR}/../post_image_common_60.sh" "${BOARD_DIR}" ${SD} "${DEVEL_KEYS}"
 
 echo "${BR2_LRD_PLATFORM^^} POST IMAGE script: done."
