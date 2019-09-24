2bfbb1e736c3a5f243707449a4b6eb71010e8587
Doug.Smith <Doug.Smith@lairdtech.com>
2019-09-24 17:12:58 -0400

    BZ15138: use kernel-fips for fips builds
    Use the kernel-fips directory for
    fips builds.
    
    Bug: 15138

---
+++ board/laird/wb50n_sysd/wb50n_sysd_fips-local.mk
@@ -0,0 +1,3 @@
+LINUX_OVERRIDE_SRCDIR = package/lrd/externals/kernel-fips
+UBOOT_OVERRIDE_SRCDIR = package/lrd/externals/u-boot-som60
+UBOOT_TOOLS_OVERRIDE_SRCDIR = package/lrd/externals/u-boot-som60
