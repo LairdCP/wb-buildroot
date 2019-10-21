2bdecc102aceaad6760f0c19cd02f6a6345c7057
sean meng <sean.meng@lairdtech.com>
2019-10-21 15:41:31 -0400

    Create new wb50n_sysd_rdvk build
    Bug 14964

---
+++ board/laird/wb50n_sysd_rdvk/post_build.sh
@@ -0,0 +1,16 @@
+# enable tracing and exit on errors
+set -x -e
+
+WB50N_SYSD_RDVK_DIR="$(dirname $0)"
+WB50N_SYSD_DIR="${WB50N_SYSD_RDVK_DIR}/../wb50n_sysd"
+
+export BR2_LRD_PLATFORM="${2}"
+
+echo "${BR2_LRD_PLATFORM^^} POST BUILD script: starting..."
+
+# source the common post build script
+. "${WB50N_SYSD_RDVK_DIR}/../post_build_common_60.sh" "${WB50N_SYSD_DIR}" 1
+
+rsync -rlptDWK --exclude=.empty "${WB50N_SYSD_RDVK_DIR}/rootfs-extra/" "${TARGET_DIR}"
+
+echo "${BR2_LRD_PLATFORM^^} POST BUILD script: done."
---
+++ board/laird/wb50n_sysd_rdvk/rootfs-extra/root/msd50n-start.sh
@@ -0,0 +1,5 @@
+rm /lib/firmware/ath6k/AR6004/hw3.0/bdata.bin
+rm /lib/firmware/ath6k/AR6004/hw3.0/fw-5.bin
+ln -s `ls /lib/firmware/ath6k/AR6004/hw3.0/50NBT* | tail -1` /lib/firmware/ath6k/AR6004/hw3.0/bdata.bin
+ln -s `ls /lib/firmware/ath6k/AR6004/hw3.0/fw_v* | tail -1` /lib/firmware/ath6k/AR6004/hw3.0/fw-5.bin
+ifrc wlan0 start
