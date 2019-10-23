bb73ff58da63c0763ce78ad03a2f5ac43261b2bd
sean meng <sean.meng@lairdtech.com>
2019-10-23 08:17:26 -0400

    buildroot: add new target - bdimx6_rdvk
    1. switch to systemd
    2. kernel updated to 4.14.98
    3. swupdate for mmc/emmc devices
    
    
    Bug 14964, 14882

--- board/boundarydevices/common/post-image.sh
+++ board/boundarydevices/common/post-image.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
 
 BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Tooling checks
+mkimage="${HOST_DIR}/bin/mkimage"
+atmel_pmecc_params="${BUILD_DIR}/uboot-custom/tools/atmel_pmecc_params"
+openssl="${HOST_DIR}/usr/bin/openssl"
+genimage="${HOST_DIR}/bin/genimage"
+veritysetup="${HOST_DIR}/sbin/veritysetup"
+mkenvimage="${HOST_DIR}/bin/mkenvimage"
+
+set -x -e
 
 rm -rf "${GENIMAGE_TMP}"
 
@@ -13,4 +22,13 @@ genimage                           \
 	--outputpath "${BINARIES_DIR}" \
 	--config "${GENIMAGE_CFG}"
 
-exit $?
+[ -z "${BR2_LRD_PRODUCT}" ] && \
+	BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_CONFIG})"
+
+if [[ "${BR2_LRD_PRODUCT}" == *"rdvk" ]]
+then
+	cp "${BOARD_DIR}"/sw-description "${BINARIES_DIR}"/
+	(cd  "${BINARIES_DIR}" && gzip -f rootfs.ext2)
+	(cd "${BINARIES_DIR}" && echo -e "sw-description\nrootfs.ext2.gz" |\
+		cpio -ov -H crc > "${BINARIES_DIR}/${BR2_LRD_PRODUCT}.swu")
+fi
