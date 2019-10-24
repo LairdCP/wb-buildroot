cb208c3a283a9b58ab00c8f1c2fdfb63237d171b
sean meng <sean.meng@lairdtech.com>
2019-10-24 10:15:27 -0400

    Firmware update by weblcm-python
    Using swupdate client API. swupdate client library needs to be installed.
    
    sw-description should only contain description of images to be installed.
    
    Bug 13442

---
+++ package/lrd/lrd-swupdate-client/lrd-swupdate-client.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# lrd-swupdate-client
+#
+################################################################################
+
+LRD_SWUPDATE_CLIENT_VERSION = local
+LRD_SWUPDATE_CLIENT_SITE = package/lrd/externals/lrd-userspace-examples/swclient
+LRD_SWUPDATE_CLIENT_SITE_METHOD = local
+LRD_SWUPDATE_CLIENT_SETUP_TYPE = distutils
+LRD_SWUPDATE_CLIENT_DEPENDENCIES = swupdate
+
+$(eval $(python-package))
