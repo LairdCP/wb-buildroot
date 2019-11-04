c24bec140ca68f5483cd6d6831c18c676e64d843
Boris Krasnovskiy <boris.krasnovskiy@lairdtech.com>
2019-11-04 11:55:39 -0500

    Changed archive directory to the $(HOME)/.br2_dl_dir
    Bug 15374

--- board/laird/backports/post_image.sh
+++ board/laird/backports/post_image.sh
@@ -1,7 +1,15 @@
 IMAGESDIR="${1}"
 
-if [ "${BR2_LRD_DEVEL_BUILD}" == "y" ] && [ -n "${BR2_DL_DIR}" ]; then
-	mkdir -p ${BR2_DL_DIR}/linux-backports
-	cp ${IMAGESDIR}/*.bz2 ${BR2_DL_DIR}/linux-backports
-	cp ${IMAGESDIR}/*.bz2 ${BR2_DL_DIR}/backports-test
+# enable tracing and exit on errors
+set -x -e
+
+if [ "${BR2_LRD_DEVEL_BUILD}" == "y" ]; then
+	[ -z "${BR2_DL_DIR}" ] && \
+		BR2_DL_DIR="$(sed -n 's,^BR2_DL_DIR="\(.*\)"$,\1,p' ${BR2_CONFIG})"
+
+	if [ -n "${BR2_DL_DIR}" ]; then
+		mkdir -p ${BR2_DL_DIR}/linux-backports
+		cp ${IMAGESDIR}/*.bz2 ${BR2_DL_DIR}/linux-backports
+		cp ${IMAGESDIR}/*.bz2 ${BR2_DL_DIR}/backports-test
+	fi
 fi
