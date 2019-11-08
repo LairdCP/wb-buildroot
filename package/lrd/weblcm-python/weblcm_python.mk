156f2c0805f355c3a7cb84dc8bbf8d91c6778ce8
sean meng <sean.meng@lairdtech.com>
2019-11-08 11:10:56 -0500

    No runtime invalidation on pyc files when BR2_REPRODUCIBLE is set
    Details on runtime code invalidation https://docs.python.org/3/reference/import.html#pyc-invalidation, and https://www.python.org/dev/peps/pep-0552/.
    
    When BR2_REPRODUCIBLE is set, the byte-code files are invalidated by checked-hash at runtime.
    
    Bug 15409

--- package/lrd/weblcm-python/weblcm_python.mk
+++ package/lrd/weblcm-python/weblcm_python.mk
@@ -13,6 +13,12 @@ WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOKS += WEBLCM_PYTHON_INSTALL_TARGET_FILES
 
 TARGET_PYTHON_VERSION := $$(find $(TARGET_DIR)/usr/lib -maxdepth 1 -type d -name python* -printf "%f\n" | egrep -o '[0-9].[0-9]')
 
+ifeq ($(BR2_REPRODUCIBLE),y)
+define WEBLCM_PYTHON_FIX_TIME
+	sed -i -e 's/ExecStart=python/ExecStart=python --check-hash-based-pycs never/g' $(TARGET_DIR)/etc/systemd/system/weblcm-python.service
+endef
+endif
+
 define WEBLCM_PYTHON_INSTALL_TARGET_FILES
 	$(INSTALL) -D -m 755 $(@D)/dist/weblcm_python-1.0-py$(TARGET_PYTHON_VERSION).egg $(TARGET_DIR)/usr/bin/weblcm-python
 	mkdir -p -m 0775 $(TARGET_DIR)/etc/weblcm-python
@@ -34,6 +40,7 @@ define WEBLCM_PYTHON_INSTALL_TARGET_FILES
 	cp -r $(WEBLCM_PYTHON_SITE)/plugins/* $(TARGET_DIR)/var/www/plugins/
 
 	$(INSTALL) -D -m 644 $(WEBLCM_PYTHON_SITE)/weblcm-python.service $(TARGET_DIR)/etc/systemd/system
+	$(WEBLCM_PYTHON_FIX_TIME)
 	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
 	ln -sf $(TARGET_DIR)/etc/systemd/system/weblcm-python.service $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
 
