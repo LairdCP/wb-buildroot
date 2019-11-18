4c3847977c33ceaeb78da9127873f53a363cade3
sean.meng <Sean.Meng@lairdconnect.com>
2019-11-18 16:58:39 -0500

    Remove bdimx6 and wb50n_rdvk off master branch
    Bug 14965

--- board/laird/wb50n_rdvk/rootfs-additions/root/wl1835-setup.sh
+++
@@ -1,2 +0,0 @@
-rm /lib/firmware/ti-connectivity/wl18xx-conf.bin
-ln -s /lib/firmware/ti-connectivity/wl1835-conf.bin /lib/firmware/ti-connectivity/wl18xx-conf.bin
--- board/laird/wb50n_rdvk/rootfs-additions/root/wl1837-setup.sh
+++
@@ -1,2 +0,0 @@
-rm /lib/firmware/ti-connectivity/wl18xx-conf.bin
-ln -s /lib/firmware/ti-connectivity/wl1837-conf.bin /lib/firmware/ti-connectivity/wl18xx-conf.bin
