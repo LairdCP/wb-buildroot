bac04a669dbb239d13985f8f889dee8eb93848f2
James Kalbfleisch <James.Kalbfleisch@lairdconnect.com>
2019-11-18 16:25:35 -0500

    Expose the updateAPS command for local services to use.
    The provisioning service can update the wireless configs from it's config
    in addition to the ble mobile app.  Expose a d-bus interface to
    accomodate.
    
    Bug 15473

--- package/lrd/igconfd/igconfd.mk
+++ package/lrd/igconfd/igconfd.mk
@@ -18,6 +18,7 @@ endif
 define IGCONFD_INSTALL_TARGET_FILES
         $(INSTALL) -D -m 755 $(@D)/dist/igconfd-1.0-py$(IGCONFD_PYTHON_VERSION).egg $(TARGET_DIR)/usr/bin/igconfd
         $(INSTALL) -D -m 644 package/lrd/igconfd/igconfd.service $(TARGET_DIR)/etc/systemd/system
+        $(INSTALL) -D -m 644 package/lrd/igconfd/com.lairdtech.security.ConfigService.conf $(TARGET_DIR)/etc/dbus-1/system.d
         mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
         ln -sf ../igconfd.service $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
 endef
