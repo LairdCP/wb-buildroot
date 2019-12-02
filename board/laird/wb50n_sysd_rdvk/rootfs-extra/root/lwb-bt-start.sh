b7ef377972f4ae0e8c6f41f9654c7ad0b5ab4df0
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2019-12-02 09:56:01 -0500

    Fixed LWB firmware symlinks and RDVK bluetooth start scripts
    Bug 15205

--- board/laird/wb50n_sysd_rdvk/rootfs-extra/root/lwb-bt-start.sh
+++ board/laird/wb50n_sysd_rdvk/rootfs-extra/root/lwb-bt-start.sh
@@ -1,37 +1,12 @@
 #!/bin/sh
 
-set -u
-set -e
-
-readonly PATCHRAM_DEBUG_ARG="-d"
-
-NOHUP_EXE="/usr/bin/nohup"
-PATCHRAM_EXE="/usr/bin/patchram"
-BLUETOOTHD_EXE="/usr/libexec/bluetooth/bluetoothd"
-BT_FW_FILE="/lib/firmware/brcm/4343w.hcd"
-BT_UART_DEV="/dev/ttyS1"
-
-test -x "${NOHUP_EXE}"
-test -x "${PATCHRAM_EXE}"
-test -x "${BLUETOOTHD_EXE}"
-test -f "${BT_FW_FILE}"
-test -c "${BT_UART_DEV}"
-
 echo 30 > /sys/class/gpio/export
 echo high > /sys/class/gpio/pioA30/direction
 
-"${NOHUP_EXE}" "${BLUETOOTHD_EXE}" &
+/usr/bin/nohup /usr/libexec/bluetooth/bluetoothd &
 
 sleep 1
 
-"${PATCHRAM_EXE}" \
-	--patchram "${BT_FW_FILE}" \
-	--no2bytes \
-	--tosleep 1000 \
-	"${BT_UART_DEV}"
+/usr/bin/nohup /usr/bin/btattach -B /dev/ttyS1 -P bcm &
 
-hciattach "${BT_UART_DEV}" any
-
-hciconfig hci0 up
-
-hciconfig
+/usr/bin/hciconfig hci0 up
