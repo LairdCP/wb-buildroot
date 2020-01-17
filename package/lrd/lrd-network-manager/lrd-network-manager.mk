95d72a9a63f57fffc789425fbfd0506cf6e64e38
Victor Yeo <victor.yeo@lairdconnect.com>
2020-01-17 08:46:17 +0800

    remove iwconfig because it wrongly shows no wireless extensions for wlan0
    If the tool is not supported then it could be removed
    from the sample laird linux build image.
    
    Bug 15649

--- package/lrd/lrd-network-manager/lrd-network-manager.mk
+++ package/lrd/lrd-network-manager/lrd-network-manager.mk
@@ -29,7 +29,7 @@ endif
 
 LRD_NETWORK_MANAGER_INSTALL_STAGING = YES
 LRD_NETWORK_MANAGER_DEPENDENCIES = host-pkgconf udev gnutls \
-	libgcrypt wireless_tools util-linux host-intltool readline libndp
+	libgcrypt util-linux host-intltool readline libndp
 # Even though the COPYING file only contains the GPL-2.0 text, many
 # parts of network-manager are under LGPL-2.0. See the "Legal" section
 # of the CONTRIBUTING file for details.
