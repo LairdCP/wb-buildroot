52ed30e0d6e3b3c4a47b2c24db8f5d4c8f581cdd
Steve.Braneky <stephen.braneky@lairdconnect.com>
2020-01-24 14:45:09 -0500

    Updated Adaptive World Mode
    Added ability to select default regulatory power database
    Added ability to select Adaptive World Mode Power Tool on x86 platforms
    Added x86-x64 support
    
    Bug: 12751

--- package/lrd-closed-source/adaptive_ww/adaptive_ww.mk
+++ package/lrd-closed-source/adaptive_ww/adaptive_ww.mk
@@ -13,19 +13,39 @@ MY_MAKE_OPTS = CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" PKG_CONFI
 #
 # BUILD
 #
-define ADAPTIVE_WW_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(MY_MAKE_OPTS) -C $(@D)/awm
+ifeq ($(BR2_PACKAGE_ADAPTIVE_WW_LPT),y)
+define LAIRD_AWM_LPT_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(MY_MAKE_OPTS) -C $(@D)/lpt
+endef
+endif
+
+define ADAPTIVE_WW_BUILD_CMDS
+	$(LAIRD_AWM_LPT_BUILD_CMDS)
+	$(TARGET_MAKE_ENV) $(MAKE) $(MY_MAKE_OPTS) -C $(@D)/awm
 endef
 
 #
 #Install
 #
-define ADAPTIVE_WW_INSTALL_TARGET_CMDS
+ifeq ($(BR2_PACKAGE_ADAPTVE_WW_REGPWRDB),y)
+define AWM_REGPWRDB_INSTALL_TARGET_CMDS
+	mkdir -p -m 0755 $(TARGET_DIR)/lib/firmware
+	$(INSTALL) -D -m 755 $(@D)/lpt/db/regpwr.db $(TARGET_DIR)/lib/firmware/
+endef
+endif
+
+define AWM_ADAPTIVE_WW_INSTALL_TARGET_CMDS
 #Note: Do *NOT* install lpt on target.  This utility is for internal use only by FAEs to build
 #      the txpower database used by adaptive ww.
+	mkdir -p -m 0755 $(TARGET_DIR)/lib/firmware
 	$(INSTALL) -D -m 755 $(@D)/awm/bin/adaptive_ww $(TARGET_DIR)/usr/bin/adaptive_ww
 endef
+
+define ADAPTIVE_WW_INSTALL_TARGET_CMDS
+	$(AWM_REGPWRDB_INSTALL_TARGET_CMDS)
+	$(AWM_ADAPTIVE_WW_INSTALL_TARGET_CMDS)
+endef
+
 
 
 #
@@ -33,6 +53,7 @@ endef
 #
 define ADAPTIVE_WW_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/adaptive_ww
+	rm -f $(TARGET_DIR)/lib/firmware/regpwr.db
 endef
 
 $(eval $(generic-package))
