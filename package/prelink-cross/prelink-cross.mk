abeba9f8dd01b95fb3a9105151116b8ef2343d89
sean.meng <Sean.Meng@lairdconnect.com>
2020-01-28 09:50:38 -0500

    Revert "buildroot: use https instead of git to download prelink-cross"
    This reverts commit c5f832e4db2fcfa7b42b97f234415c30d41a4337.

--- package/prelink-cross/prelink-cross.mk
+++ package/prelink-cross/prelink-cross.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HOST_PRELINK_CROSS_VERSION = 05aeafd053e56356ec8c62f4bb8f7b95bae192f3
-HOST_PRELINK_CROSS_SITE = https://git.yoctoproject.org/git/prelink-cross
+HOST_PRELINK_CROSS_SITE = git://git.yoctoproject.org/prelink-cross
 HOST_PRELINK_CROSS_SITE_METHOD = git
 HOST_PRELINK_CROSS_LICENSE = GPL-2.0
 HOST_PRELINK_CROSS_LICENSE_FILES = COPYING
