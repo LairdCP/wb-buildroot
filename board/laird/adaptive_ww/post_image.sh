e7ef50be5129aa22f112bec84524e8eda6120fd9
Steve.Braneky <stephen.braneky@lairdconnect.com>
2020-01-29 16:26:03 -0500

    Laird: Changed AWM post script processing
    The x86-64 post build script was saving tar with incorrect name (x86 instead of x86-64)
    so fixed that and in the process changed the way all awm post scripts retrieved
    package name which eliminated the need for the post script argument.
    
    Bug: 12751
    (cherry picked from commit 43ab7df00550002951bdf256d09e333bc1bb22ca)

--- board/laird/adaptive_ww/post_image.sh
+++ board/laird/adaptive_ww/post_image.sh
@@ -1,4 +1,4 @@
-BR2_LRD_PRODUCT="${2}"
+BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_CONFIG})"
 
 echo "${BR2_LRD_PRODUCT^^} POST IMAGE script: starting..."
 
@@ -11,8 +11,9 @@ mkdir -p ${BINARIES_DIR}
 
 [ -n "${VERSION}" ] && RELEASE_SUFFIX="-${VERSION}"
 
-sed -n "s/^${PKGNAME},//p" "${BUILD_DIR}/packages-file-list.txt" |\
-     tar -cjf "${BINARIES_DIR}/${BR2_LRD_PRODUCT}${RELEASE_SUFFIX}.tar.bz2" \
-        -C "${TARGET_DIR}" -T -
+sed -n "s|^${PKGNAME},\./||p" "${BUILD_DIR}/packages-file-list.txt" |\
+	tar -cjf "${BINARIES_DIR}/${BR2_LRD_PRODUCT}${RELEASE_SUFFIX}.tar.bz2" \
+		--owner=0 --group=0 --numeric-owner \
+		-C "${TARGET_DIR}" -T -
 
 echo "${BR2_LRD_PRODUCT^^} POST IMAGE script: done."
