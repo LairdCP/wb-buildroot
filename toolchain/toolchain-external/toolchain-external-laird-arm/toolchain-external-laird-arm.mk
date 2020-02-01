bfef2cd44f479ab1106020a938e8c9ff8c80532a
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-02-01 16:28:47 -0500

    Updated toolchain to the laird build version 7.0.0.276
    Bug 15446

--- toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
+++ toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION = 7.0.0.271
+TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION = 7.0.0.276
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_SITE = https://files.devops.rfpros.com/builds/linux/toolchain/$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION)
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_SOURCE = som60_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION).tar.gz
 
