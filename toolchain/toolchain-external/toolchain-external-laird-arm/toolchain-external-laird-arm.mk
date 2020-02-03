90c0e2e23ec8bf73ef4dd1544d2ea6e6fce46bc2
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-02-03 12:17:50 -0500

    Added toolchain URL for external builds
    Bug 15446

--- toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
+++ toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
@@ -5,7 +5,12 @@
 ################################################################################
 
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION = 7.0.0.276
-TOOLCHAIN_EXTERNAL_LAIRD_ARM_SITE = https://files.devops.rfpros.com/builds/linux/toolchain/$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION)
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_SOURCE = som60_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION).tar.gz
+
+ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
+TOOLCHAIN_EXTERNAL_LAIRD_ARM_SITE = https://files.devops.rfpros.com/builds/linux/toolchain/$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION)
+else
+TOOLCHAIN_EXTERNAL_LAIRD_ARM_SITE = https://github.com/LairdCP/wb-package-archive/releases/download/LRD-REL-$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION)
+endif
 
 $(eval $(toolchain-external-package))
