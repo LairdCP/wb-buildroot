6f0fa528e3ceaac5c66f4647c8e68b932e1942e4
Steve.Braneky <stephen.braneky@lairdconnect.com>
2020-02-06 10:39:17 -0500

    Added Adaptive WW Module to system services Changed BR2_PACKAGE_ADAPTVE_WW_REGPWRDB to default y
    Bug: 15970
    (cherry picked from commit 67141218cc06133196e66db857641a77ed958a31)

--- package/lrd-closed-source/adaptive_ww/adaptive_ww.mk
+++ package/lrd-closed-source/adaptive_ww/adaptive_ww.mk
@@ -46,6 +46,16 @@ define ADAPTIVE_WW_INSTALL_TARGET_CMDS
 	$(AWM_ADAPTIVE_WW_INSTALL_TARGET_CMDS)
 endef
 
+define ADAPTIVE_WW_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -m 0644 -D $(@D)/awm/awm.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/awm.service
+	ln -rsf $(TARGET_DIR)/usr/lib/systemd/system/awm.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/awm.service
+endef
+
+define ADAPTIVE_WW_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D $(@D)/awm/S30adaptive-ww $(TARGET_DIR)/etc/init.d/S30adaptive-ww
+endef
 
 
 #
