ffb57683036dc3ed5cf38ce3b568c1c713c8f3e2
Steve.Braneky <stephen.braneky@lairdconnect.com>
2020-02-11 14:51:50 -0500

    Fix for awm archive package missing services file.
    Bug: 15977
    (cherry picked from commit f510c89e147e711635dc4d5bb868babefba947d7)

--- package/lrd/lrd-som60-radio/adaptive_ww-binaries/adaptive_ww-binaries.mk
+++ package/lrd/lrd-som60-radio/adaptive_ww-binaries/adaptive_ww-binaries.mk
@@ -1,6 +1,5 @@
 ifneq ($(BR2_LRD_DEVEL_BUILD),y)
 ADAPTIVE_WW_BINARIES_VERSION = $(call qstrip,$(BR2_PACKAGE_LRD_RADIO_STACK_VERSION_VALUE))
-ADAPTIVE_WW_BINARIES_SOURCE =
 ADAPTIVE_WW_BINARIES_LICENSE = LGPL-2.1
 
 ifeq ($(BR2_PACKAGE_LIBOPENSSL_1_0_2),y)
@@ -9,7 +8,7 @@ else
 ADAPTIVE_WW_BINARIES_TYPE1 =
 endif
 
-ADAPTIVE_WW_BINARIES_EXTRA_DOWNLOADS = adaptive_ww$(ADAPTIVE_WW_BINARIES_TYPE1)-arm-eabihf-$(ADAPTIVE_WW_BINARIES_VERSION).tar.bz2
+ADAPTIVE_WW_BINARIES_SOURCE = adaptive_ww$(ADAPTIVE_WW_BINARIES_TYPE1)-arm-eabihf-$(ADAPTIVE_WW_BINARIES_VERSION).tar.bz2
 
 ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
   ADAPTIVE_WW_BINARIES_SITE = https://files.devops.rfpros.com/builds/linux/adaptive_ww/laird/$(ADAPTIVE_WW_BINARIES_VERSION)
@@ -18,7 +17,18 @@ else
 endif
 
 define ADAPTIVE_WW_BINARIES_INSTALL_TARGET_CMDS
-	tar -xjf $($(PKG)_DL_DIR)/$(ADAPTIVE_WW_BINARIES_EXTRA_DOWNLOADS) -C $(TARGET_DIR) --keep-directory-symlink --no-overwrite-dir --touch
+	$(INSTALL) -m 0755 -D $(@D)/usr/bin/adaptive_ww $(TARGET_DIR)/usr/bin
+endef
+
+define ADAPTIVE_WW_BINARIES_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -m 0644 -D $(@D)/usr/lib/systemd/system/awm.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/awm.service
+	ln -rsf $(TARGET_DIR)/usr/lib/systemd/system/awm.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/awm.service
+endef
+
+define ADAPTIVE_WW_BINARIES_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D $(@D)/etc/init.d/S30adaptive-ww $(TARGET_DIR)/etc/init.d/S30adaptive-ww
 endef
 
 endif
