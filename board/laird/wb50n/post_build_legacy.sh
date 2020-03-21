46b9a8a88d47f30920e62311bed405579acff0b7
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-03-21 05:58:16 -0400

    Fixed build errors
    Bug 16098

--- board/laird/wb50n/post_build_legacy.sh
+++ board/laird/wb50n/post_build_legacy.sh
@@ -1,42 +1,41 @@
-TARGETDIR=$1
+BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_CONFIG})"
+BOARD_DIR="$(realpath $(dirname $0))"
 
-export BR2_LRD_PLATFORM=wb50n
-
-echo "WB50n POST BUILD legacy script: starting..."
+echo "${BR2_LRD_PRODUCT^^} POST BUILD legacy script: starting..."
 
 # source the common post build legacy script
-source "board/laird/post_build_common_legacy.sh" "$TARGETDIR"
+source "board/laird/post_build_common_legacy.sh" "${TARGET_DIR}"
 
 # Copy the product specific rootfs additions
-tar c --exclude=.svn --exclude=.empty -C board/laird/wb50n/rootfs-additions/ . | tar x -C $TARGETDIR/
+rsync -rlptDWK --exclude=.empty "${BOARD_DIR}/rootfs-additions/" "${TARGET_DIR}"
 
 # create a compressed backup copy of the /e/n/i file
-gzip -c $TARGETDIR/etc/network/interfaces >$TARGETDIR/etc/network/interfaces~.gz
+gzip -c ${TARGET_DIR}/etc/network/interfaces >${TARGET_DIR}/etc/network/interfaces~.gz
 
 # Services to enable or disable by default
-chmod a+x $TARGETDIR/etc/init.d/S??lighttpd
+chmod a+x ${TARGET_DIR}/etc/init.d/S??lighttpd
 
 # adjust ssh_config and sshd_config to stop using root and use /etc/ instead.
-sed -i "s/AuthorizedKeysFile.*/AuthorizedKeysFile\t\/etc\/.ssh\/authorized_keys/" $TARGETDIR/etc/ssh/sshd_config
-echo "IdentityFile /etc/.ssh/identity" >> $TARGETDIR/etc/ssh/ssh_config
-echo "IdentityFile /etc/.ssh/id_rsa" >> $TARGETDIR/etc/ssh/ssh_config
-echo "IdentityFile /etc/.ssh/id_rsa" >> $TARGETDIR/etc/ssh/ssh_config
-echo "UserKnownHostsFile /etc/.ssh/known_hosts" >> $TARGETDIR/etc/ssh/ssh_config
+sed -i "s/AuthorizedKeysFile.*/AuthorizedKeysFile\t\/etc\/.ssh\/authorized_keys/" ${TARGET_DIR}/etc/ssh/sshd_config
+echo "IdentityFile /etc/.ssh/identity" >> ${TARGET_DIR}/etc/ssh/ssh_config
+echo "IdentityFile /etc/.ssh/id_rsa" >> ${TARGET_DIR}/etc/ssh/ssh_config
+echo "IdentityFile /etc/.ssh/id_rsa" >> ${TARGET_DIR}/etc/ssh/ssh_config
+echo "UserKnownHostsFile /etc/.ssh/known_hosts" >> ${TARGET_DIR}/etc/ssh/ssh_config
 
 # add SSH directorys in /etc/
-mkdir -p $TARGETDIR/etc/.ssh
-touch $TARGETDIR/etc/.ssh/authorized_keys
+mkdir -p ${TARGET_DIR}/etc/.ssh
+touch ${TARGET_DIR}/etc/.ssh/authorized_keys
 
 # make sure SSH permissions are correct
-chmod 700 $TARGETDIR/etc/.ssh
-chmod 600 $TARGETDIR/etc/.ssh/authorized_keys
+chmod 700 ${TARGET_DIR}/etc/.ssh
+chmod 600 ${TARGET_DIR}/etc/.ssh/authorized_keys
 
 # adjust DCAS SSH location
-sed -i "s/dcas_auth_dir.*/dcas_auth_dir=\/etc\/.ssh/" $TARGETDIR/etc/dcas.conf
-sed -i "s/DEFAULT_AUTH_DIR=.*/DEFAULT_AUTH_DIR=\/etc\/.ssh/" $TARGETDIR/etc/init.d/opt/S99dcas
+sed -i "s/dcas_auth_dir.*/dcas_auth_dir=\/etc\/.ssh/" ${TARGET_DIR}/etc/dcas.conf
+sed -i "s/DEFAULT_AUTH_DIR=.*/DEFAULT_AUTH_DIR=\/etc\/.ssh/" ${TARGET_DIR}/etc/init.d/opt/S99dcas
 
 # Fixup and add debugfs to fstab
-grep -q "/sys/kernel/debug" $TARGETDIR/etc/fstab ||\
-	echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> $TARGETDIR/etc/fstab
+grep -q "/sys/kernel/debug" ${TARGET_DIR}/etc/fstab ||\
+	echo 'nodev /sys/kernel/debug   debugfs   defaults   0  0' >> ${TARGET_DIR}/etc/fstab
 
-echo "WB50n POST BUILD script: done."
+echo "${BR2_LRD_PRODUCT^^} POST BUILD script: done."
