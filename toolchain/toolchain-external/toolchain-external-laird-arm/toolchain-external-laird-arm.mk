01e8630192da24d6f274784f347573372af14514
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-03-23 08:18:50 -0400

    Converted legacy and RDVK builds to latest toolchains
    - Added new eabi toolchain
    - Converted legacy and rdvk builds to use
    - Resolved discrepancies in RDVK configs
    
    Bug 16340

--- toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
+++ toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
@@ -5,7 +5,11 @@
 ################################################################################
 
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION = 7.0.0.276
+ifeq ($(BR2_ARM_EABIHF),y)
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_SOURCE = som60_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION).tar.gz
+else
+TOOLCHAIN_EXTERNAL_LAIRD_ARM_SOURCE = wb4x_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION).tar.gz
+endif
 
 ifeq ($(MSD_BINARIES_SOURCE_LOCATION),laird_internal)
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_SITE = https://files.devops.rfpros.com/builds/linux/toolchain/$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION)
