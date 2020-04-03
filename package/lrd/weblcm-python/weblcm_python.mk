fe46cbdcf37c7555180386aa1f7daa61c8cd7e77
sean.meng <Sean.Meng@lairdconnect.com>
2020-04-03 17:13:20 -0400

    weblcm-python: customize default user name and password
    Bug 15996

--- package/lrd/weblcm-python/weblcm_python.mk
+++ package/lrd/weblcm-python/weblcm_python.mk
@@ -2,7 +2,6 @@
 # Laird Web Configuration Utility
 #####################################################################
 
-WEBLCM_PYTHON_SET_KEY_LOCATION_VALUE = $(call qstrip,$(BR2_PACKAGE_WEBLCM_PYTHON_SWUPDATE_KEY_LOCATION))
 WEBLCM_PYTHON_VERSION = local
 WEBLCM_PYTHON_SITE = package/lrd/externals/weblcm-python
 WEBLCM_PYTHON_SITE_METHOD = local
@@ -11,6 +10,9 @@ WEBLCM_PYTHON_BUILD_OPTS = bdist_egg --exclude-source-files
 WEBLCM_PYTHON_DEPENDENCIES = lrd-swupdate-client
 
 TARGET_PYTHON_VERSION := $$(find $(TARGET_DIR)/usr/lib -maxdepth 1 -type d -name python* -printf "%f\n" | egrep -o '[0-9].[0-9]')
+WEBLCM_PYTHON_SET_KEY_LOCATION_VALUE = $(call qstrip,$(BR2_PACKAGE_WEBLCM_PYTHON_SWUPDATE_KEY_LOCATION))
+WEBLCM_PYTHON_DEFAULT_USERNAME = $(call qstrip,$(BR2_PACKAGE_WEBLCM_PYTHON_DEFAULT_USERNAME))
+WEBLCM_PYTHON_DEFAULT_PASSWORD = $(call qstrip,$(BR2_PACKAGE_WEBLCM_PYTHON_DEFAULT_PASSWORD))
 
 ifeq ($(BR2_REPRODUCIBLE),y)
 define WEBLCM_PYTHON_FIX_TIME
@@ -18,10 +20,15 @@ define WEBLCM_PYTHON_FIX_TIME
 endef
 endif
 
-define WEBLCM_PYTHON_SET_KEY_LOCATION
+define POST_INSTALL_TARGET_HOOKS
 	 ! grep -q 'CONFIG_SIGNED_IMAGES=y' ${BUILD_DIR}/swupdate*/include/config/auto.conf \
 		|| sed -i -e 's=swupdate=swupdate -k $(WEBLCM_PYTHON_SET_KEY_LOCATION_VALUE)=g' $(TARGET_DIR)/usr/sbin/swupdate.sh
+
+	sed -i -e '/^default_/d' $(TARGET_DIR)/etc/weblcm-python/weblcm-python.ini
+	sed -i -e '/\[weblcm\]/a default_username: \"$(WEBLCM_PYTHON_DEFAULT_USERNAME)\"\ndefault_password: \"$(WEBLCM_PYTHON_DEFAULT_PASSWORD)\"' $(TARGET_DIR)/etc/weblcm-python/weblcm-python.ini
+
 endef
+WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOKS += POST_INSTALL_TARGET_HOOKS
 
 define WEBLCM_PYTHON_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 755 $(@D)/dist/weblcm_python-1.0-py$(TARGET_PYTHON_VERSION).egg $(TARGET_DIR)/usr/bin/weblcm-python
@@ -38,7 +45,6 @@ define WEBLCM_PYTHON_INSTALL_TARGET_CMDS
 	cp -fr $(WEBLCM_PYTHON_SITE)/plugins $(TARGET_DIR)/var/www/
 
 	$(INSTALL) -D -t $(TARGET_DIR)/usr/sbin -m 755 $(WEBLCM_PYTHON_SITE)/swupdate.sh
-	$(WEBLCM_PYTHON_SET_KEY_LOCATION)
 	$(INSTALL) -D -t $(TARGET_DIR)/usr/sbin -m 755 $(WEBLCM_PYTHON_SITE)/weblcm_file_import_export.sh
 	$(INSTALL) -D -t $(TARGET_DIR)/usr/sbin -m 755 $(WEBLCM_PYTHON_SITE)/weblcm_update_checking.sh
 	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/*.ini
@@ -54,7 +60,6 @@ define WEBLCM_PYTHON_INSTALL_INIT_SYSTEMD
 	$(WEBLCM_PYTHON_FIX_TIME)
 
 	$(INSTALL) -D -t $(TARGET_DIR)/usr/lib/systemd/system -m 644 $(WEBLCM_PYTHON_SITE)/swupdate.service
-	ln -rsf $(TARGET_DIR)/usr/lib/systemd/system/swupdate.service $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/swupdate.service
 endef
 
 $(eval $(python-package))
