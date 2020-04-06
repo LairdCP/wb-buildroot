8a4854131359679e2069983068ac7bfd15751667
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-04-06 11:42:50 -0400

    buildroot: Added boot script in uboot and other changes
    - Added support for bootscript in uboot
    - Removed code that was creating image for the ubi partition
      but never produced anything correct or useful
    - Commonized scripts between som60 and wb50
    - Synced configs between projects
    - Added support for the customer x2 builds
    - Change to allow custom post_image to overwrite image scripts
    
    Bug 16273

--- board/laird/som60/post_build.sh
+++ board/laird/som60/post_build.sh
@@ -1,16 +1,8 @@
 BOARD_DIR="$(realpath $(dirname $0))"
+BUILD_TYPE="${2}"
+DEVEL_KEYS="${3}"
 
-export BR2_LRD_PLATFORM="${2}"
-
-echo "${BR2_LRD_PLATFORM^^} POST BUILD script: starting..."
-
-[[ ${BR2_LRD_PLATFORM} == *"sd" ]] || [[ ${BR2_LRD_PLATFORM} == *"sd_mfg" ]]
-SD=$?
-
-# source the common post build script
-. "${BOARD_DIR}/../post_build_common_60.sh" "${BOARD_DIR}"  ${SD}
+. "${BOARD_DIR}/../post_build_common_60.sh" "${BOARD_DIR}" "${BUILD_TYPE}" "${DEVEL_KEYS}"
 
 [ -f ${TARGET_DIR}/lib/firmware/regulatory_summit60.db ] && \
     ln -sfr ${TARGET_DIR}/lib/firmware/regulatory_summit60.db ${TARGET_DIR}/lib/firmware/regulatory.db
-
-echo "${BR2_LRD_PLATFORM^^} POST BUILD script: done."
