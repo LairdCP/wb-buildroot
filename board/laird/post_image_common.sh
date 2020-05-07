3b1d74ded9fbd7a80327ee85376613f5b5dd005e
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-05-07 10:53:06 -0400

    Miscelanious fixes for few issues
    - Removed unneeded config options
    - som60x2sd_mfg does not use correct dts
    - /etc/os_release does not use double quote multi word values
    - Store product name in the /etc/os_release
    - Use symlinks for legacy builds
    - Dereference symlinks during size check
    
    Bug 16609

--- board/laird/post_image_common.sh
+++ board/laird/post_image_common.sh
@@ -8,15 +8,15 @@ set -x -e
 BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_CONFIG})"
 
 if grep -qF "BR2_LINUX_KERNEL_APPENDED_DTB=y" ${BR2_CONFIG}; then
-cp "${BINARIES_DIR}/uImage."* "${BINARIES_DIR}/kernel.bin"
+ln -rsf "${BINARIES_DIR}/uImage."* "${BINARIES_DIR}/kernel.bin"
 else
-cp "${BINARIES_DIR}/uImage" "${BINARIES_DIR}/kernel.bin"
+ln -rsf "${BINARIES_DIR}/uImage" "${BINARIES_DIR}/kernel.bin"
 fi
-cp "${BINARIES_DIR}/rootfs.ubi" "${BINARIES_DIR}/rootfs.bin"
-cp "${BINARIES_DIR}/at91bootstrap.bin" "${BINARIES_DIR}/at91bs.bin"
+ln -rsf "${BINARIES_DIR}/rootfs.ubi" "${BINARIES_DIR}/rootfs.bin"
+ln -rsf "${BINARIES_DIR}/at91bootstrap.bin" "${BINARIES_DIR}/at91bs.bin"
 
-cp board/laird/rootfs-additions-common/usr/sbin/fw_select "${BINARIES_DIR}/"
-cp board/laird/rootfs-additions-common/usr/sbin/fw_update "${BINARIES_DIR}/"
+ln -rsf board/laird/rootfs-additions-common/usr/sbin/fw_select "${BINARIES_DIR}/fw_select"
+ln -rsf board/laird/rootfs-additions-common/usr/sbin/fw_update "${BINARIES_DIR}/fw_update"
 
 [ -z "${LAIRD_FW_TXT_URL}" ] && \
 	LAIRD_FW_TXT_URL="http://$(hostname)/${BR2_LRD_PRODUCT}"
@@ -28,7 +28,7 @@ ${TOPDIR}/board/laird/mkfwtxt.sh "${LAIRD_FW_TXT_URL}"
 ${TOPDIR}/board/laird/mkfwusi.sh
 
 size_check () {
-	[ $(stat -c "%s" ${BINARIES_DIR}/${1}) -le $((${2}*128*1024)) ] || \
+	[ $(stat -Lc "%s" ${BINARIES_DIR}/${1}) -le $((${2}*128*1024)) ] || \
 		{ echo "${1} size exceeded ${2} block limit, failed"; exit 1; }
 }
 
@@ -41,8 +41,8 @@ esac
 size_check 'kernel.bin' ${limit}
 size_check 'u-boot.bin' 3
 
-tar -cjf "${BR2_LRD_PRODUCT}-laird${RELEASE_SUFFIX}.tar.bz2" \
-	--owner=0 --group=0 --numeric-owner \
+tar -cjhf "${BR2_LRD_PRODUCT}-laird${RELEASE_SUFFIX}.tar.bz2" \
+	--owner=root --group=root \
 	at91bs.bin u-boot.bin kernel.bin rootfs.bin \
 	fw_update fw_select fw_usi fw.txt \
 	$(ls userfs.bin sqroot.bin prep_nand_for_update 2>/dev/null)
