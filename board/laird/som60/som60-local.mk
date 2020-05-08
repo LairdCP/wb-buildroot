881ce551205401be1bd968c9ae7d4051265eac89
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-05-07 20:33:45 -0400

    Fixed Sterling Supplicant compilation

--- board/laird/som60/som60-local.mk
+++ board/laird/som60/som60-local.mk
@@ -1,3 +1,4 @@
 LINUX_OVERRIDE_SRCDIR = package/lrd/externals/kernel
 UBOOT_OVERRIDE_SRCDIR = package/lrd/externals/u-boot-som60
 UBOOT_TOOLS_OVERRIDE_SRCDIR = package/lrd/externals/u-boot-som60
+WPA_SUPPLICANT_OVERRIDE_SRCDIR = package/lrd/externals/sterling_supplicant
