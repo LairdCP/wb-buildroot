25425e5bd989f580650834882273c650ea9ec9ad
sean.meng <Sean.Meng@lairdconnect.com>
2020-05-11 17:48:49 -0400

    Make firewalld configurable
    and let encrypted toolkit force select reset toolkit to better align
    NAND and SD images.
    
    Bug 16638

--- package/lrd/lrd-factory-reset-toolkit/rootfs/usr/sbin/do_factory_reset.sh
+++ package/lrd/lrd-factory-reset-toolkit/rootfs/usr/sbin/do_factory_reset.sh
@@ -6,9 +6,13 @@ NM_CONF_FILE=${NM_CONF_DIR}/NetworkManager.conf
 WEBLCM_CONF_DIR=/etc/weblcm-python
 WEBLCM_CONF_FILE=${WEBLCM_CONF_DIR}/weblcm-python.ini
 
+FIREWALLD_CONF_DIR=/etc/firewalld
+FIREWALLD_CONF_FILE=${FIREWALLD_CONF_DIR}/firewalld.conf
+
 FACTORY_SETTING_TARGET=/data/secret
 NM_CONF_FILE_TARGET=${FACTORY_SETTING_TARGET}/NetworkManager/NetworkManager.conf
 WEBLCM_CONF_FILE_TARGET=${FACTORY_SETTING_TARGET}/weblcm-python/weblcm-python.ini
+FIREWALLD_CONF_FILE_TARGET=${FACTORY_SETTING_TARGET}/firewalld/firewalld.conf
 
 exit_on_error() {
 	echo "${1}"
@@ -19,9 +23,16 @@ do_check_and_reset() {
 
 	mkdir -p ${FACTORY_SETTING_TARGET} || exit_on_error "Creating target dir.. Failed"
 
-	if [ ! -f "${NM_CONF_FILE_TARGET}" ] || [ ! -f "${WEBLCM_CONF_FILE_TARGET}" ]; then
+	if [ ! -f "${NM_CONF_FILE_TARGET}" ] && [ -f "${NM_CONF_FILE}" ]; then
 		cp -fa ${NM_CONF_DIR} ${FACTORY_SETTING_TARGET}/ || exit_on_error "Copying NetworkManager data.. Failed"
+	fi
+
+	if [ ! -f "${WEBLCM_CONF_FILE_TARGET}" ] && [ -f "${WEBLCM_CONF_FILE}" ]; then
 		cp -fa ${WEBLCM_CONF_DIR} ${FACTORY_SETTING_TARGET}/ || exit_on_error "Copying weblcm-python data.. Failed"
+	fi
+
+	if [ ! -f "${FIREWALLD_CONF_FILE_TARGET}" ] && [ -f "${FIREWALLD_CONF_FILE}" ]; then
+		cp -fa ${FIREWALLD_CONF_DIR} ${FACTORY_SETTING_TARGET}/ || exit_on_error "Copying firewalld data.. Failed"
 	fi
 }
 
