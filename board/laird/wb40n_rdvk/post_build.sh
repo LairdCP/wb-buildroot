2de2f843b73afa07fcbcbbda49a70c7c4cbac24d
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-05-19 12:20:57 -0400

    Fixed build wb40_rdvk with kernel 3.2 on lrd-8
    Bug 14693

--- board/laird/wb40n_rdvk/post_build.sh
+++ board/laird/wb40n_rdvk/post_build.sh
@@ -3,16 +3,16 @@ BOARD_DIR="$(realpath $(dirname $0))"
 
 echo "${BR2_LRD_PRODUCT^^} POST BUILD script: starting..."
 
-# source the common post build script
-source "board/laird/post_build_common.sh" "${TARGET_DIR}"
-
-rm -f ${TARGET_DIR}/etc/init.d/S95bluetooth.bg
-rm -f ${TARGET_DIR}/etc/init.d/S40wifi
-rm -f ${TARGET_DIR}/etc/init.d/S99lighttpd
-rm -f ${TARGET_DIR}/etc/init.d/S03cryptodev
-
 # Copy the product specific rootfs additions
 rsync -rlptDWK --exclude=.empty "${BOARD_DIR}/rootfs-additions/" "${TARGET_DIR}"
+cp "${BOARD_DIR}/../rootfs-additions-common/usr/sbin/fw_"* "${TARGET_DIR}/usr/sbin"
+
+if grep -q 'BR2_LINUX_KERNEL_VERSION="3.2.102"' ${BR2_CONFIG}; then
+	# On pre pinctrl kernels GPIO number is +32
+	sed 's/reset_pwd_gpio=24/reset_pwd_gpio=56/' -i ${TARGET_DIR}/etc/modprobe.d/lrdmwl.conf
+	sed 's/ 24 / 56 /' -i ${TARGET_DIR}/root/*.sh
+	sed 's/ 23 / 55 /' -i ${TARGET_DIR}/root/*.sh
+fi
 
 # Fixup and add debugfs to fstab
 grep -q "/sys/kernel/debug" ${TARGET_DIR}/etc/fstab ||\
