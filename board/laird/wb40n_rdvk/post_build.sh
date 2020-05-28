64e8f8b77009445ed1a28d71861478b21625ed45
EongLoon.Chew <EongLoon.Chew@lairdconnect.com>
2020-05-28 11:08:46 +0800

    Remove generation of /etc/laird-release
    /etc/laird-release is depricated.
    
    bug 16704

--- board/laird/wb40n_rdvk/post_build.sh
+++ board/laird/wb40n_rdvk/post_build.sh
@@ -7,7 +7,6 @@ LOCRELSTR="${LAIRD_RELEASE_STRING}"
 if [ -z "${LOCRELSTR}" ] || [ "${LOCRELSTR}" == "0.0.0.0" ]; then
 	LOCRELSTR="Summit Linux development build 0.${BR2_LRD_BRANCH}.0.0 $(date +%Y%m%d)"
 fi
-echo "${LOCRELSTR}" > "${TARGET_DIR}/etc/laird-release"
 echo "${LOCRELSTR}" > "${TARGET_DIR}/etc/issue"
 
 [ -z "${VERSION}" ] && LOCVER="0.${BR2_LRD_BRANCH}.0.0" || LOCVER="${VERSION}"
