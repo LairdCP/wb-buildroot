479ffad01b5b58b9f852cd59c24ab560dd813550
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-06-29 15:08:50 -0400

    Removed uneeded operation during wbx3 image build
    Bug 16611

--- board/laird/wbx3/post_image.sh
+++ board/laird/wbx3/post_image.sh
@@ -5,14 +5,11 @@ BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_C
 
 echo "${BR2_LRD_PRODUCT^^} POST IMAGE script: starting..."
 
-# Then update SPL with appended keyed DTB
-cat "${BINARIES_DIR}/u-boot-spl-nodtb.bin" "${BINARIES_DIR}/u-boot-spl.dtb" > "${BINARIES_DIR}/u-boot-spl.bin"
-
 # wbx3 sd card loading tools
 ln -rsf board/laird/scripts-common/mksdcard-wbx3.sh ${BINARIES_DIR}/mksdcard.sh
 ln -rsf board/laird/scripts-common/mksdimg-wbx3.sh ${BINARIES_DIR}/mksdimg.sh
 
-[ -n "${VERSION}" ] && RELEASE_SUFFIX="-${VERSION}"
+[ -z "${VERSION}" ] || RELEASE_SUFFIX="-${VERSION}"
 
 tar -C ${BINARIES_DIR} \
 	-chjf ${BINARIES_DIR}/${BR2_LRD_PRODUCT}-laird${RELEASE_SUFFIX}.tar.bz2 \
