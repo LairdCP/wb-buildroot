5027e2e506b2d2c7b44ee49ce2e587ae2dfd35e2
Steve.Braneky <stephen.braneky@lairdconnect.com>
2020-07-21 13:48:55 -0400

    Combine LRU for the 45 and LRU 50 into a single source base
    Bug: 17035

--- package/lrd-closed-source/lru_ath6kl/lru_ath6kl.mk
+++ package/lrd-closed-source/lru_ath6kl/lru_ath6kl.mk
@@ -8,31 +8,27 @@ LRU_ATH6KL_SITE    = package/lrd-closed-source/externals/lru_ath6kl
 LRU_ATH6KL_SITE_METHOD = local
 LRU_ATH6KL_DEPENDENCIES = libnl host-pkgconf libedit
 
-MY_MAKE_OPTS = CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR=$(TARGET_AR) LD="$(TARGET_LD)" PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config"
-
 ifeq ($(BR2_PACKAGE_LRU_ATH6KL_WB50N_SUPPORT),y)
 LRU_ATH6KL_SUFFIX = 50
 else
 LRU_ATH6KL_SUFFIX = 45
 endif
 
-ifeq ($(BR2_PACKAGE_LRU_ATH6KL_TEST_MODE),y)
-MY_MAKE_OPTS += LRU_ATH6KL_TEST_MODE="-DLRU_TESTMODE"
-endif
+MY_MAKE_OPTS = CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR=$(TARGET_AR) LD="$(TARGET_LD)" PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config"
 
 #
 # BUILD
 #
 define LRU_ATH6KL_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(MY_MAKE_OPTS) -C $(@D)/Proprietary_tools/libtcmd
-	$(TARGET_MAKE_ENV) $(MAKE) $(MY_MAKE_OPTS) -C $(@D)/Proprietary_tools/lru_$(LRU_ATH6KL_SUFFIX)
+	$(TARGET_MAKE_ENV) $(MY_MAKE_OPTS) $(MAKE)  -C $(@D)/Proprietary_tools/libtcmd
+	$(TARGET_MAKE_ENV) $(MY_MAKE_OPTS) $(MAKE)  -C $(@D)/Proprietary_tools/lru_50  wb$(LRU_ATH6KL_SUFFIX)
 endef
 
 #
 #Install
 #
 define LRU_ATH6KL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/Proprietary_tools/lru_$(LRU_ATH6KL_SUFFIX)/lru_$(LRU_ATH6KL_SUFFIX) $(TARGET_DIR)/usr/bin/lru
+	$(INSTALL) -D -m 755 $(@D)/Proprietary_tools/lru_50/lru_$(LRU_ATH6KL_SUFFIX) $(TARGET_DIR)/usr/bin/lru
 endef
 
 
