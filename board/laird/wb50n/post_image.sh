b48784fd528026eb0c5cf02dfb009cbf7a49694f
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2020-07-28 17:34:30 -0400

    Fixed wb45n and wb50n legacy builds
    Bug 16184

--- board/laird/wb50n/post_image.sh
+++ board/laird/wb50n/post_image.sh
@@ -7,14 +7,6 @@ echo "${BR2_LRD_PLATFORM^^} POST IMAGE script: starting..."
 # enable tracing and exit on errors
 set -x -e
 
-# Ubinize the squash filesystems
-(
-cd ${BINARIES_DIR}
-${HOST_DIR}/sbin/ubinize -o sqroot.ubi -m 0x800 -p 0x20000 \
-	${BOARD_DIR}/squashfs-ubinize.cfg
-cp sqroot.ubi sqroot.bin
-)
-
 # source the common post image script
 . "${BOARD_DIR}/../post_image_common.sh" "${BINARIES_DIR}"
 
