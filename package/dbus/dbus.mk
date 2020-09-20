7e4398e327417e38c8d94149ff9effe12cfd83ed
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-09-20 12:50:04 +0200

    package/dbus: drop unrecognized option
    --with-xml option has been dropped seven years ago in version 1.7.4 and
    https://github.com/freedesktop/dbus/commit/46602768c537bfa51af80ba2c67857f3a30c0e67
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/dbus/dbus.mk
+++ package/dbus/dbus.mk
@@ -26,7 +26,6 @@ DBUS_CONF_OPTS = \
 	--disable-asserts \
 	--disable-xml-docs \
 	--disable-doxygen-docs \
-	--with-xml=expat \
 	--with-system-socket=/run/dbus/system_bus_socket \
 	--with-system-pid-file=/run/messagebus.pid
 
@@ -108,8 +107,7 @@ HOST_DBUS_CONF_OPTS = \
 	--disable-xml-docs \
 	--disable-doxygen-docs \
 	--disable-systemd \
-	--without-x \
-	--with-xml=expat
+	--without-x
 
 # dbus for the host
 DBUS_HOST_INTROSPECT = $(HOST_DBUS_DIR)/introspect.xml
