f5d51a0a9e540bfc911ae7216729c86325d9f8d6
Victor Yeo <victor.yeo@lairdconnect.com>
2020-11-06 10:03:26 +0800

    Remove wb50n_sysd_rdvk from master, lrd-7.0.0.x, and lrd-8.0.0.x
    Bug 17599

--- board/laird/wb50n_sysd_rdvk/post_build.sh
+++
@@ -1,9 +0,0 @@
-WB50N_SYSD_RDVK_DIR="$(realpath $(dirname $0))"
-WB50N_SYSD_DIR="$(realpath ${WB50N_SYSD_RDVK_DIR}/../wb50n_sysd)"
-
-BUILD_TYPE="${2}"
-DEVEL_KEYS="${3}"
-
-. "${WB50N_SYSD_RDVK_DIR}/../post_build_common_60.sh" "${WB50N_SYSD_DIR}" "${BUILD_TYPE}" "${DEVEL_KEYS}"
-
-rsync -rlptDWK --no-perms --exclude=.empty "${WB50N_SYSD_RDVK_DIR}/rootfs-extra/" "${TARGET_DIR}"
