a6f6a85a7f633dd53346eddfb7e81777642d3f4e
Victor Yeo <victor.yeo@lairdconnect.com>
2020-11-06 11:59:18 +0800

    Create a build target that has sdc_cli and sdcsdk_nm wrapper on the 60 SOM
    Bug 16000

--- package/lrd-closed-source/sdccli/sdccli.mk
+++ package/lrd-closed-source/sdccli/sdccli.mk
@@ -20,25 +20,47 @@ SDCCLI_MAKE_ENV += CC="$(TARGET_CC)" \
                   ARCH="$(KERNEL_ARCH)" \
                   CFLAGS="$(TARGET_CFLAGS)"
 
-define SDCCLI_BUILD_CMDS
-    $(MAKE) -C $(@D) clean
-	$(SDCCLI_MAKE_ENV) $(MAKE) -C $(@D)
+ifeq ($(BR2_PACKAGE_SDCCLI_SDC_CLI),y)
+define BUILD_SDC_CLI_CMD
+	$(SDCCLI_MAKE_ENV) $(MAKE) -C $(@D) sdc_cli
 endef
 
-ifeq ($(BR2_PACKAGE_SDCCLI_SMU_CLI),y)
-define SDCCLI_INSTALL_SMU_CLI
-	$(INSTALL) -D -m 755 $(@D)/bin/smu_cli $(TARGET_DIR)/usr/sbin/smu_cli
+define INSTALL_SDC_CLI_CMD
+	$(INSTALL) -D -m 755 $(@D)/bin/sdc_cli $(TARGET_DIR)/usr/bin/sdc_cli
+endef
+
+define UNINSTALL_SDC_CLI_CMD
+	rm -f $(TARGET_DIR)/usr/bin/sdc_cli
 endef
 endif
 
+ifeq ($(BR2_PACKAGE_SDCCLI_SMU_CLI),y)
+define BUILD_SMU_CLI_CMD
+	$(SDCCLI_MAKE_ENV) $(MAKE) -C $(@D) smu_cli
+endef
+
+define INSTALL_SDC_SMU_CMD
+	$(INSTALL) -D -m 755 $(@D)/bin/smu_cli $(TARGET_DIR)/usr/sbin/smu_cli
+endef
+
+define UNINSTALL_SDC_SMU_CMD
+	rm -f $(TARGET_DIR)/usr/sbin/smu_cli
+endef
+endif
+
+define SDCCLI_BUILD_CMDS
+	$(BUILD_SDC_CLI_CMD)
+	$(BUILD_SMU_CLI_CMD)
+endef
+
 define SDCCLI_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/bin/sdc_cli $(TARGET_DIR)/usr/bin/sdc_cli
-	$(SDCCLI_INSTALL_SMU_CLI)
+	$(INSTALL_SDC_CLI_CMD)
+	$(INSTALL_SDC_SMU_CMD)
 endef
 
 define SDCCLI_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/sdc_cli
-	rm -f $(TARGET_DIR)/usr/sbin/smu_cli
+	$(UNINSTALL_SDC_CLI_CMD)
+	$(UNINSTALL_SDC_SMU_CMD)
 endef
 
 $(eval $(generic-package))
