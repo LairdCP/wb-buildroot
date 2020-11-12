3769d552d418c0c35b50599ab23b24dedcf2e148
Joe Conley <joe.conley@lairdconnect.com>
2020-11-12 09:54:51 -0500

    laird: weblcm-python: Install all scripts to /etc/weblcm-python/scripts instead of /sbin
    bug 12537

--- package/lrd/weblcm-python/weblcm_python.mk
+++ package/lrd/weblcm-python/weblcm_python.mk
@@ -56,7 +56,7 @@ define WEBLCM_PYTHON_INSTALL_TARGET_CMDS
 
 	cp -fr $(WEBLCM_PYTHON_SITE)/plugins $(TARGET_DIR)/var/www/
 
-	$(INSTALL) -D -t $(TARGET_DIR)/usr/sbin -m 755 $(WEBLCM_PYTHON_SITE)/*.sh
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/scripts -m 755 $(WEBLCM_PYTHON_SITE)/*.sh
 	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/*.ini
 	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/zonelist.db
 	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/server.key
