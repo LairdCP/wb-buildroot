dc133e607d8bd8c042c5a550e23d89296d7b3c0a
sean.meng <Sean.Meng@lairdconnect.com>
2020-11-16 11:37:46 -0500

    Remove rsync from dependency list
    from lrd factory reset and lrd encyprted toolkit packages.
    
    Also don't preserve modification times when doing rsync
    
    17752

--- package/lrd/lrd-encrypted-storage-toolkit/lrd-encrypted-storage-toolkit.mk
+++ package/lrd/lrd-encrypted-storage-toolkit/lrd-encrypted-storage-toolkit.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 define LRD_ENCRYPTED_STORAGE_TOOLKIT_INSTALL_TARGET_CMDS
-	rsync -rlptDWK package/lrd/lrd-encrypted-storage-toolkit/rootfs/ $(TARGET_DIR)/
+	rsync -rlpDWK package/lrd/lrd-encrypted-storage-toolkit/rootfs/ $(TARGET_DIR)/
 endef
 
 $(eval $(generic-package))
