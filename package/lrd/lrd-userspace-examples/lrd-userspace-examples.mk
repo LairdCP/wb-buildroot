2455b4391a85e8eb8567c83f164668a889566fa1
sean.meng <Sean.Meng@lairdconnect.com>
2021-01-22 23:03:59 -0500

    Add new package cmux
    Bug 18122

--- package/lrd/lrd-userspace-examples/lrd-userspace-examples.mk
+++ package/lrd/lrd-userspace-examples/lrd-userspace-examples.mk
@@ -16,11 +16,22 @@ LRD_USERSPACE_EXAMPLES_AUTORECONF = YES
 LRD_USERSPACE_EXAMPLES_CONF_OPTS =
 
 ifeq ($(BR2_PACKAGE_LRD_USERSPACE_NETWORKMANAGER_EXAMPLES),y)
-	LRD_USERSPACE_EXAMPLES_CONF_OPTS = --enable-nm-examples
+	LRD_USERSPACE_EXAMPLES_CONF_OPTS += --enable-nm-examples
 endif
 
 ifeq ($(BR2_PACKAGE_LRD_USERSPACE_NETLINK_EXAMPLES),y)
-	LRD_USERSPACE_EXAMPLES_CONF_OPTS = --enable-nl-examples
+	LRD_USERSPACE_EXAMPLES_CONF_OPTS += --enable-nl-examples
+endif
+
+ifeq ($(BR2_PACKAGE_LRD_USERSPACE_CMUX),y)
+	LRD_USERSPACE_EXAMPLES_CONF_OPTS += --enable-cmux
+define LRD_USERSPACE_EXAMPLES_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 package/lrd/lrd-userspace-examples/cmux.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/cmux.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -rsf $(TARGET_DIR)/usr/lib/systemd/system/cmux.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/cmux.service
+endef
 endif
 
 $(eval $(autotools-package))
