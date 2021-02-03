d615d6bf16f174bcea823b501fd929f070a24498
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2021-02-03 10:27:32 -0500

    buildroot: Added  ACVP Parser for the FIPS certification
    Bug 18225

---
+++ package/lrd-closed-source/parser-acvp/parser-acvp.mk
@@ -0,0 +1,35 @@
+#############################################################
+#
+#  PARSER ACVP
+#
+#############################################################
+
+PARSER_ACVP_VERSION = local
+PARSER_ACVP_SITE    = package/lrd-closed-source/externals/cavs_api/acvpparser
+PARSER_ACVP_SITE_METHOD = local
+PARSER_ACVP_DEPENDENCIES = libgcrypt libgpg-error openssl keyutils
+
+PARSER_ACVP_MAKE_ENV = \
+	$(TARGET_MAKE_ENV) \
+	CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include" \
+
+PARSER_ACVP_MAKE_ENV2 = \
+	CC="$(TARGET_CC)" \
+	OS=buildroot \
+	UNAME_S=Linux
+
+define PARSER_ACVP_BUILD_CMDS
+	$(MAKE) -C $(@D) clean
+	$(PARSER_ACVP_MAKE_ENV) $(MAKE) -C $(@D) kcapi $(PARSER_ACVP_MAKE_ENV2)
+	$(MAKE) -C $(@D) clean
+	$(PARSER_ACVP_MAKE_ENV) $(MAKE) -C $(@D) openssl $(PARSER_ACVP_MAKE_ENV2)
+endef
+
+define PARSER_ACVP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/acvp-parser-kcapi $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 755 $(@D)/acvp-parser-openssl $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 755 $(@D)/exec_kcapi.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 755 $(@D)/exec_openssl.sh $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(generic-package))
