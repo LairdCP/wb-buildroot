bb267409272bfc339a6c1c0212905d7ba2774e42
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2021-04-07 12:58:08 -0400

    Updated glibc to resolve CVE-2021-27645
    Bug 17520

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -13,7 +13,7 @@ GLIBC_SITE = $(call github,riscv,riscv-glibc,$(GLIBC_VERSION))
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master
-GLIBC_VERSION = glibc-2.28-138-gf82072183ad5b328f6a7cb91868cb1709e85d96c
+GLIBC_VERSION = glibc-2.28-151-g4352eb4e5d3e8e53497250b1929db731143abe16
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
