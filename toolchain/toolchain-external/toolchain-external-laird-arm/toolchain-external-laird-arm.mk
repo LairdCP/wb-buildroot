e02354280d3ef08a392f44699ffc991fc2535eb1
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2021-04-08 18:13:43 -0400

    Updated toolchain to fix CVE-2021-27645
    Bug 17520

--- toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
+++ toolchain/toolchain-external/toolchain-external-laird-arm/toolchain-external-laird-arm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION = 7.0.0.430
+TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION = 7.0.0.518
 ifeq ($(BR2_ARM_EABIHF),y)
 TOOLCHAIN_EXTERNAL_LAIRD_ARM_SOURCE = som60_toolchain-laird-$(TOOLCHAIN_EXTERNAL_LAIRD_ARM_VERSION).tar.gz
 else
