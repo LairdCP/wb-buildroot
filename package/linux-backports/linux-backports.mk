a621a3e865ee68240d48ef269a99461bbd6f5bef
Dean Ramsier <dean.ramsier@lairdconnect.com>
2021-08-26 15:55:59 -0400

    Build failure when specifying backports version number
    Bug: 19647
    (cherry picked from commit 9e987f78a955f7bd5872c80a456e9115e496b7ac)

--- package/linux-backports/linux-backports.mk
+++ package/linux-backports/linux-backports.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 
-ifneq ($(BR2_PACKAGE_LINUX_BACKPORTS_VERSION),)
-LINUX_BACKPORTS_VERSION = $(call qstrip,$(BR2_PACKAGE_LINUX_BACKPORTS_VERSION))
+ifneq ($(BR2_PACKAGE_LINUX_BACKPORTS_VERSION_VALUE),)
+LINUX_BACKPORTS_VERSION = $(call qstrip,$(BR2_PACKAGE_LINUX_BACKPORTS_VERSION_VALUE))
 else ifneq ($(BR2_LRD_DEVEL_BUILD),)
 LINUX_BACKPORTS_VERSION = 0.$(BR2_LRD_BRANCH).0.0
 else
