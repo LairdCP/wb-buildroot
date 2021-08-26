eba61ae629c0b7fa1b0b1564526f62c7caafd5a1
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2021-08-26 12:02:37 -0700

    buildroot: Fixed SOM60x2 RDVK build as functionality is in common
    Bug 14020

--- board/laird/som60x2_rdvk/post_build.sh
+++ board/laird/som60x2_rdvk/post_build.sh
@@ -1,18 +1,18 @@
-BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_CONFIG})"
 SOM60X2_RDVK_DIR="$(realpath $(dirname $0))"
 SOM60_DIR="$(realpath ${SOM60X2_RDVK_DIR}/../som60)"
+
 BUILD_TYPE="${2}"
 DEVEL_KEYS="${3}"
+
+BR2_LRD_PRODUCT="$(sed -n 's,^BR2_DEFCONFIG=".*/\(.*\)_defconfig"$,\1,p' ${BR2_CONFIG})"
 
 echo "${BR2_LRD_PRODUCT^^} POST BUILD script: starting..."
 
 . "${SOM60X2_RDVK_DIR}/../post_build_common_60.sh" "${SOM60_DIR}" "${BUILD_TYPE}" "${DEVEL_KEYS}"
 
-rsync -rlptDWK --exclude=.empty "${SOM60X2_RDVK_DIR}/rootfs-extra/" "${TARGET_DIR}"
+rsync -rlptDWK --no-perms --exclude=.empty "${SOM60X2_RDVK_DIR}/rootfs-additions/" "${TARGET_DIR}"
 
-rm ${TARGET_DIR}/usr/lib/systemd/system/btattach.service
-rm ${TARGET_DIR}/etc/ts.conf ${TARGET_DIR}/etc/pointercal
-rm ${TARGET_DIR}/etc/udev/rules.d/80-btattach.rules
-rm ${TARGET_DIR}/etc/profile.d/ts-setup.sh
+rm -f ${TARGET_DIR}/usr/lib/systemd/system/btattach.service
+rm -f ${TARGET_DIR}/etc/udev/rules.d/80-btattach.rules
 
 echo "${BR2_LRD_PRODUCT^^} POST BUILD script: done."
