99fe2faa12d8ded26d2d4254661814288248b688
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2021-11-12 10:50:19 -0700

    buildroot: Changed to the Summit Linux 7.1 FIPS certification
    Bug 16184

---
+++ board/laird/wb45n/wb45n_fips-local.mk
@@ -0,0 +1,4 @@
+LINUX_OVERRIDE_SRCDIR = package/lrd/externals/kernel_fips
+AT91BOOTSTRAP3_OVERRIDE_SRCDIR = package/lrd/externals/at91bootstrap3
+UBOOT_OVERRIDE_SRCDIR = package/lrd/externals/u-boot-som60
+UBOOT_TOOLS_OVERRIDE_SRCDIR = package/lrd/externals/u-boot-som60
