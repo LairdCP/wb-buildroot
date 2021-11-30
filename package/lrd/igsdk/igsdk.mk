87cc4374ac5e063a42afb4ce33f0fb6e3084bc98
Erik Strack <erik.strack@lairdconnect.com>
2021-11-30 18:55:40 -0500

    weblcm-python: Add BLE API based upon ig sdk's BLE Lambdas.
    Bug 20528

---
+++ package/lrd/igsdk/igsdk.mk
@@ -0,0 +1,27 @@
+#####################################################################
+# Laird Industrial Gateway SDK
+#####################################################################
+
+IGSDK_VERSION = local
+IGSDK_SITE = package/lrd/externals/igsdk
+IGSDK_SITE_METHOD = local
+
+ifeq ($(BR2_REPRODUCIBLE),y)
+define IGSDK_PYTHON3_FIX_TIME
+	find $(TARGET_DIR)/usr/lib -name '*.py' -print0 | \
+		xargs -0 --no-run-if-empty touch -d @$(SOURCE_DATE_EPOCH)
+endef
+endif
+
+define IGSDK_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/python/igsdk/bt_module.py $(TARGET_DIR)/usr/lib
+
+	$(IGSDK_PYTHON3_FIX_TIME)
+	PYTHONPATH="$(PYTHON3_PATH)" \
+	cd $(TARGET_DIR) && $(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR) \
+		$(TOPDIR)/support/scripts/pycompile.py \
+		$(if $(BR2_REPRODUCIBLE),--force) \
+		usr/lib && rm -f $(TARGET_DIR)/usr/lib/bt_module.py
+endef
+
+$(eval $(generic-package))
