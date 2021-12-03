9f13ba18ec98af992302d01810081044429a3700
Erik Strack <erik.strack@lairdconnect.com>
2021-12-02 23:33:35 -0500

    weblcm-python: Install igsdk python files to /usr/lib/igsdk/, limiting python compilation scope.
    Bug 20528

--- package/lrd/igsdk/igsdk.mk
+++ package/lrd/igsdk/igsdk.mk
@@ -14,14 +14,14 @@ endef
 endif
 
 define IGSDK_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/python/igsdk/bt_module.py $(TARGET_DIR)/usr/lib
+	$(INSTALL) -D -m 755 $(@D)/python/igsdk/bt_module.py $(TARGET_DIR)/usr/lib/igsdk/bt_module.py
 
 	$(IGSDK_PYTHON3_FIX_TIME)
 	PYTHONPATH="$(PYTHON3_PATH)" \
 	cd $(TARGET_DIR) && $(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR) \
 		$(TOPDIR)/support/scripts/pycompile.py \
 		$(if $(BR2_REPRODUCIBLE),--force) \
-		usr/lib && rm -f $(TARGET_DIR)/usr/lib/bt_module.py
+		usr/lib/igsdk && rm -f $(TARGET_DIR)/usr/lib/igsdk/*.py
 endef
 
 $(eval $(generic-package))
