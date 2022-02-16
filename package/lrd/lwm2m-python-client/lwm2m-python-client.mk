ad1665d7a44bab2c6aec16d383794d0b98ae5253
Don Ferencz <Donald.Ferencz@lairdconnect.com>
2022-02-16 13:58:22 +0000

    Remove specific Python version from LwM2M client install
    Bug 21061

--- package/lrd/lwm2m-python-client/lwm2m-python-client.mk
+++ package/lrd/lwm2m-python-client/lwm2m-python-client.mk
@@ -8,10 +8,8 @@ LWM2M_PYTHON_CLIENT_SITE_METHOD = local
 LWM2M_PYTHON_CLIENT_SETUP_TYPE = setuptools
 LWM2M_PYTHON_CLIENT_BUILD_OPTS = bdist_egg --exclude-source-files
 
-LWM2M_PYTHON_CLIENT_PYTHON_VERSION := 3.7
-
 define LWM2M_PYTHON_CLIENT_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 755 $(@D)/dist/lwm2m_python_client-$(LWM2M_PYTHON_CLIENT_VERSION)-py$(LWM2M_PYTHON_CLIENT_PYTHON_VERSION).egg $(TARGET_DIR)/usr/bin/lwm2m-python-client
+        $(INSTALL) -D -m 755 $(@D)/dist/lwm2m_python_client-$(LWM2M_PYTHON_CLIENT_VERSION)-py*.egg $(TARGET_DIR)/usr/bin/lwm2m-python-client
         $(INSTALL) -D -m 755 $(@D)/ig60_fw_update.sh $(TARGET_DIR)/usr/bin/ig60_fw_update.sh
 endef
 
