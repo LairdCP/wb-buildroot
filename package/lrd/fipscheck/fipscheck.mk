9ce0c9a7524286da4cad218de1d4856e806a9c2a
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-04-05 12:25:33 -0400

    buildroot: fipscheck: modifications to make hash match to lrd-7
    Bug 21274

--- package/lrd/fipscheck/fipscheck.mk
+++ package/lrd/fipscheck/fipscheck.mk
@@ -13,6 +13,13 @@ FIPSCHECK_LICENSE_FILES = COPYING
 FIPSCHECK_DEPENDENCIES += openssl host-pkgconf
 FIPSCHECK_AUTORECONF = YES
 
+ifeq ($(BR2_TOOLCHAIN_EXTERNAL_LAIRD_ARM_7),y)
+# this is need to prevent enabling code hardenning options, that were not enabled
+# on lrd-7 builds and not included in hash calculation
+FIPSCHECK_CONF_ENV = CFLAGS+=" -fno-stack-protector -U_FORTIFY_SOURCE -D__UBOOT__"
+FIPSCHECK_MAKE_OPTS = LDFLAGS+="-XCClinker -D__UBOOT__ "
+endif
+
 HOST_FIPSCHECK_DEPENDENCIES += host-openssl host-pkgconf
 HOST_FIPSCHECK_AUTORECONF = YES
 
