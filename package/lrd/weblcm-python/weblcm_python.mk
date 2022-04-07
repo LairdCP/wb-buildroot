d71c301667ace0eefacb55c3c453688d80f4d6ce
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-04-07 13:34:39 -0400

    buildroot: weblcm-python: Converted to standard python install
    Bug 21416

--- package/lrd/weblcm-python/weblcm_python.mk
+++ package/lrd/weblcm-python/weblcm_python.mk
@@ -6,7 +6,6 @@ WEBLCM_PYTHON_VERSION = local
 WEBLCM_PYTHON_SITE = package/lrd/externals/weblcm-python
 WEBLCM_PYTHON_SITE_METHOD = local
 WEBLCM_PYTHON_SETUP_TYPE = setuptools
-WEBLCM_PYTHON_BUILD_OPTS = bdist_egg --exclude-source-files
 WEBLCM_PYTHON_DEPENDENCIES = lrd-swupdate-client
 
 TARGET_PYTHON_VERSION := $$(find $(TARGET_DIR)/usr/lib -maxdepth 1 -type d -name python* -printf "%f\n" | egrep -o '[0-9].[0-9]')
@@ -15,15 +14,16 @@ WEBLCM_PYTHON_DEFAULT_USERNAME = $(call qstrip,$(BR2_PACKAGE_WEBLCM_PYTHON_DEFAU
 WEBLCM_PYTHON_DEFAULT_PASSWORD = $(call qstrip,$(BR2_PACKAGE_WEBLCM_PYTHON_DEFAULT_PASSWORD))
 
 ifeq ($(BR2_PACKAGE_WEBLCM_PYTHON_BLUETOOTH),y)
-	WEBLCM_PYTHON_EXTRA_MODULES += WEBLCM_PYTHON_BLUETOOTH
+	WEBLCM_PYTHON_EXTRA_PACKAGES += weblcm/bluetooth
 endif
 ifeq ($(BR2_PACKAGE_WEBLCM_PYTHON_HID),y)
-	WEBLCM_PYTHON_EXTRA_MODULES += WEBLCM_PYTHON_HID
+	WEBLCM_PYTHON_EXTRA_PACKAGES += weblcm/hid
 endif
 ifeq ($(BR2_PACKAGE_WEBLCM_PYTHON_VSP),y)
-	WEBLCM_PYTHON_EXTRA_MODULES += WEBLCM_PYTHON_VSP
+	WEBLCM_PYTHON_EXTRA_PACKAGES += weblcm/vsp
 endif
-WEBLCM_PYTHON_ENV = WEBLCM_PYTHON_EXTRA_MODULES='$(WEBLCM_PYTHON_EXTRA_MODULES)'
+
+WEBLCM_PYTHON_ENV = WEBLCM_PYTHON_EXTRA_PACKAGES='$(WEBLCM_PYTHON_EXTRA_PACKAGES)'
 
 ifeq ($(BR2_REPRODUCIBLE),y)
 define WEBLCM_PYTHON_FIX_TIME
@@ -31,7 +31,23 @@ define WEBLCM_PYTHON_FIX_TIME
 endef
 endif
 
-define POST_INSTALL_TARGET_HOOKS
+define WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOK_CMDS
+	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/fonts -m 644 $(WEBLCM_PYTHON_SITE)/assets/fonts/*
+	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/css -m 644 $(WEBLCM_PYTHON_SITE)/assets/css/*.css
+	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/img -m 644 $(WEBLCM_PYTHON_SITE)/assets/img/*.png
+	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/js -m 644 $(WEBLCM_PYTHON_SITE)/assets/js/*.js
+	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/i18n -m 644 $(WEBLCM_PYTHON_SITE)/assets/i18n/*.json
+	$(INSTALL) -D -t $(TARGET_DIR)/var/www -m 644 $(WEBLCM_PYTHON_SITE)/LICENSE
+
+	cp -fr $(WEBLCM_PYTHON_SITE)/plugins $(TARGET_DIR)/var/www/
+
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/scripts -m 755 $(WEBLCM_PYTHON_SITE)/*.sh
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/*.ini
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/zonelist.db
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/server.key
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/server.crt
+	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/ca.crt
+
 	cat /dev/null > $(TARGET_DIR)/etc/weblcm-python/swcert.conf
 	 ! grep -q 'CONFIG_SIGNED_IMAGES=y' ${BUILD_DIR}/swupdate*/include/config/auto.conf \
 		|| echo 'SWCERT=-k $(WEBLCM_PYTHON_SET_KEY_LOCATION_VALUE) --cert-purpose codeSigning' > $(TARGET_DIR)/etc/weblcm-python/swcert.conf
@@ -48,29 +64,9 @@ define POST_INSTALL_TARGET_HOOKS
 
 	sed -i -e '/^awm_cfg/d' $(TARGET_DIR)/etc/weblcm-python/weblcm-python.ini
 	sed -i -e '/\[weblcm\]/a awm_cfg:$(BR2_PACKAGE_ADAPTIVE_WW_BINARIES_CFG_FILE)' $(TARGET_DIR)/etc/weblcm-python/weblcm-python.ini
-
 endef
-WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOKS += POST_INSTALL_TARGET_HOOKS
 
-define WEBLCM_PYTHON_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/dist/weblcm_python-1.0-py$(TARGET_PYTHON_VERSION).egg $(TARGET_DIR)/usr/bin/weblcm-python
-
-	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/fonts -m 644 $(WEBLCM_PYTHON_SITE)/assets/fonts/*
-	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/css -m 644 $(WEBLCM_PYTHON_SITE)/assets/css/*.css
-	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/img -m 644 $(WEBLCM_PYTHON_SITE)/assets/img/*.png
-	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/js -m 644 $(WEBLCM_PYTHON_SITE)/assets/js/*.js
-	$(INSTALL) -D -t $(TARGET_DIR)/var/www/assets/i18n -m 644 $(WEBLCM_PYTHON_SITE)/assets/i18n/*.json
-	$(INSTALL) -D -t $(TARGET_DIR)/var/www -m 644 $(WEBLCM_PYTHON_SITE)/LICENSE
-
-	cp -fr $(WEBLCM_PYTHON_SITE)/plugins $(TARGET_DIR)/var/www/
-
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/scripts -m 755 $(WEBLCM_PYTHON_SITE)/*.sh
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/*.ini
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python -m 644 $(WEBLCM_PYTHON_SITE)/zonelist.db
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/server.key
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/server.crt
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/weblcm-python/ssl -m 644 $(WEBLCM_PYTHON_SITE)/ssl/ca.crt
-endef
+WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOKS += WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOK_CMDS
 
 define WEBLCM_PYTHON_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -d $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
