ab3f04b0aa7a530a3477292ab67d4318ba40dd10
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-04-18 10:19:07 -0400

    buildroot: igupd: Fixed missing dependencies and changed to standard install
    Replaced python-libconfig with python-libconf as it has more
    availability on other distros and much faster to build as it
    does not need boost
    
    Bug 21274

--- package/lrd/igupd/igupd.mk
+++ package/lrd/igupd/igupd.mk
@@ -6,16 +6,16 @@ IGUPD_VERSION = local
 IGUPD_SITE = package/lrd/externals/igupd
 IGUPD_SITE_METHOD = local
 IGUPD_SETUP_TYPE = setuptools
-IGUPD_BUILD_OPTS = bdist_egg --exclude-source-files
 
-define IGUPD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/dist/igupd-1.0-py$(PYTHON3_VERSION_MAJOR).egg $(TARGET_DIR)/usr/bin/igupd
-	$(INSTALL) -D -t $(TARGET_DIR)/etc/dbus-1/system.d -m 644 package/lrd/igupd/com.lairdtech.security.UpdateService.conf
-	$(INSTALL) -D -t $(TARGET_DIR)/etc -m 644 package/lrd/igupd/secupdate.cfg
+define IGUPD_POST_INSTALL_TARGET_HOOK_CMDS
+	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc -m 644 package/lrd/igupd/secupdate.cfg
 endef
+
+IGUPD_POST_INSTALL_TARGET_HOOKS += IGUPD_POST_INSTALL_TARGET_HOOK_CMDS
 
 define IGUPD_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/systemd/system package/lrd/igupd/igupd.service
+	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/etc/dbus-1/system.d package/lrd/igupd/com.lairdtech.security.UpdateService.conf
 endef
 
 $(eval $(python-package))
