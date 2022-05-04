a76077c5a4e9df354b0a3239b8fb432280a05fb3
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-05-04 15:59:46 -0400

    buildroot: python-pyudev: Updated to version 23.2
    To support latest libudev interface
    
    Bug 21274

--- package/python-pyudev/python-pyudev.mk
+++ package/python-pyudev/python-pyudev.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYUDEV_VERSION = 0.22.0
+PYTHON_PYUDEV_VERSION = 0.23.2
 PYTHON_PYUDEV_SOURCE = pyudev-$(PYTHON_PYUDEV_VERSION).tar.gz
-PYTHON_PYUDEV_SITE = https://files.pythonhosted.org/packages/72/c8/4660d815a79b1d42c409012aaa10ebd6b07a47529b4cb6880f27a24bd646
+PYTHON_PYUDEV_SITE = https://files.pythonhosted.org/packages/f8/fa/ae6c1a1a75f19560bbd875a579b2ca9b32deeae6a4c4a1997f4ec69a013e
 PYTHON_PYUDEV_LICENSE = LGPL-2.1+
 PYTHON_PYUDEV_LICENSE_FILES = COPYING
 PYTHON_PYUDEV_SETUP_TYPE = setuptools
