78c231ed1492145f0b60e1b363ad7b2d1cf33219
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-05-26 14:32:45 -0400

    buildroot: Fixed dcal & dcas builds
    Bug 21274

--- package/lrd/dcal/dcal.mk
+++ package/lrd/dcal/dcal.mk
@@ -8,14 +8,14 @@ DCAL_VERSION = local
 DCAL_SITE = package/lrd/externals/dcal
 DCAL_SITE_METHOD = local
 
-DCAL_DEPENDENCIES += host-flatcc flatcc libssh
+DCAL_DEPENDENCIES += flatcc libssh
 
-DCAL_MAKE_ENV += CROSS_COMPILE="$(TARGET_CROSS)"
+DCAL_MAKE_ENV = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS)
 
 ifeq ($(BR2_PACKAGE_DCAL_TEST_APPS),y)
 define BUILD_EXAMPLE_APPS
-    $(DCAL_MAKE_ENV) $(MAKE) -j 1 -C $(@D) test_clean
-    $(DCAL_MAKE_ENV) $(MAKE) -j 1 -C $(@D) test_apps
+    $(DCAL_MAKE_ENV) $(MAKE) -C $(@D) test_clean
+    $(DCAL_MAKE_ENV) $(MAKE) -C $(@D) test_apps
 endef
 endif
 
@@ -25,20 +25,15 @@ define DCAL_BUILD_CMDS
     $(BUILD_EXAMPLE_APPS)
 endef
 
-define DCAL_INSTALL_STAGING_CMDS
-endef
-
 ifeq ($(BR2_PACKAGE_DCAL_TEST_APPS),y)
 define INSTALL_EXAMPLE_APPS
-		mkdir -p $(TARGET_DIR)/user/share/dcal/examples
-		mkdir -p $(TARGET_DIR)/user/share/dcal/unit-tests/
-		$(INSTALL) -D -m 755 $(@D)/apps/examples/* $(TARGET_DIR)/user/share/dcal/examples/
-		$(INSTALL) -D -m 755 $(@D)/apps/unit-tests/* $(TARGET_DIR)/user/share/dcal/unit-tests/
+	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/user/share/dcal/examples/ $(@D)/apps/examples/*
+	$(INSTALL) -D -m 755 -t $(TARGET_DIR)/user/share/dcal/unit-tests/ $(@D)/apps/unit-tests/*
 endef
 endif
 
 define DCAL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/api/libdcal.so.1.0 $(TARGET_DIR)/usr/lib/libdcal.so.1.0
+	$(INSTALL) -D -m 644 $(@D)/api/libdcal.so.1.0 $(TARGET_DIR)/usr/lib/libdcal.so.1.0
 	cd $(TARGET_DIR)/usr/lib && ln -fs libdcal.so.1.0 libdcal.so.1
 	cd $(TARGET_DIR)/usr/lib && ln -fs libdcal.so.1.0 libdcal.so
 	$(INSTALL_EXAMPLE_APPS)
