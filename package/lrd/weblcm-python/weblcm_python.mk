a88cf4dbf8531d97395986a3d75b764b239f69bc
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2022-05-27 16:42:01 -0400

    swupdate: Systemd Integration
    This change enables better integration of swupdate running as a systemd
    service.
    
    Firstly, the swupdate-progress service is preset to be disabled as we are not
    using this functionality and monitor update progress ourselves (e.g., in
    igupd, etc.).
    
    Secondly, an environment variable configuration file has been added to set the
    run-time configuration file to use (/etc/swupdate.cfg) and the accepted select
    software image sets for use with swupdate.
    
    Lastly, since systemd is now responsible for starting the swupdate binary, the
    fw_update script has been updated to call the 'swupdate-client' application
    instead of 'swupdate' directly. Additionally, as the 'swupdate-client'
    application does not support downloading an update file from a remote URL, a
    call to curl (or wget if curl is not available) was added to download the file
    and stream it to 'swupdate-client'.
    
    Also, weblcm-python previously installed a systemd service unit file for
    swupdate that conflicts with the one provided by swupdate itself. This change
    removes that service unit file, replicates the proper generation of the
    SWUPDATE_ARGS environment variable to support signed images, and adds a
    dependency on systemd to weblcm-python.
    
    Bug 21723

--- package/lrd/weblcm-python/weblcm_python.mk
+++ package/lrd/weblcm-python/weblcm_python.mk
@@ -49,7 +49,7 @@ define WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOK_CMDS
 
 	cat /dev/null > $(TARGET_DIR)/etc/weblcm-python/swcert.conf
 	 ! grep -q 'CONFIG_SIGNED_IMAGES=y' ${BUILD_DIR}/swupdate*/include/config/auto.conf \
-		|| echo 'SWCERT=-k $(WEBLCM_PYTHON_SET_KEY_LOCATION_VALUE) --cert-purpose codeSigning' > $(TARGET_DIR)/etc/weblcm-python/swcert.conf
+		|| echo 'SWUPDATE_ARGS=${SWUPDATE_ARGS} -k $(WEBLCM_PYTHON_SET_KEY_LOCATION_VALUE) --cert-purpose codeSigning' > $(TARGET_DIR)/etc/swupdate/conf.d/99-weblcm-python.conf
 
 	sed -i -e '/^default_/d' $(TARGET_DIR)/etc/weblcm-python/weblcm-python.ini
 	sed -i -e '/\[weblcm\]/a default_password: \"$(WEBLCM_PYTHON_DEFAULT_PASSWORD)\"' $(TARGET_DIR)/etc/weblcm-python/weblcm-python.ini
@@ -70,8 +70,6 @@ WEBLCM_PYTHON_POST_INSTALL_TARGET_HOOKS += WEBLCM_PYTHON_POST_INSTALL_TARGET_HOO
 define WEBLCM_PYTHON_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -t $(TARGET_DIR)/usr/lib/systemd/system -m 644 $(WEBLCM_PYTHON_SITE)/weblcm-python.service
 	$(WEBLCM_PYTHON_FIX_TIME)
-
-	$(INSTALL) -D -t $(TARGET_DIR)/usr/lib/systemd/system -m 644 $(WEBLCM_PYTHON_SITE)/swupdate.service
 endef
 
 $(eval $(python-package))
