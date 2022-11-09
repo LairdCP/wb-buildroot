e6fe6a67d16999b2474975c9c0d3fe33bfd6268a
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-11-09 08:23:19 -0500

    buildroot: lrd-network-manager: Convert build to meson
    That what upstream is using now and it is way faster
    
    Bug 23077

--- package/lrd/lrd-network-manager/lrd-network-manager.mk
+++ package/lrd/lrd-network-manager/lrd-network-manager.mk
@@ -1,10 +1,16 @@
 ################################################################################
 #
-# Laird network-manager
+# Summit network-manager
 #
 ################################################################################
 
-LRD_NETWORK_MANAGER_CVE_PRODUCT = network-manager
+LRD_NETWORK_MANAGER_INSTALL_STAGING = YES
+LRD_NETWORK_MANAGER_LICENSE = GPL-2.0+ (app), LGPL-2.1+ (libnm)
+LRD_NETWORK_MANAGER_LICENSE_FILES = COPYING COPYING.LGPL CONTRIBUTING.md
+LRD_NETWORK_MANAGER_CPE_ID_VENDOR = gnome
+LRD_NETWORK_MANAGER_CPE_ID_PRODUCT = networkmanager
+LRD_NETWORK_MANAGER_SELINUX_MODULES = networkmanager
+LRD_NETWORK_MANAGER_CVE_PRODUCT = networkmanager
 LRD_NETWORK_MANAGER_CVE_VERSION = 1.36.8
 
 ifeq ($(BR2_LRD_DEVEL_BUILD),)
@@ -29,100 +35,172 @@ LRD_NETWORK_MANAGER_SITE_METHOD = local
 
 endif
 
-LRD_NETWORK_MANAGER_INSTALL_STAGING = YES
-LRD_NETWORK_MANAGER_DEPENDENCIES = host-pkgconf udev gnutls libglib2 \
-	util-linux host-intltool readline libndp
-LRD_NETWORK_MANAGER_LICENSE = GPL-2.0+ (app), LGPL-2.1+ (libnm)
-LRD_NETWORK_MANAGER_LICENSE_FILES = COPYING COPYING.LGPL CONTRIBUTING.md
-
-LRD_NETWORK_MANAGER_AUTORECONF = YES
-
-LRD_NETWORK_MANAGER_CONF_ENV = \
-	ac_cv_file__etc_fedora_release=no \
-	ac_cv_file__etc_mandriva_release=no \
-	ac_cv_file__etc_debian_version=no \
-	ac_cv_file__etc_redhat_release=no \
-	ac_cv_file__etc_SuSE_release=no
+LRD_NETWORK_MANAGER_DEPENDENCIES = \
+	host-intltool \
+	host-pkgconf \
+	dbus \
+	libglib2 \
+	libndp \
+	udev \
+	util-linux
 
 LRD_NETWORK_MANAGER_CONF_OPTS = \
-	--disable-tests \
-	--disable-qt \
-	--disable-more-warnings \
-	--with-crypto=gnutls \
-	--with-iptables=/usr/sbin/iptables \
-	--disable-ifupdown \
-	--without-nm-cloud-setup
+	-Ddocs=false \
+	-Dtests=no \
+	-Dqt=false \
+	-Diptables=/usr/sbin/iptables \
+	-Difupdown=false \
+	-Dnm_cloud_setup=false \
+	-Dsession_tracking_consolekit=false \
+	-Dwext=false
 
-ifeq ($(BR2_PACKAGE_OFONO),y)
-LRD_NETWORK_MANAGER_DEPENDENCIES += ofono
-LRD_NETWORK_MANAGER_CONF_OPTS += --with-ofono
+ifeq ($(BR2_PACKAGE_AUDIT),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += audit
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dlibaudit=yes
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --without-ofono
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dlibaudit=no
+endif
+
+ifeq ($(BR2_PACKAGE_DHCP_CLIENT),y)
+LRD_NETWORK_MANAGER_CONF_OPTS += -Ddhclient=/sbin/dhclient
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Ddhclient=no
+endif
+
+ifeq ($(BR2_PACKAGE_DHCPCD),y)
+LRD_NETWORK_MANAGER_CONF_OPTS += -Ddhcpcd=/sbin/dhcpcd
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Ddhcpcd=no
+endif
+
+ifeq ($(BR2_PACKAGE_IWD),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += iwd
+LRD_NETWORK_MANAGER_CONF_OPTS += -Diwd=true
+ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT),y)
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dconfig_wifi_backend_default=wpa_supplicant
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dconfig_wifi_backend_default=iwd
+endif
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += \
+	-Diwd=false \
+	-Dconfig_wifi_backend_default=wpa_supplicant
 endif
 
 ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_CONCHECK),y)
 LRD_NETWORK_MANAGER_DEPENDENCIES += libcurl
-LRD_NETWORK_MANAGER_CONF_OPTS += --enable-concheck
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dconcheck=true
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --disable-concheck
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dconcheck=false
 endif
 
-ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_TUI),y)
-LRD_NETWORK_MANAGER_DEPENDENCIES += newt
-LRD_NETWORK_MANAGER_CONF_OPTS += --with-nmtui=yes
+ifeq ($(BR2_PACKAGE_LIBNSS),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += libnss
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dcrypto=nss
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --with-nmtui=no
+LRD_NETWORK_MANAGER_DEPENDENCIES += gnutls
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dcrypto=gnutls
+endif
+
+ifeq ($(BR2_PACKAGE_LIBPSL),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += libpsl
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dlibpsl=true
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dlibpsl=false
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSELINUX),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += libselinux
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dselinux=true
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dselinux=false
+endif
+
+ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_MODEM_MANAGER),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += modem-manager mobile-broadband-provider-info
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dmodem_manager=true
+ifeq ($(BR2_PACKAGE_OFONO),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += ofono
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dofono=true
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dofono=false
+endif
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dmodem_manager=false -Dofono=false
+endif
+
+ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_OVS),y)
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dovs=true
+LRD_NETWORK_MANAGER_DEPENDENCIES += jansson
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dovs=false
 endif
 
 ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_PPPD),y)
 LRD_NETWORK_MANAGER_DEPENDENCIES += pppd
-LRD_NETWORK_MANAGER_CONF_OPTS += --enable-ppp
+LRD_NETWORK_MANAGER_CONF_OPTS += \
+	-Dppp=true \
+	-Dpppd=/usr/sbin/pppd \
+	-Dpppd_plugin_dir=/usr/lib/pppd/$(PPPD_VERSION)
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --disable-ppp
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dppp=false
 endif
 
-ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_MODEM_MANAGER),y)
-LRD_NETWORK_MANAGER_DEPENDENCIES += modem-manager
-LRD_NETWORK_MANAGER_CONF_OPTS += --with-modem-manager-1
+ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_TUI),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += newt
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dnmtui=true
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --without-modem-manager-1
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dnmtui=false
 endif
 
-ifeq ($(BR2_PACKAGE_DHCP_CLIENT),y)
-LRD_NETWORK_MANAGER_CONF_OPTS += --with-dhclient=/sbin/dhclient
-endif
-
-ifeq ($(BR2_PACKAGE_DHCPCD),y)
-LRD_NETWORK_MANAGER_CONF_OPTS += --with-dhcpcd=/sbin/dhcpcd --with-dhcpcd-supports-ipv6=yes
-endif
-
-ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_OVS),y)
-LRD_NETWORK_MANAGER_CONF_OPTS += --enable-ovs
-LRD_NETWORK_MANAGER_DEPENDENCIES += jansson
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += systemd
+LRD_NETWORK_MANAGER_CONF_OPTS += \
+	-Dsystemd_journal=true \
+	-Dconfig_logging_backend_default=journal \
+	-Dsession_tracking=systemd \
+	-Dsuspend_resume=systemd
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --disable-ovs
+LRD_NETWORK_MANAGER_CONF_OPTS += \
+	-Dsystemd_journal=false \
+	-Dconfig_logging_backend_default=syslog \
+	-Dsession_tracking=no \
+	-Dsuspend_resume=upower \
+	-Dsystemdsystemunitdir=no
+endif
+
+ifeq ($(BR2_PACKAGE_POLKIT),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += polkit
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dpolkit=true
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dpolkit=false
+endif
+
+ifeq ($(BR2_PACKAGE_LRD_NETWORK_MANAGER_CLI),y)
+LRD_NETWORK_MANAGER_DEPENDENCIES += readline
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dnmcli=true
+else
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dnmcli=false
 endif
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
 LRD_NETWORK_MANAGER_DEPENDENCIES += gobject-introspection
-LRD_NETWORK_MANAGER_CONF_OPTS += --enable-introspection
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dintrospection=true
 else
-LRD_NETWORK_MANAGER_CONF_OPTS += --disable-introspection
+LRD_NETWORK_MANAGER_CONF_OPTS += -Dintrospection=false
 endif
 
-define LRD_NETWORK_MANAGER_UPDATE_SERVICE
-	$(SED) '/^ExecStart=/ s#.*#ExecStart=/usr/sbin/NetworkManager --no-daemon --state-file=/etc/NetworkManager/NetworkManager.state#g' $(TARGET_DIR)/usr/lib/systemd/system/NetworkManager.service
-endef
-
 define LRD_NETWORK_MANAGER_INSTALL_INIT_SYSV
-	$(INSTALL) -m 0755 -D $(LRD_NETWORK_MANAGER_PKGDIR)/S45network-manager $(TARGET_DIR)/etc/init.d/S45network-manager
+	$(INSTALL) -m 0755 -D $(LRD_NETWORK_MANAGER_PKGDIR)/S45network-manager \
+		$(TARGET_DIR)/etc/init.d/S45network-manager
 endef
 
 define LRD_NETWORK_MANAGER_INSTALL_INIT_SYSTEMD
-	ln -rsf $(TARGET_DIR)/usr/lib/systemd/system/NetworkManager.service \
+	ln -sf /usr/lib/systemd/system/NetworkManager.service \
 		$(TARGET_DIR)/etc/systemd/system/dbus-org.freedesktop.NetworkManager.service
 
-	$(LRD_NETWORK_MANAGER_UPDATE_SERVICE)
+	$(SED) 's,--no-daemon,--no-daemon --state-file=/etc/NetworkManager/NetworkManager.state,' \
+		$(TARGET_DIR)/usr/lib/systemd/system/NetworkManager.service
 endef
 
 # create directories that may not be populated on certain builds
@@ -133,4 +211,4 @@ endef
 
 LRD_NETWORK_MANAGER_TARGET_FINALIZE_HOOKS += LRD_NETWORK_MANAGER_CREATE_EMPTY_DIRS
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
