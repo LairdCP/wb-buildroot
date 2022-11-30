c5f0133d317ed37f36c328ea5ebfeea66ab611c9
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-11-30 16:20:41 -0500

    Added swupdate based wbx3 builds
    Bug 23269

--- board/laird/som60/post_build.sh
+++ board/laird/som60/post_build.sh
@@ -4,5 +4,5 @@ DEVEL_KEYS="${3}"
 
 . "${BOARD_DIR}/../post_build_common_60.sh" "${BOARD_DIR}" "${BUILD_TYPE}" "${DEVEL_KEYS}"
 
-[ -f ${TARGET_DIR}/lib/firmware/regulatory_summit60.db ] && \
+[ ! -f ${TARGET_DIR}/lib/firmware/regulatory_summit60.db ] || \
     ln -sfr ${TARGET_DIR}/lib/firmware/regulatory_summit60.db ${TARGET_DIR}/lib/firmware/regulatory.db
