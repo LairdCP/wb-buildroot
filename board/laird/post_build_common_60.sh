dcca7a47300a0d407c498cde5b061b21fc21c00c
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-12-01 15:57:51 -0500

    Added swupdate based wbx3 builds
    Bug 23269

--- board/laird/post_build_common_60.sh
+++ board/laird/post_build_common_60.sh
@@ -223,8 +223,16 @@ if grep -q 'BR2_DEFCONFIG=.*_fips_dev_.*' ${BR2_CONFIG}; then
 	${fipshmac} -d ${TARGET_DIR}/usr/lib/fipscheck/ ${TARGET_DIR}/usr/lib/libcrypto.so.1.0.0
 fi
 
-if [ -f ${TARGET_DIR}/etc/inittab ] && grep -qF "BR2_TARGET_ENABLE_ROOT_LOGIN=y" ${BR2_CONFIG}; then
-	sed -i -e 's,^#.*/getty.*,::respawn:-/bin/sh,' ${TARGET_DIR}/etc/inittab
+if grep -qF 'BR2_TARGET_GENERIC_ROOT_PASSWD=""' ${BR2_CONFIG} && \
+   grep -qF BR2_TARGET_ENABLE_ROOT_LOGIN=y ${BR2_CONFIG}
+then
+
+if [ -f ${TARGET_DIR}/etc/inittab ]; then
+	sed -i -e 's,^.*/getty.*,::respawn:-/bin/sh,' ${TARGET_DIR}/etc/inittab
+else
+	sed -i -e 's,/agetty -o,/agetty -a root -o,g' ${TARGET_DIR}/usr/lib/systemd/system/serial-getty@.service
+fi
+
 fi
 
 echo "${BR2_LRD_PRODUCT^^} POST BUILD script: done."
