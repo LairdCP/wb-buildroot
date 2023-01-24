ceacedc490355e148d1f34817c60f8859633ec62
James Kalbfleisch <james.kalbfleisch@lairdconnect.com>
2023-01-24 12:29:33 -0500

    Revert "brcm_patchram_plus integration fixes"
    This reverts commit af57582340042d317a63aed4226be4fa19639e6a.

--- package/brcm-patchram-plus/brcm-patchram-plus.mk
+++ package/brcm-patchram-plus/brcm-patchram-plus.mk
@@ -4,25 +4,10 @@
 #
 ################################################################################
 
-BRCM_PATCHRAM_PLUS_VERSION = local
-BRCM_PATCHRAM_PLUS_SITE = package/lrd/externals/brcm_patchram
-BRCM_PATCHRAM_PLUS_SITE_METHOD = local
-
+BRCM_PATCHRAM_PLUS_VERSION = 95b7b6916d661a4da3f9c0adf52d5e1f4f8ab042
+BRCM_PATCHRAM_PLUS_SITE = $(call github,AsteroidOS,brcm-patchram-plus,$(BRCM_PATCHRAM_PLUS_VERSION))
 BRCM_PATCHRAM_PLUS_LICENSE = Apache-2.0
-BRCM_PATCHRAM_PLUS_LICENSE_FILES = LICENSE
-
-ifeq ($(BR2_PACKAGE_BLUEZ_UTILS),y)
-	BRCM_PATCHRAM_PLUS_DEPENDENCIES = bluez_utils
-else
-	BRCM_PATCHRAM_PLUS_DEPENDENCIES = bluez5_utils
-endif
-
-define BRCM_PATCHRAM_PLUS_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) brcm_patchram_plus
-endef
-
-define BRCM_PATCHRAM_PLUS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/brcm_patchram_plus $(TARGET_DIR)/usr/bin/
-endef
+BRCM_PATCHRAM_PLUS_LICENSE_FILES = COPYING
+BRCM_PATCHRAM_PLUS_AUTORECONF = YES
 
 $(eval $(generic-package))
